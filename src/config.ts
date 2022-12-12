d2c298b079f6b855c4139fb7868fec8a20ca48c7
lukaw3d <lukajeran@oasislabs.com>
2022-12-12 18:15:47 +0100

    Simplify network config

--- src/config.ts
+++ src/config.ts
@@ -1,76 +1,4 @@
-export enum BackendAPIs {
-  OasisMonitor = 'oasismonitor',
-  OasisScan = 'oasisscan',
-}
-
-export type NetworkType = 'local' | 'testnet' | 'mainnet'
-
 export const consensusDecimals = 9
-
-type BackendApiUrls = {
-  explorer: string
-  blockExplorer: string
-  blockExplorerParatimes?: string
-}
-
-type BackendProviders = {
-  grpc: string
-  ticker: string // from nic.stakingTokenSymbol()
-  min_delegation: number // from nic.stakingConsensusParameters().min_delegation
-  [BackendAPIs.OasisMonitor]: BackendApiUrls
-  [BackendAPIs.OasisScan]: BackendApiUrls
-}
-
-type BackendConfig = {
-  [key in NetworkType]: BackendProviders
-}
-
-export const config: BackendConfig = {
-  mainnet: {
-    grpc: 'https://grpc.oasis.dev',
-    ticker: 'ROSE',
-    min_delegation: 100,
-    [BackendAPIs.OasisMonitor]: {
-      explorer: 'https://monitor.oasis.dev',
-      blockExplorer: 'https://oasismonitor.com/operation/{{txHash}}',
-    },
-    [BackendAPIs.OasisScan]: {
-      explorer: 'https://api.oasisscan.com/mainnet',
-      blockExplorer: 'https://oasisscan.com/transactions/{{txHash}}',
-      blockExplorerParatimes: 'https://oasisscan.com/paratimes/transactions/{{txHash}}?runtime={{runtimeId}}',
-    },
-  },
-  testnet: {
-    grpc: 'https://testnet.grpc.oasis.dev',
-    ticker: 'TEST',
-    min_delegation: 100,
-    [BackendAPIs.OasisMonitor]: {
-      explorer: 'https://monitor.oasis.dev/api/testnet',
-      blockExplorer: 'https://testnet.oasismonitor.com/operation/{{txHash}}',
-    },
-    [BackendAPIs.OasisScan]: {
-      explorer: 'https://api.oasisscan.com/testnet',
-      blockExplorer: 'https://testnet.oasisscan.com/transactions/{{txHash}}',
-      blockExplorerParatimes:
-        'https://testnet.oasisscan.com/paratimes/transactions/{{txHash}}?runtime={{runtimeId}}',
-    },
-  },
-  local: {
-    grpc: 'http://localhost:42280',
-    ticker: 'TEST',
-    min_delegation: 100,
-    [BackendAPIs.OasisMonitor]: {
-      explorer: 'http://localhost:9001',
-      blockExplorer: 'http://localhost:9001/data/transactions?operation_id={{txHash}}',
-    },
-    [BackendAPIs.OasisScan]: {
-      explorer: 'http://localhost:9001',
-      blockExplorer: 'http://localhost:9001/data/transactions?operation_id={{txHash}}',
-      blockExplorerParatimes:
-        'http://localhost:9001/data/paratimes/transactions/{{txHash}}?runtime={{runtimeId}}',
-    },
-  },
-}
 
 type ParaTimeNetwork = {
   address: string | undefined
@@ -157,6 +85,3 @@ export const paraTimesConfig: ParaTimesConfig = {
   [ParaTime.Emerald]: emeraldConfig,
   [ParaTime.Sapphire]: sapphireConfig,
 }
-
-// https://github.com/mozilla/webextension-polyfill/blob/6e3e26c/src/browser-polyfill.js#L9
-export const runtimeIs = (window as any).chrome?.runtime?.id ? 'extension' : 'webapp'
