11fc44d41c72df77098b78df379338e92927b207
Michal Zielenkiewicz <michal@oasisprotocol.org>
2022-12-19 12:36:29 +0100

    Remove margin in mobile PageLayout

--- src/app/components/PageLayout/index.tsx
+++ src/app/components/PageLayout/index.tsx
@@ -2,10 +2,15 @@ import { ReactNode } from 'react'
 import { Header } from './Header'
 import { Footer } from './Footer'
 import Box from '@mui/material/Box'
+import useMediaQuery from '@mui/material/useMediaQuery'
+import { useTheme } from '@mui/material/styles'
 
 export function PageLayout({ children }: { children: ReactNode }) {
+  const theme = useTheme()
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
+
   return (
-    <Box sx={{ pt: 5, px: '5%' }}>
+    <Box sx={{ pt: 5, px: isMobile ? 0 : '5%' }}>
       <Header />
       <main>{children}</main>
       <Footer />
