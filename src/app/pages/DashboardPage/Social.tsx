537fb267dea89ed1b0bd930bff990d640f1a8b30
Michal Zielenkiewicz <michal@oasisprotocol.org>
2022-12-19 12:59:43 +0100

    Tweak mobile Social section

--- src/app/pages/DashboardPage/Social.tsx
+++ src/app/pages/DashboardPage/Social.tsx
@@ -3,6 +3,8 @@ import Box from '@mui/material/Box'
 import Grid from '@mui/material/Unstable_Grid2'
 import Link from '@mui/material/Link'
 import Typography from '@mui/material/Typography'
+import useMediaQuery from '@mui/material/useMediaQuery'
+import { useTheme } from '@mui/material/styles'
 import social from './images/social.png'
 import telegram from './images/telegram.svg'
 import twitter from './images/twitter.svg'
@@ -13,32 +15,39 @@ import reddit from './images/reddit.svg'
 type SocialLinkProps = {
   label: string
   href: string
+  isMobile: boolean
   img: string
 }
 
-const SocialLink: FC<SocialLinkProps> = ({ label, href, img }) => {
+const SocialLink: FC<SocialLinkProps> = ({ label, href, isMobile, img }) => {
   return (
     <Link
       href={href}
       color="#fff"
       underline="none"
-      sx={{ display: 'flex', flexDirection: 'column', mr: 6 }}
+      sx={{ display: 'flex', flexDirection: 'column', mx: isMobile ? 5 : 0, mt: isMobile ? 4 : 0 }}
       rel="noopener noreferrer"
       target="_blank"
     >
-      <img src={img} alt={label} height={40} />
-      {label}
+      <Box sx={{ display: 'flex', justifyContent: 'center', mb: 3 }}>
+        <img src={img} alt={label} height={40} />
+      </Box>
+      <Typography sx={{ mb: isMobile ? 4 : 0 }}>{label}</Typography>
     </Link>
   )
 }
 
 export function Social() {
+  const theme = useTheme()
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
+
   return (
     <Grid
       container
       sx={{
-        px: 6,
-        py: 4,
+        px: isMobile ? 5 : 6,
+        pt: 5,
+        pb: isMobile ? 4 : 5,
         border: 'solid 1px #fff',
         borderRadius: 12,
         backgroundImage: `url(${social})`,
@@ -58,20 +67,42 @@ export function Social() {
           sx={{
             display: 'flex',
             alignItems: 'center',
-            justifyContent: 'flex-end',
+            flexWrap: 'wrap',
+            justifyContent: isMobile ? 'center' : 'space-between',
             width: '100%',
             height: '100%',
           }}
         >
-          <SocialLink label="Telegram" href="https://t.me/oasisprotocolcommunity" img={telegram} />
-          <SocialLink label="Twitter" href="https://twitter.com/oasisprotocol" img={twitter} />
-          <SocialLink label="Discord" href="https://discord.gg/BQCxwhT5wS" img={discord} />
           <SocialLink
+            isMobile={isMobile}
+            label="Telegram"
+            href="https://t.me/oasisprotocolcommunity"
+            img={telegram}
+          />
+          <SocialLink
+            isMobile={isMobile}
+            label="Twitter"
+            href="https://twitter.com/oasisprotocol"
+            img={twitter}
+          />
+          <SocialLink
+            isMobile={isMobile}
+            label="Discord"
+            href="https://discord.gg/BQCxwhT5wS"
+            img={discord}
+          />
+          <SocialLink
+            isMobile={isMobile}
             label="Youtube"
             href="https://www.youtube.com/channel/UC35UFPcZ2F1wjPxhPrSsESQ"
             img={youtube}
           />
-          <SocialLink label="Reddit" href="https://www.reddit.com/r/oasisnetwork/" img={reddit} />
+          <SocialLink
+            isMobile={isMobile}
+            label="Reddit"
+            href="https://www.reddit.com/r/oasisnetwork/"
+            img={reddit}
+          />
         </Box>
       </Grid>
     </Grid>
