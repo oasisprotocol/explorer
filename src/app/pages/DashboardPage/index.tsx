b0f39dfc558a357d5a5199cdec95f21a385a05b4
Matej Lubej <9722540+lubej@users.noreply.github.com>
2022-12-21 12:48:05 +0100

    Add duration select

--- src/app/pages/DashboardPage/index.tsx
+++ src/app/pages/DashboardPage/index.tsx
@@ -9,23 +9,36 @@ import { TransactionsStats } from './TransactionsStats'
 import { AverageTransactionSize } from './AverageTransactionSize'
 import { PageLayout } from '../../components/PageLayout'
 import { TransactionsChartCard } from './TransactionsChartCard'
+import Typography from '@mui/material/Typography'
+import { COLORS } from '../../../styles/theme/colors'
+import { DurationSelect } from '../../components/DurationSelect'
 
-export const DashboardPage: FC = () => (
-  <PageLayout>
-    <Grid container spacing="10px">
-      <Grid item xs={12} md={3}>
-        <TransactionsChartCard />
+export const DashboardPage: FC = () => {
+  return (
+    <PageLayout>
+      <Grid container spacing="2">
+        <Grid item>
+          <Typography variant="h3" sx={{color: COLORS.white}}>Paratime Snapshot</Typography>
+        </Grid>
+        <Grid item>
+          <DurationSelect />
+        </Grid>
       </Grid>
-      <Grid item xs={12} md={3}>
-        Active accounts
+
+      <Grid container spacing="10px">
+        <Grid item xs={12} md={3}>
+          <TransactionsChartCard />
+        </Grid>
+        <Grid item xs={12} md={3}>
+          Active accounts
+        </Grid>
+        <Grid item xs={12} md={3}>
+          Compute nodes
+        </Grid>
+        <Grid item xs={12} md={3}>
+          ROSE Price
+        </Grid>
       </Grid>
-      <Grid item xs={12} md={3}>
-        Compute nodes
-      </Grid>
-      <Grid item xs={12} md={3}>
-        ROSE Price
-      </Grid>
-    </Grid>
 
     <Divider variant="layout" />
     <LatestTransactions />
