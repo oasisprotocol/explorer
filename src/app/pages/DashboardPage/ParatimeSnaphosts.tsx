96c9c7ae0c91bbb7d9be7bc97ca2b051bfe6674a
Matej Lubej <9722540+lubej@users.noreply.github.com>
2022-12-22 12:30:04 +0100

    Make DurationSelect uncontrolled

--- src/app/pages/DashboardPage/ParatimeSnaphosts.tsx
+++ src/app/pages/DashboardPage/ParatimeSnaphosts.tsx
@@ -1,4 +1,4 @@
-import { FC, useCallback, useState } from 'react'
+import { FC, useState } from 'react'
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
 import { COLORS } from '../../../styles/theme/colors'
@@ -6,33 +6,38 @@ import { DurationSelect } from './DurationSelect'
 import { TransactionsChartCard } from './TransactionsChartCard'
 import { ChartDuration } from '../../utils/chart-utils'
 import { useTranslation } from 'react-i18next'
+import { useConstant } from '../../hooks/useConstant'
 
 export const ParatimeSnaphosts: FC = () => {
   const { t } = useTranslation()
-  const [chartDuration, setChartDuration] = useState<ChartDuration>(ChartDuration.TODAY)
+  const defaultChartDurationValue = useConstant<ChartDuration>(() => ChartDuration.TODAY)
+  const [chartDuration, setChartDuration] = useState<ChartDuration>(defaultChartDurationValue)
 
-  const handleDurationSelectedChange = useCallback((duration: ChartDuration | null) => {
+  const handleDurationSelectedChange = (duration: ChartDuration | null) => {
     if (!duration) {
       return
     }
 
     setChartDuration(duration)
-  }, [])
+  }
 
   return (
     <>
       <Grid container sx={{ display: 'flex', alignItems: 'center', gap: 3, mb: 4 }}>
         <Grid item>
           <Typography variant="h3" sx={{ color: COLORS.white }}>
-            {t('.paratimeSnaphosts.header')}
+            {t('paratimeSnaphosts.header')}
           </Typography>
         </Grid>
         <Grid item>
-          <DurationSelect defaultValue={chartDuration} handleChange={handleDurationSelectedChange} />
+          <DurationSelect
+            defaultValue={defaultChartDurationValue}
+            handleChange={handleDurationSelectedChange}
+          />
         </Grid>
       </Grid>
 
-      <Grid container spacing="10px">
+      <Grid container spacing={4}>
         <Grid item xs={12} md={3}>
           <TransactionsChartCard chartDuration={chartDuration} />
         </Grid>
