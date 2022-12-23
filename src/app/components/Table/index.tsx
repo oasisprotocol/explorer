4ae466d63328b0e1fa9a3f5553f60c0c6136b08b
Kristof Csillag <csillag@nolmecolindor.com>
2022-12-23 12:56:52 +0100

    Factor out and export some more types in Table

--- src/app/components/Table/index.tsx
+++ src/app/components/Table/index.tsx
@@ -40,16 +40,17 @@ type TableCellProps = {
   key: string
 }
 
-type TableRowProps = {
+export type TableRowProps = {
   key: string
   data: TableCellProps[]
 }
 
+export type TableColProps = {
+  content: string
+  align?: TableCellAlign
+}
 type TableProps = {
-  columns: {
-    content: string
-    align?: TableCellAlign
-  }[]
+  columns: TableColProps[]
   columnsNumber?: number
   name: string
   isLoading: boolean
