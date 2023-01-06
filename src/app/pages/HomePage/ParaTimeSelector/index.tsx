176aa5193526f8f342cd9e9d4c315361d259d939
lubej <9722540+lubej@users.noreply.github.com>
2023-01-06 13:44:05 +0100

    Optimize imports

--- src/app/pages/HomePage/ParaTimeSelector/index.tsx
+++ src/app/pages/HomePage/ParaTimeSelector/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, memo, useState } from 'react'
+import { FC, memo, useState } from 'react'
 import { styled } from '@mui/material/styles'
 import Box from '@mui/material/Box'
 import paratimeSelectorGlow from './images/paratime-selector-glow.svg'
