6646311ba1ab98ea2a5ebf61bc6980601492218c
lukaw3d <lukajeran@oasislabs.com>
2023-01-10 02:40:40 +0100

    Improve wrapped API's types

--- src/app/pages/TransactionsPage/index.tsx
+++ src/app/pages/TransactionsPage/index.tsx
@@ -5,9 +5,10 @@ import useMediaQuery from '@mui/material/useMediaQuery'
 import { useTheme } from '@mui/material/styles'
 import { PageLayout } from '../../components/PageLayout'
 import { SubPageCard } from '../../components/SubPageCard'
-import { Transactions } from '../../components/Transactions'
+import { TableRuntimeTransactionList, Transactions } from '../../components/Transactions'
 import { useGetEmeraldTransactions } from '../../../oasis-indexer/api'
 import { NUMBER_OF_ITEMS_ON_SEPARATE_PAGE, REFETCH_INTERVAL } from '../../config'
+import { AxiosResponse } from 'axios'
 
 const limit = NUMBER_OF_ITEMS_ON_SEPARATE_PAGE
 
@@ -15,23 +16,20 @@ export const TransactionsPage: FC = () => {
   const { t } = useTranslation()
   const theme = useTheme()
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
-  const transactionsQuery = useGetEmeraldTransactions(
+  const transactionsQuery = useGetEmeraldTransactions<AxiosResponse<TableRuntimeTransactionList>>(
     { limit: limit },
     {
       query: {
         refetchInterval: REFETCH_INTERVAL,
         structuralSharing: (previousState, nextState) => {
-          if (!previousState) {
-            return nextState
-          }
-          const oldTxHashes = new Set(previousState.data?.transactions?.map(tx => tx.hash!))
+          const oldTxHashes = new Set(previousState?.data?.transactions?.map(tx => tx.hash!))
           return {
             ...nextState,
             data: {
               transactions: nextState.data?.transactions?.map(tx => {
                 return {
                   ...tx,
-                  markAsNew: !oldTxHashes.has(tx.hash!),
+                  markAsNew: previousState ? !oldTxHashes.has(tx.hash!) : false,
                 }
               }),
             },
