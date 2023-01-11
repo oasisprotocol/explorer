773fa74237abf7e08ecaca7dcd0d85981144aa12
Michal Zielenkiewicz <michal@oasisprotocol.org>
2023-01-11 14:08:35 +0100

    Update detail page header

--- src/app/components/SubPageCard/index.tsx
+++ src/app/components/SubPageCard/index.tsx
@@ -1,4 +1,5 @@
 import { FC, PropsWithChildren } from 'react'
+import Box from '@mui/material/Box'
 import Card from '@mui/material/Card'
 import CardHeader from '@mui/material/CardHeader'
 import CardContent from '@mui/material/CardContent'
@@ -6,10 +7,29 @@ import { Typography } from '@mui/material'
 import useMediaQuery from '@mui/material/useMediaQuery'
 import { useTheme } from '@mui/material/styles'
 import { COLORS } from '../../../styles/theme/colors'
+import { styled, css } from '@mui/material/styles'
 
-type SubPageCardProps = PropsWithChildren & { title: string }
+type SubPageCardProps = PropsWithChildren & { leadingHeader?: boolean; title: string }
 
-export const SubPageCard: FC<SubPageCardProps> = ({ children, title }) => {
+type StyledBoxProps = {
+  leadingHeader?: boolean
+}
+
+const StyledBox = styled(Box, {
+  shouldForwardProp: prop => prop !== 'headerShadow',
+})<StyledBoxProps>(
+  ({ leadingHeader, theme }) => css`
+    ${leadingHeader &&
+    `
+      margin: -${theme.spacing(4)} -${theme.spacing(5)} ${theme.spacing(5)};
+      padding: ${theme.spacing(5)} ${theme.spacing(6)} ${theme.spacing(4)};
+      border-radius: 12px;
+      box-shadow: -20px 4px 40px rgba(34, 47, 63, 0.24);
+    `};
+  `,
+)
+
+export const SubPageCard: FC<SubPageCardProps> = ({ children, leadingHeader, title }) => {
   const theme = useTheme()
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
 
@@ -21,7 +41,11 @@ export const SubPageCard: FC<SubPageCardProps> = ({ children, title }) => {
         </Typography>
       )}
       <Card>
-        {!isMobile && <CardHeader disableTypography component="h3" title={title} />}
+        {!isMobile && (
+          <StyledBox leadingHeader={leadingHeader}>
+            <CardHeader disableTypography component="h3" title={title} />
+          </StyledBox>
+        )}
         <CardContent>{children}</CardContent>
       </Card>
     </>
