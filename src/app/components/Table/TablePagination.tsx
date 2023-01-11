2d6040f4e31d7dbeb87fd6ff9682d9c22c8ace4e
lukaw3d <lukajeran@oasislabs.com>
2023-01-11 19:15:31 +0100

    Use links for pagination

--- src/app/components/Table/TablePagination.tsx
+++ src/app/components/Table/TablePagination.tsx
@@ -3,6 +3,7 @@ import PaginationItem from '@mui/material/PaginationItem'
 import { FC } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useSearchParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 type TablePaginationProps = {
   /** Number of pages */
@@ -13,7 +14,7 @@ type TablePaginationProps = {
 
 export const TablePagination: FC<TablePaginationProps> = ({ count, rowsNumber }) => {
   const { t } = useTranslation()
-  const [searchParams, setSearchParams] = useSearchParams()
+  const [searchParams] = useSearchParams()
   const offsetSearchQuery = searchParams.get('offset')
   const offset = (offsetSearchQuery && parseInt(offsetSearchQuery, 10)) || 0
   const page = offset ? offset / rowsNumber + 1 : 1
@@ -22,23 +23,14 @@ export const TablePagination: FC<TablePaginationProps> = ({ count, rowsNumber })
     <Pagination
       count={count}
       page={page}
-      onChange={(e, newPage) => {
-        const newOffset = (newPage - 1) * rowsNumber
-        if (newOffset) {
-          setSearchParams({
-            offset: `${newOffset}`,
-          })
-        } else {
-          searchParams.delete('offset')
-          setSearchParams(searchParams)
-        }
-      }}
       renderItem={item => (
         <PaginationItem
           slots={{
             first: () => <>{t('pagination.first')}</>,
             last: () => <>{t('pagination.last')}</>,
           }}
+          component={Link}
+          to={{ search: item.page && item.page > 1 ? `?offset=${(item.page - 1) * rowsNumber}` : '' }}
           {...item}
         />
       )}
