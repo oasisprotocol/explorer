0fa4b9822fdb29209984aec322b6cad82305f172
lukaw3d <lukajeran@oasislabs.com>
2023-01-17 04:24:48 +0100

    Migrate Transactions Per Second chart

--- src/app/pages/DashboardPage/TransactionsChartCard.tsx
+++ src/app/pages/DashboardPage/TransactionsChartCard.tsx
@@ -2,8 +2,8 @@ import { useTranslation } from 'react-i18next'
 import Card from '@mui/material/Card'
 import CardHeader from '@mui/material/CardHeader'
 import CardContent from '@mui/material/CardContent'
-import { TpsCheckpoint, useGetConsensusStatsTps } from '../../../oasis-indexer/generated/api'
-import { ChartDuration, ChartUtils } from '../../utils/chart-utils'
+import { useGetConsensusStatsTxVolume } from '../../../oasis-indexer/api'
+import { ChartDuration, durationToQueryParams } from '../../utils/chart-utils'
 import { CardActions } from '@mui/material'
 import { LineChart } from '../../components/charts/LineChart'
 import Box from '@mui/material/Box'
@@ -22,27 +22,26 @@ const TransactionsChartCardCmp: FC<TransactionsChartCardProps> = ({ chartDuratio
   const { t } = useTranslation()
   const theme = useTheme()
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
-
+  const statsParams = durationToQueryParams[chartDuration]
   // TODO: Replace with Emerald stats
-  const { data } = useGetConsensusStatsTps({
-    // TODO: Can interval_minutes be configured? 5 interval on 1 month is too granular
-    limit: ChartUtils.getLimitByDuration(chartDuration),
+  const { data } = useGetConsensusStatsTxVolume(statsParams)
+
+  const lineChartData = data?.data.buckets.map(bucket => {
+    return {
+      bucket_start: bucket.start,
+      volume_per_second: bucket.volume / statsParams.bucket_size_seconds
+    }
   })
 
-  const lineChartData = data?.data.tps_checkpoints ?? null
-
-  const totalTransactions =
-    data?.data.tps_checkpoints?.reduce((acc, curr) => {
-      return acc + (curr?.tx_volume ?? 0)
-    }, 0) ?? 0
+  const totalTransactions = data?.data.buckets.reduce((acc, curr) => acc + curr.volume, 0) ?? 0
 
   return (
     <Card sx={{ p: 0 }}>
       <CardHeader component="h5" title={t('transactionsTpsChart.header')} sx={{ pb: 0, pl: 4, pt: 4 }} />
       <CardContent sx={{ pt: 4 }}>
-        {lineChartData !== null && (
-          <LineChart<TpsCheckpoint>
-            dataKey="tx_volume"
+        {lineChartData && (
+          <LineChart
+            dataKey="volume_per_second"
             data={lineChartData}
             margin={{ left: 0, right: isMobile ? 80 : 40 }}
             strokeWidth={1.09}
