8348fbde342ba7f88640f16ba2d5ce9c444bdaad
lukaw3d <lukajeran@oasislabs.com>
2023-01-19 00:40:44 +0100

    Throw on invalid page number

--- src/app/components/Table/useSearchParamsPagination.ts
+++ src/app/components/Table/useSearchParamsPagination.ts
@@ -3,6 +3,7 @@ import { To, useSearchParams } from 'react-router-dom'
 export function useSearchParamsPagination(paramName: string) {
   const [searchParams] = useSearchParams()
   const selectedPage = parseInt(searchParams.get(paramName) ?? '1', 10)
+  if (isNaN(selectedPage)) throw new Error('400 Bad Request')
 
   function linkToPage(page: number): To {
     const newSearchParams = new URLSearchParams(searchParams)
