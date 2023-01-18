849f13041e0df472506ff8d559bafe043054063f
lukaw3d <lukajeran@oasislabs.com>
2023-01-18 06:03:35 +0100

    Install @emotion/jest snapshot serializer

--- internals/jest/setupTests.ts
+++ internals/jest/setupTests.ts
@@ -2,6 +2,7 @@
 // react-testing-library renders your components to document.body,
 // this adds jest-dom's custom assertions
 import '@testing-library/jest-dom/extend-expect'
+import * as emotionJestUtils from '@emotion/jest'
 
 import 'react-app-polyfill/stable'
 
@@ -14,3 +15,14 @@ global.TextEncoder = require('util').TextEncoder
 global.TextDecoder = require('util').TextDecoder
 window.TextDecoder = global.TextDecoder
 window.TextEncoder = global.TextEncoder
+
+expect.addSnapshotSerializer(
+  emotionJestUtils.createSerializer({
+    // By default classes were replaced with emotion-${index}.
+    // This replaces emotion/styled hash with emotion-${index}, but also keeps
+    // the meaningful part of class name
+    classNameReplacer(className, index) {
+      return className.replace(/^css-.+?-/, `emotion-${index}-`)
+    }
+  })
+)
