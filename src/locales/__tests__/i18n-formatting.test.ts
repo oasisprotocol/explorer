6c837c68b7209d8b682570e8ce2d308ed1a13ad1
lukaw3d <lukajeran@oasislabs.com>
2023-01-19 18:45:05 +0100

    Test formatting dates with i18n

---
+++ src/locales/__tests__/i18n-formatting.test.ts
@@ -0,0 +1,25 @@
+import { useTranslation } from 'react-i18next'
+import { renderHook } from '@testing-library/react'
+
+test('formatting dates with i18n', () => {
+  const { result } = renderHook(() => {
+    const { t } = useTranslation()
+    return t('does_not_exist.use_default', '{{timestamp, dateTime}}', {
+      timestamp: new Date('2022-01-02T03:04:05.006Z'),
+      formatParams: {
+        timestamp: {
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+          hour: 'numeric',
+          minute: 'numeric',
+          timeZoneName: 'short',
+          timeZone: 'UTC',
+          locale: 'en-US',
+        },
+      },
+    })
+  })
+
+  expect(result.current).toBe('January 2, 2022, 3:04 AM UTC')
+})
