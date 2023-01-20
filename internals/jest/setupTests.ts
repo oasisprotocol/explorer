5cd7b7b805bdd71ebc9b046557ee0d94ff326587
lukaw3d <lukajeran@oasislabs.com>
2023-01-20 02:16:31 +0100

    Reorder test setup imports so env variables are available in i18n

--- internals/jest/setupTests.ts
+++ internals/jest/setupTests.ts
@@ -1,15 +1,14 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+import 'dotenv/config'
+import 'react-app-polyfill/stable'
+
 // react-testing-library renders your components to document.body,
 // this adds jest-dom's custom assertions
 import '@testing-library/jest-dom/extend-expect'
 import * as emotionJestUtils from '@emotion/jest'
 
-import 'react-app-polyfill/stable'
-
 // Init i18n for the tests needing it
 import '../../src/locales/i18n'
-
-require('dotenv').config()
 
 global.TextEncoder = require('util').TextEncoder
 global.TextDecoder = require('util').TextDecoder
