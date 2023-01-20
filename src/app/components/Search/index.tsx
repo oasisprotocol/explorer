926403c4e171578cddb9bc9e6c4e89c2cc2a2d77
lubej <9722540+lubej@users.noreply.github.com>
2023-01-20 13:58:45 +0100

    Mobile search bar

--- src/app/components/Search/index.tsx
+++ src/app/components/Search/index.tsx
@@ -2,34 +2,135 @@ import { FC, FormEvent, memo, useRef, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import TextField from '@mui/material/TextField'
 import { InputAdornment } from '@mui/material'
-import { styled } from '@mui/material/styles'
+import { styled, useTheme } from '@mui/material/styles'
 import Button from '@mui/material/Button'
 import SearchIcon from '@mui/icons-material/Search'
 import { useTranslation } from 'react-i18next'
 import { COLORS } from '../../../styles/theme/colors'
-import { SearchUtils } from '../../components/Search/search-utils'
+import { SearchUtils } from './search-utils'
+import { StandardTextFieldProps } from '@mui/material/TextField/TextField'
+import useMediaQuery from '@mui/material/useMediaQuery'
 
-const SearchForm = styled('form')(() => ({
-  width: '100%',
+export type SearchVariant = 'button' | 'icon' | 'expandable'
+
+interface StyledBaseProps {
+  searchVariant: SearchVariant
+  expanded: boolean
+}
+
+interface SearchFormProps extends StyledBaseProps {}
+
+const SearchForm = styled('form', {
+  shouldForwardProp: (prop: PropertyKey) =>
+    !(['expanded', 'searchVariant'] as (keyof SearchFormProps)[]).includes(prop as keyof SearchFormProps),
+})<SearchFormProps>(({ theme, searchVariant, expanded }) => ({
+  ...(searchVariant === 'expandable'
+    ? {
+        position: 'absolute',
+        right: theme.spacing(4),
+
+        ...(expanded
+          ? {
+              left: theme.spacing(4),
+              width: `calc(100% - ${theme.spacing(4)} - ${theme.spacing(4)})`,
+            }
+          : {}),
+      }
+    : {
+        position: 'relative',
+        width: '100%',
+      }),
 }))
 
-const SearchButton = styled(Button)(({ theme }) => ({
-  paddingLeft: theme.spacing(5),
-  paddingRight: theme.spacing(5),
+interface SearchTextFieldProps extends StandardTextFieldProps {
+  searchVariant: SearchVariant
+  expanded: boolean
+  hasStartAdornment: boolean
+}
+
+const SearchTextField = styled(TextField, {
+  shouldForwardProp: (prop: PropertyKey) =>
+    !(['searchVariant', 'expanded', 'hasStartAdornment'] as (keyof SearchTextFieldProps)[]).includes(
+      prop as keyof SearchTextFieldProps,
+    ),
+})<SearchTextFieldProps>(({ theme, searchVariant, expanded, hasStartAdornment }) => ({
+  ...(hasStartAdornment
+    ? {}
+    : {
+        paddingLeft: theme.spacing(4),
+      }),
+  ...(searchVariant === 'expandable'
+    ? {
+        ...(expanded
+          ? {}
+          : {
+              paddingLeft: 0,
+              input: {
+                display: 'none',
+              },
+            }),
+      }
+    : {}),
+}))
+
+interface InputEndAdornmentProps extends StyledBaseProps {}
+
+const InputEndAdornment = styled(InputAdornment, {
+  shouldForwardProp: (prop: PropertyKey) =>
+    !(['expanded', 'searchVariant'] as (keyof InputEndAdornmentProps)[]).includes(
+      prop as keyof InputEndAdornmentProps,
+    ),
+})<InputEndAdornmentProps>(({ searchVariant, expanded }) => ({
+  ...(searchVariant === 'expandable'
+    ? {
+        ...(expanded
+          ? {}
+          : {
+              marginLeft: 0,
+            }),
+      }
+    : {}),
+}))
+
+interface SearchButtonProps extends StyledBaseProps {}
+
+const SearchButton = styled(Button, {
+  shouldForwardProp: (prop: PropertyKey) =>
+    !(['expanded', 'searchVariant'] as (keyof SearchButtonProps)[]).includes(prop as keyof SearchButtonProps),
+})<SearchButtonProps>(({ theme, searchVariant }) => ({
+  minWidth: 'unset',
+  padding: '12px',
+  [theme.breakpoints.up('sm')]: {
+    paddingLeft: theme.spacing(5),
+    paddingRight: theme.spacing(5),
+  },
+  ...(searchVariant === 'expandable'
+    ? {
+        backgroundColor: COLORS.brandDark,
+      }
+    : {}),
 }))
 
 export interface SearchProps {
+  variant: SearchVariant
   disabled?: boolean
   onFocusChange?: (hasFocus: boolean) => void
 }
 
-const SearchCmp: FC<SearchProps> = ({ disabled, onFocusChange }) => {
+const blurTimeout = 16
+
+const SearchCmp: FC<SearchProps> = ({ variant, disabled, onFocusChange }) => {
   const { t } = useTranslation()
   const navigate = useNavigate()
-  const searchPlaceholderTranslated = t('search.placeholder')
+  const theme = useTheme()
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
+  const searchPlaceholderTranslated = isMobile ? t('search.mobilePlaceholder') : t('search.placeholder')
   const searchTermRequiredTranslated = t('search.searchTermRequired')
   const inputRef = useRef<HTMLInputElement>(null)
+  const blurTimeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null)
+  const [expanded, setExpanded] = useState(variant !== 'expandable')
   const [hasError, setHasError] = useState(false)
+
   const onSearchSubmit = (searchTerm: string) => {
     try {
       const navigateTo = SearchUtils.getNavigationPath(searchTerm)
@@ -39,16 +140,42 @@ const SearchCmp: FC<SearchProps> = ({ disabled, onFocusChange }) => {
     }
   }
 
+  const setBlurTimeout = (fn: () => void) => {
+    if (variant !== 'expandable') {
+      return
+    }
+
+    if (blurTimeoutRef.current) {
+      clearTimeout(blurTimeoutRef.current)
+    }
+
+    blurTimeoutRef.current = setTimeout(fn, blurTimeout)
+  }
+
   const onFocus = () => {
+    setBlurTimeout(() => {
+      setExpanded(true)
+    })
+
     onFocusChange?.(true)
   }
 
+  const onSearchButtonBlur = () => {
+    setBlurTimeout(() => {
+      setExpanded(false)
+    })
+  }
+
   const onBlur = () => {
+    setBlurTimeout(() => {
+      setExpanded(false)
+    })
+
     onFocusChange?.(false)
   }
 
-  const onFormSubmit = (e: FormEvent) => {
-    e.preventDefault()
+  const onFormSubmit = (e?: FormEvent) => {
+    e?.preventDefault()
 
     setHasError(false)
 
@@ -62,9 +189,35 @@ const SearchCmp: FC<SearchProps> = ({ disabled, onFocusChange }) => {
     onSearchSubmit(searchTerm)
   }
 
+  const onSearchButtonClick = () => {
+    if (expanded) {
+      onFormSubmit()
+    } else {
+      setExpanded(true)
+    }
+  }
+
+  const startAdornment = variant === 'button' && (
+    <InputAdornment position="start">
+      <SearchIcon sx={{ color: COLORS.grayMedium }} />
+    </InputAdornment>
+  )
+
+  const searchButtonContent =
+    variant !== 'button' ? <SearchIcon sx={{ color: COLORS.grayMedium }} /> : t('search.searchBtnText')
+
   return (
-    <SearchForm onSubmit={onFormSubmit} role="search" aria-label={searchPlaceholderTranslated}>
-      <TextField
+    <SearchForm
+      expanded={expanded}
+      searchVariant={variant}
+      onSubmit={onFormSubmit}
+      role="search"
+      aria-label={searchPlaceholderTranslated}
+    >
+      <SearchTextField
+        expanded={expanded}
+        searchVariant={variant}
+        hasStartAdornment={!!startAdornment}
         InputProps={{
           disabled,
           inputRef,
@@ -73,17 +226,22 @@ const SearchCmp: FC<SearchProps> = ({ disabled, onFocusChange }) => {
           inputProps: {
             sx: { p: 0 },
           },
-          startAdornment: (
-            <InputAdornment position="start">
-              <SearchIcon sx={{ color: COLORS.grayMedium }} />
-            </InputAdornment>
-          ),
+          startAdornment,
           endAdornment: (
-            <InputAdornment position="end">
-              <SearchButton disabled={disabled} color="primary" variant="contained" type="submit">
-                {t('search.searchBtnText')}
+            <InputEndAdornment position="end" expanded={expanded} searchVariant={variant}>
+              <SearchButton
+                disabled={disabled}
+                expanded={expanded}
+                searchVariant={variant}
+                color="primary"
+                variant="contained"
+                type="submit"
+                onClick={onSearchButtonClick}
+                onBlur={onSearchButtonBlur}
+              >
+                {searchButtonContent}
               </SearchButton>
-            </InputAdornment>
+            </InputEndAdornment>
           ),
         }}
         placeholder={searchPlaceholderTranslated}
--- src/app/pages/DashboardPage/ParaTimeSnapshot.tsx
+++ src/app/pages/DashboardPage/ParaTimeSnapshot.tsx
@@ -3,7 +3,7 @@ import Box from '@mui/material/Box'
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
 import useMediaQuery from '@mui/material/useMediaQuery'
-import { useTheme } from '@mui/material/styles'
+import { styled, useTheme } from '@mui/material/styles'
 import { COLORS } from '../../../styles/theme/colors'
 import { DurationSelect } from './DurationSelect'
 import { TransactionsChartCard } from './TransactionsChartCard'
@@ -11,6 +11,32 @@ import { RoseChartCard } from './RoseChartCard'
 import { ChartDuration } from '../../utils/chart-utils'
 import { useTranslation } from 'react-i18next'
 import { useConstant } from '../../hooks/useConstant'
+import { Search } from '../../components/Search'
+
+const HeaderBox = styled(Box)(({ theme }) => ({
+  position: 'relative',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  paddingLeft: theme.spacing(0),
+  paddingBottom: theme.spacing(0),
+  '> h3': {
+    flex: '0 1 calc(100% - 66px)',
+    paddingLeft: theme.spacing(4),
+  },
+  '> div': {
+    flex: '0 1 66px',
+    height: '47px',
+    paddingRight: theme.spacing(4),
+  },
+  [theme.breakpoints.up('sm')]: {
+    paddingLeft: theme.spacing(4),
+    paddingBottom: theme.spacing(3),
+    '> *': {
+      flex: 'unset',
+    },
+  },
+}))
 
 export const ParaTimeSnapshot: FC = () => {
   const { t } = useTranslation()
@@ -29,12 +55,18 @@ export const ParaTimeSnapshot: FC = () => {
   return (
     <>
       <Grid container sx={{ display: 'flex', alignItems: 'center', gap: 3, mb: 4 }}>
-        <Grid item>
-          <Box sx={{ pl: isMobile ? 4 : 0, pb: isMobile ? 3 : 0 }}>
+        <Grid item xs={12}>
+          <HeaderBox>
             <Typography variant="h3" sx={{ color: COLORS.white }}>
               {t('paraTimeSnapshot.header')}
             </Typography>
-          </Box>
+
+            {isMobile && (
+              <Box>
+                <Search variant="expandable" />
+              </Box>
+            )}
+          </HeaderBox>
         </Grid>
         <Grid item xs={12} md={8}>
           <Box sx={{ pl: isMobile ? 4 : 0, pb: isMobile ? 3 : 0 }}>
