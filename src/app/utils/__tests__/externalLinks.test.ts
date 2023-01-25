db2ae14f78d090e349276db2686a25828c65978e
lukaw3d <lukajeran@oasislabs.com>
2023-01-25 07:57:26 +0100

    Remove retrying from externalLinks and use GET requests

--- src/app/utils/__tests__/externalLinks.test.ts
+++ src/app/utils/__tests__/externalLinks.test.ts
@@ -2,7 +2,6 @@ import nodeFetch from 'node-fetch'
 import * as externalLinksModule from '../externalLinks'
 
 jest.setTimeout(30_000)
-jest.retryTimes(3) // Reddit is unreliable
 
 // Requesting external links is sometimes slow; and links will rarely break.
 // So only run this on CI, so local tests remain quick.
@@ -22,8 +21,8 @@ onlyRunOnCI('externalLinks', () => {
   describe('should be reachable', () => {
     for (const [linksGroupName, linksGroup] of Object.entries(externalLinksModule)) {
       for (const [linkName, url] of Object.entries(linksGroup)) {
-        it(`${linksGroupName} ${linkName} ${url}`, async () => {
-          const response = await nodeFetch(url, { method: 'HEAD' })
+        it.concurrent(`${linksGroupName} ${linkName} ${url}`, async () => {
+          const response = await nodeFetch(url, { method: 'GET' })
           expect(response.status).toBe(200)
         })
       }
