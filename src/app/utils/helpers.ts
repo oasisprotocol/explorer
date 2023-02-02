a8b632d50e0c24d362750b51fc469fc2ecdc9558
Michal Zielenkiewicz <michal@oasisprotocol.org>
2023-02-02 18:17:56 +0100

    Create helper utilities

---
+++ src/app/utils/helpers.ts
@@ -0,0 +1,35 @@
+import * as oasis from '@oasisprotocol/client'
+import * as oasisRT from '@oasisprotocol/client-rt'
+
+export const isValidOasisAddress = (addr: string) => {
+  try {
+    oasis.staking.addressFromBech32(addr)
+    return true
+  } catch (e) {
+    return false
+  }
+}
+
+export const isValidEthAddress = (hexAddress: string): boolean => {
+  return /^0x[0-9a-fA-F]{40}$/.test(hexAddress)
+}
+
+export async function getEvmBech32Address(evmAddress: string) {
+  const ethAddrU8 = oasis.misc.fromHex(evmAddress.replace('0x', ''))
+  const addr = await oasis.address.fromData(
+    oasisRT.address.V0_SECP256K1ETH_CONTEXT_IDENTIFIER,
+    oasisRT.address.V0_SECP256K1ETH_CONTEXT_VERSION,
+    ethAddrU8,
+  )
+  return oasis.staking.addressToBech32(addr)
+}
+
+export const getOasisAddress = async (address: string): Promise<string> => {
+  if (isValidOasisAddress(address)) {
+    return address
+  } else if (isValidEthAddress(address)) {
+    return await getEvmBech32Address(address)
+  } else {
+    throw new Error('Invalid address')
+  }
+}
