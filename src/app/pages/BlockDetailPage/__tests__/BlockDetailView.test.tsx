e4303dc438d23c897c06bb4a32f35f9d502222f0
Michal Zielenkiewicz <michal@oasisprotocol.org>
2023-02-02 15:54:08 +0100

    Move test to proper directory

--- src/app/pages/BlockDetailPage/__tests__/BlockDetailView.test.tsx
+++ src/app/pages/BlockDetailPage/__tests__/BlockDetailView.test.tsx
@@ -1,5 +1,5 @@
 import { render } from '@testing-library/react'
-import { BlockDetailView } from './'
+import { BlockDetailView } from '../'
 
 describe('BlockDetailView', () => {
   beforeEach(() => {
