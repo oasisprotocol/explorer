aabbcd53532c83c70c360cc49fef778b01955106
lubej <9722540+lubej@users.noreply.github.com>
2023-02-14 10:40:07 +0100

    Simplify simple switch statements

--- src/app/pages/HomePage/ParaTimeSelector/para-time-selector-utils.ts
+++ src/app/pages/HomePage/ParaTimeSelector/para-time-selector-utils.ts
@@ -3,21 +3,11 @@ import { GraphEndpoint } from './Graph/types'
 
 export abstract class ParaTimeSelectorUtils {
   static getIsGraphTransparent(step: ParaTimeSelectorStep) {
-    switch (step) {
-      case ParaTimeSelectorStep.Explore:
-        return false
-      default:
-        return true
-    }
+    return step !== ParaTimeSelectorStep.Explore
   }
 
   static showExploreBtn(step: ParaTimeSelectorStep) {
-    switch (step) {
-      case ParaTimeSelectorStep.EnableExplore:
-        return true
-      default:
-        return false
-    }
+    return step === ParaTimeSelectorStep.EnableExplore
   }
 
   static showMobileHelpScreen(step: ParaTimeSelectorStep, isMobile: boolean) {
@@ -25,12 +15,7 @@ export abstract class ParaTimeSelectorUtils {
       return false
     }
 
-    switch (step) {
-      case ParaTimeSelectorStep.ShowHelpScreen:
-        return true
-      default:
-        return false
-    }
+    return step === ParaTimeSelectorStep.ShowHelpScreen
   }
 
   static showZoomOutBtn(isMobile: boolean, endpoint?: GraphEndpoint) {
