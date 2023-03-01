1538dc8eab9a99e8bfc7b9320e454e86a8198c98
lukaw3d <lukajeran@oasislabs.com>
2023-03-01 19:02:59 +0100

    Test BlockDetailView using search-suggested block

--- src/app/pages/BlockDetailPage/__tests__/BlockDetailView.test.tsx
+++ src/app/pages/BlockDetailPage/__tests__/BlockDetailView.test.tsx
@@ -1,6 +1,7 @@
 import { screen } from '@testing-library/react'
 import { renderWithProviders } from '../../../utils/renderWithProviders'
 import { BlockDetailView } from '../'
+import { suggestedParsedBlock } from '../../../utils/test-fixtures'
 
 describe('BlockDetailView', () => {
   beforeEach(() => {
@@ -13,21 +14,9 @@ describe('BlockDetailView', () => {
   })
 
   it('should display formatted values', () => {
-    renderWithProviders(
-      <BlockDetailView
-        isLoading={false}
-        block={{
-          round: 1158800,
-          hash: '194cdf5b6a75be37f0ee6ff52bb2680ec95a7c28a89c5214ba25809600c72f92',
-          timestamp: '2022-04-23T12:20:56Z',
-          num_transactions: 10,
-          size: 6434,
-          gas_used: 673539,
-        }}
-      />,
-    )
-    expect(screen.getByText('1,158,800')).toBeInTheDocument()
-    expect(screen.getByText('April 23, 2022 at 12:20 PM UTC (9 months ago)')).toBeInTheDocument()
-    expect(screen.getByText('6,434 bytes')).toBeInTheDocument()
+    renderWithProviders(<BlockDetailView isLoading={false} block={suggestedParsedBlock} />)
+    expect(screen.getByText('1,396,255')).toBeInTheDocument()
+    expect(screen.getByText('May 13, 2022 at 6:39 AM UTC (9 months ago)')).toBeInTheDocument()
+    expect(screen.getByText('4,214 bytes')).toBeInTheDocument()
   })
 })
