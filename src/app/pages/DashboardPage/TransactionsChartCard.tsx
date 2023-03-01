19eaa229aecda9eac615ed110a831d535e018f5c
lubej <9722540+lubej@users.noreply.github.com>
2023-03-01 09:59:29 +0100

    Transactions chart use staleTime

--- src/app/pages/DashboardPage/TransactionsChartCard.tsx
+++ src/app/pages/DashboardPage/TransactionsChartCard.tsx
@@ -1,11 +1,11 @@
 import { useTranslation } from 'react-i18next'
 import { Layer, useGetLayerStatsTxVolume } from '../../../oasis-indexer/api'
-import { ChartDuration, durationToQueryParams } from '../../utils/chart-utils'
+import { ChartDuration, chartUseQueryStaleTimeMs, durationToQueryParams } from '../../utils/chart-utils'
 import { LineChart } from '../../components/charts/LineChart'
 import { useTheme } from '@mui/material/styles'
 import useMediaQuery from '@mui/material/useMediaQuery'
 import { intlDateFormat } from '../../utils/dateFormatter'
-import { FC } from 'react'
+import { FC, memo } from 'react'
 import { SnapshotCard } from './SnapshotCard'
 import { PercentageGain } from '../../components/PercentageGain'
 
@@ -13,20 +13,21 @@ interface TransactionsChartCardProps {
   chartDuration: ChartDuration
 }
 
-export const TransactionsChartCard: FC<TransactionsChartCardProps> = ({ chartDuration }) => {
+const TransactionsChartCardCmp: FC<TransactionsChartCardProps> = ({ chartDuration }) => {
   const { t } = useTranslation()
   const theme = useTheme()
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
-  const { data } = useGetLayerStatsTxVolume(Layer.emerald, durationToQueryParams[chartDuration])
+  const statsParams = durationToQueryParams[chartDuration]
+  const { data } = useGetLayerStatsTxVolume(Layer.emerald, statsParams, {
+    query: { staleTime: chartUseQueryStaleTimeMs },
+  })
 
-  const lineChartData = data?.data.buckets
-    .map(bucket => {
-      return {
-        bucket_start: bucket.bucket_start,
-        volume_per_second: bucket.tx_volume / durationToQueryParams[chartDuration].bucket_size_seconds,
-      }
-    })
-    .reverse()
+  const lineChartData = data?.data.buckets.map(bucket => {
+    return {
+      bucket_start: bucket.bucket_start,
+      volume_per_second: bucket.tx_volume / statsParams.bucket_size_seconds,
+    }
+  })
 
   const totalTransactions = data?.data.buckets.reduce((acc, curr) => acc + curr.tx_volume, 0) ?? 0
 
@@ -40,7 +41,7 @@ export const TransactionsChartCard: FC<TransactionsChartCardProps> = ({ chartDur
       {lineChartData && (
         <LineChart
           dataKey="volume_per_second"
-          data={lineChartData}
+          data={lineChartData.slice().reverse()}
           margin={{ left: 0, right: isMobile ? 80 : 40 }}
           formatters={{
             data: (value: number) =>
@@ -59,3 +60,5 @@ export const TransactionsChartCard: FC<TransactionsChartCardProps> = ({ chartDur
     </SnapshotCard>
   )
 }
+
+export const TransactionsChartCard = memo(TransactionsChartCardCmp)
