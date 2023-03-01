27fa6f77f679ae8fad7ef834a96ce7382953529f
Michal Zielenkiewicz <michal@oasisprotocol.org>
2023-03-01 09:29:34 +0100

    Prepare Active Accounts chart

--- src/app/pages/DashboardPage/ActiveAccounts.tsx
+++ src/app/pages/DashboardPage/ActiveAccounts.tsx
@@ -1,9 +1,26 @@
 import { FC } from 'react'
 import { useTranslation } from 'react-i18next'
 import { SnapshotCard } from './SnapshotCard'
+import { BarChart } from '../../components/charts/BarChart'
 
 export const ActiveAccounts: FC = () => {
   const { t } = useTranslation()
+  // TODO: sync with BE active accounts endpoint when it's ready
 
-  return <SnapshotCard title={t('activeAccounts.title')}>{/* TODO: Add chart */}</SnapshotCard>
+  return (
+    <SnapshotCard title={t('activeAccounts.title')} label="1,992,677">
+      <BarChart
+        data={[]}
+        dataKey="active_accounts"
+        formatters={{
+          data: (value: number) => value.toLocaleString(),
+          label: (value: string) =>
+            t('common.formattedDateTime', {
+              timestamp: new Date(value),
+            }),
+        }}
+        withBarBackground
+      />
+    </SnapshotCard>
+  )
 }
