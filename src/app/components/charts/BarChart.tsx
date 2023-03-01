d7622223b6a42e7f85cd9be461b7b25bef7b6c5d
Michal Zielenkiewicz <michal@oasisprotocol.org>
2023-03-01 12:10:35 +0100

    Make BarChart more customisable

--- src/app/components/charts/BarChart.tsx
+++ src/app/components/charts/BarChart.tsx
@@ -11,29 +11,58 @@ import { TooltipContent, type Formatters } from './Tooltip'
 import { COLORS } from '../../../styles/theme/colors'
 
 interface BarChartProps<T extends object> extends Formatters {
+  cartesianGrid?: boolean
   data: T[]
   dataKey: Extract<keyof T, string>
+  rounded?: boolean
+  withBarBackground?: boolean
+  withLabels?: boolean
 }
 
-const BarChartCmp = <T extends object>({ data, dataKey, formatters }: BarChartProps<T>) => (
+const barSize = 12
+
+const BarChartCmp = <T extends object>({
+  cartesianGrid,
+  data,
+  dataKey,
+  formatters,
+  rounded,
+  withLabels,
+  withBarBackground,
+}: BarChartProps<T>) => (
   <ResponsiveContainer width="100%" aspect={4}>
-    <RechartsBarChart data={data} margin={{ right: 0, bottom: 0 }}>
-      <CartesianGrid vertical={false} stroke={COLORS.antiFlashWhite3} />
-      <YAxis
-        tick={{ fill: COLORS.brandDark, strokeWidth: 0 }}
-        axisLine={false}
-        tickLine={false}
-        type="number"
-        padding={{ bottom: 20 }}
-        tickMargin={0}
-      />
+    <RechartsBarChart data={data} margin={{ right: 8, bottom: 0, left: 8 }}>
+      {cartesianGrid && <CartesianGrid vertical={false} stroke={COLORS.antiFlashWhite3} />}
+      {withLabels && (
+        <YAxis
+          tick={{ fill: COLORS.brandDark, strokeWidth: 0 }}
+          axisLine={false}
+          tickLine={false}
+          type="number"
+          padding={{ bottom: 20 }}
+          tickMargin={0}
+        />
+      )}
       <Tooltip
         cursor={false}
         wrapperStyle={{ outline: 'none' }}
         content={<TooltipContent formatters={formatters} />}
         offset={15}
       />
-      <Bar dataKey={dataKey} barSize={12} fill={COLORS.denimBlue} radius={[10, 10, 0, 0]} />
+      <Bar
+        background={
+          withBarBackground
+            ? {
+                fill: COLORS.grayLight,
+                radius: barSize,
+              }
+            : undefined
+        }
+        dataKey={dataKey}
+        barSize={barSize}
+        fill={COLORS.denimBlue}
+        radius={rounded ? barSize : [barSize, barSize, 0, 0]}
+      />
     </RechartsBarChart>
   </ResponsiveContainer>
 )
