4b75ecb0cb7f4aecc7f73f4d36fb328fb6c49028
lukaw3d <lukajeran@oasislabs.com>
2023-03-01 19:12:32 +0100

    Failing test of deep links on search results

---
+++ src/app/pages/SearchResultsPage/__tests__/SearchResultsView.test.tsx
@@ -0,0 +1,59 @@
+import { screen } from '@testing-library/react'
+import { renderWithProviders } from '../../../utils/renderWithProviders'
+import { SearchResultsView } from '../'
+import { suggestedParsedAccount, suggestedParsedBlock } from '../../../utils/test-fixtures'
+
+describe('SearchResultsView', () => {
+  beforeEach(() => {
+    jest.useFakeTimers()
+    jest.setSystemTime(new Date(2023, 1, 1))
+  })
+
+  afterEach(() => {
+    jest.useRealTimers()
+  })
+
+  it.todo('unskip tests') // TODO: fix deep links
+  it.skip('block should correctly link to transactions', () => {
+    renderWithProviders(
+      <SearchResultsView
+        searchQueries={{
+          emeraldBlockHeight: {
+            isLoading: false,
+            results: [suggestedParsedBlock],
+          },
+          emeraldTxHash: { isLoading: false, results: [] },
+          consensusAccount: { isLoading: false, results: [] },
+          evmBech32Account: { isLoading: false, results: [] },
+        }}
+        roseFiatValue={1}
+      />,
+    )
+    expect(screen.getByText('1,396,255')).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: '10 transactions' })).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: '10 transactions' })).toHaveAttribute(
+      'href',
+      '/emerald/blocks/1396255#transactions',
+    )
+  })
+
+  it.skip('account should correctly link to erc-20 tokens', () => {
+    renderWithProviders(
+      <SearchResultsView
+        searchQueries={{
+          emeraldBlockHeight: { isLoading: false, results: [] },
+          emeraldTxHash: { isLoading: false, results: [] },
+          consensusAccount: { isLoading: false, results: [] },
+          evmBech32Account: { isLoading: false, results: [suggestedParsedAccount] },
+        }}
+        roseFiatValue={1}
+      />,
+    )
+    expect(screen.getByText('EVM tokens')).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: '337325.43837 FTP' })).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: '337325.43837 FTP' })).toHaveAttribute(
+      'href',
+      '/emerald/account/0xBA504818FdD8D3dBA2Ef8fD9B4F4D5c71aD1d1D3/tokens/erc-20#oasis1qpssvkplnlpzdvwxpgmrhf9j5lkyvaylcvujhjhg',
+    )
+  })
+})
--- src/app/utils/test-fixtures.ts
+++ src/app/utils/test-fixtures.ts
@@ -1,4 +1,4 @@
-import { RuntimeBlock } from '../../oasis-indexer/api'
+import { RuntimeAccount, RuntimeBlock } from '../../oasis-indexer/api'
 
 export const suggestedParsedBlock: RuntimeBlock = {
   round: 1396255,
@@ -8,4 +8,37 @@ export const suggestedParsedBlock: RuntimeBlock = {
   num_transactions: 10,
   gas_used: 1482530,
   layer: 'emerald',
+}
+
+export const suggestedParsedAccount: RuntimeAccount = {
+  address: 'oasis1qrvha284gfztn7wwq6z50c86ceu28jp7csqhpx9t',
+  address_preimage: {
+    address_data: 'ulBIGP3Y09ui74/ZtPTVxxrR0dM=',
+    context: 'oasis-runtime-sdk/address: secp256k1eth',
+    context_version: 0,
+  },
+  balances: [],
+  evm_balances: [
+    {
+      balance: '337325.438367711264207872',
+      token_contract_addr: 'oasis1qpssvkplnlpzdvwxpgmrhf9j5lkyvaylcvujhjhg',
+      token_decimals: 18,
+      token_name: 'FTP',
+      token_symbol: 'FTP',
+      token_type: 'ERC20',
+    },
+    {
+      balance: '-3372955.09999999999999995',
+      token_contract_addr: 'oasis1qqz8706pmf38wmptl6dkcaec8yykw0rvfv7ql6fc',
+      token_decimals: 18,
+      token_name: 'YUZU',
+      token_symbol: 'YUZU',
+      token_type: 'ERC20',
+    },
+  ],
+  stats: {
+    num_txns: 55313,
+    total_received: '0',
+    total_sent: '0',
+  },
 }
