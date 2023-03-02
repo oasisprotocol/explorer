b5b404f1d966b60fcc53fdbf498f889b94dfbfc8
lubej <9722540+lubej@users.noreply.github.com>
2023-03-02 15:41:56 +0100

    Refactor GraphTooltipMobile component

--- src/app/pages/HomePage/GraphTooltip/GraphTooltipMobile.tsx
+++ src/app/pages/HomePage/GraphTooltip/GraphTooltipMobile.tsx
@@ -5,6 +5,7 @@ import { COLORS } from '../../../../styles/theme/colors'
 import { useTranslation } from 'react-i18next'
 import { styled } from '@mui/material/styles'
 import Box from '@mui/material/Box'
+import IconButton from '@mui/material/IconButton'
 
 export const MobileBackdrop = styled(Box)(() => ({
   position: 'fixed',
@@ -14,16 +15,16 @@ export const MobileBackdrop = styled(Box)(() => ({
   zIndex: 4,
 }))
 
-export const MobileGraphTooltip = styled(Box)(() => ({
+export const MobileGraphTooltip = styled(Box)(({ theme }) => ({
   position: 'fixed',
   bottom: 0,
   left: 0,
   right: 0,
   height: 120,
   zIndex: 4,
-  '> svg': {
+  '> button': {
     position: 'fixed',
-    right: 10,
+    right: theme.spacing(2),
     bottom: 125,
   },
 }))
@@ -37,15 +38,12 @@ export const GraphTooltipMobile: FC<PropsWithChildren<GraphTooltipMobileProps>> 
 
   return (
     <>
-      <MobileBackdrop />
+      <MobileBackdrop onClick={onClose} />
       <Fade in>
         <MobileGraphTooltip>
-          <CloseIcon
-            onClick={onClose}
-            fontSize="medium"
-            sx={{ color: COLORS.white }}
-            aria-label={t('home.tooltip.close')}
-          />
+          <IconButton color="inherit" onClick={onClose}>
+            <CloseIcon fontSize="medium" sx={{ color: COLORS.white }} aria-label={t('home.tooltip.close')} />
+          </IconButton>
           {children}
         </MobileGraphTooltip>
       </Fade>
