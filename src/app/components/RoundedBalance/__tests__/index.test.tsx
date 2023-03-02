27d1bc129056237c8dfa250264bdee5971c53790
Michal Zielenkiewicz <michal@oasisprotocol.org>
2023-03-02 11:12:22 +0100

    Fix label when showing rounded balance

---
+++ src/app/components/RoundedBalance/__tests__/index.test.tsx
@@ -0,0 +1,30 @@
+import { render, screen } from '@testing-library/react'
+import { RoundedBalance, RoundedRoseBalance } from './../'
+
+describe('RoundedBalance', () => {
+  it('should render value', () => {
+    render(<RoundedBalance value="0.002231" ticker="USDC" />)
+
+    expect(screen.getByText('0.00223 USDC')).toBeInTheDocument()
+  })
+
+  it('should render rounded value', () => {
+    render(<RoundedBalance value="0.00223100001" ticker="USDC" />)
+
+    expect(screen.getByText('0.00223 USDC')).toBeInTheDocument()
+  })
+
+  it('should render variant when rounded value is equal zero', () => {
+    render(<RoundedBalance value="0.00000000000002231" ticker="USDC" />)
+
+    expect(screen.getByText('< 0.00000 USDC')).toBeInTheDocument()
+  })
+})
+
+describe('RoundedRoseBalance', () => {
+  it('should render value with ROSE ticker symbol', () => {
+    render(<RoundedRoseBalance value="0.002231" />)
+
+    expect(screen.getByText('0.00223 ROSE')).toBeInTheDocument()
+  })
+})
