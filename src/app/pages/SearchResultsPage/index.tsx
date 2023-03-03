ef196f90fa9c00d9baa5f8ff06bdb6207c29fdd4
lukaw3d <lukajeran@oasislabs.com>
2023-03-03 22:48:11 +0100

    Split NoResults into a component

--- src/app/pages/SearchResultsPage/index.tsx
+++ src/app/pages/SearchResultsPage/index.tsx
@@ -1,14 +1,8 @@
 import { FC } from 'react'
-import Box from '@mui/material/Box'
 import Divider from '@mui/material/Divider'
 import { PageLayout } from '../../components/PageLayout'
 import { useParamSearch } from '../../components/Search/search-utils'
-import { EmptyState } from '../../components/EmptyState'
-import { Trans, useTranslation } from 'react-i18next'
-import { Link as RouterLink } from 'react-router-dom'
-import Link from '@mui/material/Link'
-import { SearchSuggestionsLinks } from '../../components/Search/SearchSuggestionsLinks'
-import { COLORS } from '../../../styles/theme/colors'
+import { useTranslation } from 'react-i18next'
 import {
   RuntimeAccount,
   RuntimeBlock,
@@ -18,10 +12,10 @@ import {
   useGetRuntimeTransactionsTxHash,
   Runtime,
 } from '../../../oasis-indexer/api'
-import { OptionalBreak } from '../../components/OptionalBreak'
 import { SubPageCard } from '../../components/SubPageCard'
 import { TextSkeleton } from '../../components/Skeleton'
 import { useRedirectIfSingleResult } from './useRedirectIfSingleResult'
+import { NoResults } from './NoResults'
 
 type ConditionalResults<T> = { isLoading: boolean; results: T[] | undefined }
 export type SearchQueries = {
@@ -87,26 +81,7 @@ export const SearchResultsView: FC<{
       )}
 
       {hasNoResults && (
-        <EmptyState
-          title={t('search.noResults.header')}
-          description={
-            <Box sx={{ textAlign: 'center', a: { color: COLORS.white, textDecoration: 'underline' } }}>
-              <p>
-                <Trans
-                  t={t}
-                  i18nKey="search.noResults.description"
-                  components={{
-                    OptionalBreak: <OptionalBreak />,
-                    HomeLink: <Link component={RouterLink} to={'/'} />,
-                  }}
-                ></Trans>
-              </p>
-              <p>
-                <SearchSuggestionsLinks />
-              </p>
-            </Box>
-          }
-        />
+        <NoResults />
       )}
 
       {!isAnyLoading && !hasNoResults && <>TODO: Multiple results</>}
