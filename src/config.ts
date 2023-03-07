00919e59067a20cf4b379ffdf6ede8e6b18782b4
lukaw3d <lukajeran@oasislabs.com>
2023-03-07 18:09:19 +0100

    Document source of blockGasLimit values

--- src/config.ts
+++ src/config.ts
@@ -19,9 +19,12 @@ export enum RuntimeTypes {
   Oasis = 'oasis',
 }
 
+// consensusConfig: max_block_size = 22020096, max_block_gas = unlimited right now
+
 const emeraldConfig: LayerConfig = {
   mainnet: {
     address: 'oasis1qzvlg0grjxwgjj58tx2xvmv26era6t2csqn22pte',
+    // Match max_batch_gas https://github.com/oasisprotocol/emerald-paratime/blob/5a36a646b989e510fadc0029178fe96a24cad101/src/lib.rs#L112-L112
     blockGasLimit: 10_000_000,
     runtimeId: '000000000000000000000000000000000000000000000000e2eaa99fc008f87f',
   },
@@ -62,6 +65,7 @@ const cipherConfig: LayerConfig = {
 const sapphireConfig: LayerConfig = {
   mainnet: {
     address: 'oasis1qrd3mnzhhgst26hsp96uf45yhq6zlax0cuzdgcfc',
+    // See max_batch_gas https://github.com/oasisprotocol/sapphire-paratime/blob/5cbcdf764390bba3ae27411c551a4f4444117f3f/runtime/src/lib.rs#L146
     blockGasLimit: 15_000_000,
     runtimeId: '000000000000000000000000000000000000000000000000f80306c9858e7279',
   },
