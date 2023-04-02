bee04d458d1653f60f2bec357ff34965ba770d91
lubej <9722540+lubej@users.noreply.github.com>
2023-04-02 19:51:45 +0200

    Implement paratime picker

---
+++ src/app/components/LayerDetailsModal/index.tsx
@@ -0,0 +1,182 @@
+import { FC } from 'react'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import { RouteUtils } from '../../utils/route-utils'
+import { COLORS } from '../../../styles/theme/colors'
+import { Circle } from '../Circle'
+import CheckIcon from '@mui/icons-material/Check'
+import { useTranslation } from 'react-i18next'
+import { styled, useTheme } from '@mui/material/styles'
+import useMediaQuery from '@mui/material/useMediaQuery'
+import HighlightOffIcon from '@mui/icons-material/HighlightOff'
+import IconButton from '@mui/material/IconButton'
+import Button from '@mui/material/Button'
+import { useLayerParam } from '../../hooks/useLayerParam'
+import { BlockchainIcon } from '../BlockchainIcon'
+import Link from '@mui/material/Link'
+import { Link as RouterLink } from 'react-router-dom'
+import { Layer } from 'oasis-indexer/api'
+import { TFunction } from 'i18next'
+import { Modal } from '../Modal'
+
+const CloseModalIconButton = styled(IconButton)(({ theme }) => ({
+  position: 'absolute',
+  top: theme.spacing(0),
+  right: theme.spacing(0),
+  [theme.breakpoints.up('sm')]: {
+    top: theme.spacing(5),
+    right: theme.spacing(5),
+  },
+}))
+
+const EnabledLayersGrid = styled(Box)(({ theme }) => ({
+  display: 'grid',
+  gridTemplateColumns: 'fit-content(70%) 1fr',
+  '&:not(:last-child)': {
+    marginBottom: theme.spacing(5),
+  },
+}))
+
+const SelectButton = styled(Button)(() => ({
+  color: COLORS.brandExtraDark,
+  textTransform: 'capitalize',
+  fontWeight: 700,
+  fontSize: '16px',
+  '&&': {
+    backgroundColor: 'transparent',
+  },
+  '&:disabled': {
+    color: COLORS.brandDark,
+    cursor: 'not-allowed',
+    pointerEvents: 'none',
+  },
+}))
+
+interface LayerDetailsModalProps {
+  open: boolean
+  setOpen: (open: boolean) => void
+}
+
+const getLabels = (t: TFunction): { [key in Layer]: { title?: string; description?: string } } => ({
+  emerald: {
+    title: t('common.emerald'),
+    description: t('pageHeader.emerald'),
+  },
+  sapphire: {
+    title: t('common.sapphire'),
+    description: t('pageHeader.sapphire'),
+  },
+  consensus: {},
+  cipher: {},
+})
+
+export const LayerDetailsModal: FC<LayerDetailsModalProps> = ({ open, setOpen }) => {
+  const { t } = useTranslation()
+  const theme = useTheme()
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
+  const enabledLayers = RouteUtils.getEnabledLayers()
+  const activeLayer = useLayerParam()
+  const labels = getLabels(t)
+
+  const onModalClose = () => {
+    setOpen(false)
+  }
+
+  return (
+    <Modal
+      open={open}
+      onClose={onModalClose}
+      aria-labelledby="paratime-details-modal-title"
+      aria-describedby="paratime-details-modal-description"
+      hideBackdrop={false}
+    >
+      <>
+        <CloseModalIconButton aria-label={'close'} onClick={onModalClose}>
+          <HighlightOffIcon fontSize="large" sx={{ color: COLORS.brandExtraDark }} />
+        </CloseModalIconButton>
+
+        <Typography id="paratime-details-modal-title" component="h3" variant="h3" sx={{ mb: 4 }}>
+          {t('layerPickerModal.title')}
+        </Typography>
+        <Typography id="paratime-details-modal-description" variant="body2" sx={{ mb: 5 }}>
+          {t('layerPickerModal.description')}
+        </Typography>
+        {enabledLayers.map(layer => (
+          <EnabledLayersGrid key={layer}>
+            <Box sx={{ display: 'flex' }}>
+              <Circle color={COLORS.white} size={6} sx={{ mr: 4 }}>
+                <BlockchainIcon selected={activeLayer === layer} />
+              </Circle>
+              <Box
+                key={layer}
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                }}
+              >
+                <Box
+                  sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                  }}
+                >
+                  <Typography
+                    variant="h2"
+                    color={COLORS.brandExtraDark}
+                    sx={{
+                      pr: isMobile ? 3 : 4,
+                      fontSize: '24px',
+                      fontWeight: 700,
+                      textTransform: 'capitalize',
+                    }}
+                  >
+                    {labels[layer].title}
+                  </Typography>
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      alignItems: 'center',
+                    }}
+                  >
+                    {!isMobile && (
+                      <Typography sx={{ fontSize: 10, color: COLORS.ceil, mr: 3 }} component="span">
+                        {t('common.paraTimeOnline')}
+                      </Typography>
+                    )}
+                    <Circle color={COLORS.eucalyptus} size={4}>
+                      <CheckIcon sx={{ fontSize: 16, color: COLORS.white }} />
+                    </Circle>
+                  </Box>
+                </Box>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                  }}
+                >
+                  <Typography component="span" variant="caption">
+                    {labels[layer].description}
+                  </Typography>
+                </Box>
+              </Box>
+            </Box>
+            <Box sx={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}>
+              <Link
+                component={RouterLink}
+                to={RouteUtils.getDashboardRoute(layer)}
+                sx={{
+                  textDecoration: 'none',
+                  ...(layer === activeLayer && { pointerEvents: 'none' }),
+                }}
+              >
+                <SelectButton variant="text" disabled={activeLayer === layer} onClick={() => setOpen(false)}>
+                  {activeLayer === layer ? t('common.active') : t('common.select')}
+                </SelectButton>
+              </Link>
+            </Box>
+          </EnabledLayersGrid>
+        ))}
+      </>
+    </Modal>
+  )
+}
--- src/app/components/PageLayout/NetworkHeader.tsx
+++ src/app/components/PageLayout/NetworkHeader.tsx
@@ -1,18 +1,17 @@
-import { FC } from 'react'
-import { Link as RouterLink } from 'react-router-dom'
+import { FC, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { TFunction } from 'i18next'
 import Box from '@mui/material/Box'
-import Link from '@mui/material/Link'
 import Typography from '@mui/material/Typography'
 import CheckIcon from '@mui/icons-material/Check'
 import useMediaQuery from '@mui/material/useMediaQuery'
 import { useTheme } from '@mui/material/styles'
 import { COLORS } from '../../../styles/theme/colors'
 import { Circle } from '../Circle'
-import { RouteUtils } from '../../utils/route-utils'
 import { AppError, AppErrors } from '../../../types/errors'
 import { Layer } from '../../../oasis-indexer/api'
+import Button from '@mui/material/Button'
+import { LayerDetailsModal } from '../LayerDetailsModal'
 import { LayerIcon } from '../../components/CustomIcons/LayerIcon'
 
 const getContent = (t: TFunction, layer: Layer) => {
@@ -43,59 +42,107 @@ const getContent = (t: TFunction, layer: Layer) => {
 export const NetworkHeader: FC<{ layer: Layer }> = ({ layer }) => {
   const { t } = useTranslation()
   const theme = useTheme()
-  const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
+  const isTablet = useMediaQuery(theme.breakpoints.down('md'))
   const content = getContent(t, layer)
+  const [open, setOpen] = useState(false)
+
+  const onLayerModalOpen = () => {
+    setOpen(true)
+  }
 
   return (
-    <Box sx={{ display: 'flex', justifyContent: isMobile ? 'flex-end' : 'flex-start', pr: isMobile ? 0 : 4 }}>
-      {!isMobile && (
-        <Circle color={COLORS.white} size={6} sx={{ mr: 4 }}>
-          <LayerIcon sx={{ color: COLORS.brandExtraDark, fontSize: 33 }} />
-        </Circle>
-      )}
-      <Box>
-        <Box
-          sx={{
-            display: 'flex',
-            alignItems: 'center',
-          }}
-        >
-          <Link
-            component={RouterLink}
-            to={RouteUtils.getDashboardRoute(layer)}
-            sx={{ textDecoration: 'none' }}
-          >
-            <Typography
-              variant="h2"
-              color={COLORS.white}
-              sx={{ pr: isMobile ? 3 : 4, fontSize: isMobile ? '16px' : '24px', fontWeight: 700 }}
-            >
-              {content.header}
-            </Typography>
-          </Link>
+    <>
+      <Box
+        sx={{
+          position: 'relative',
+          display: 'flex',
+          py: isTablet ? 0 : 4,
+          pl: isTablet ? 0 : 4,
+          pr: isTablet ? 0 : 6,
+          border: isTablet ? 'none' : `3px solid ${COLORS.white025A}`,
+          borderRadius: isTablet ? 'none' : 3,
+        }}
+      >
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          {!isTablet && (
+            <Circle color={COLORS.white} size={6} sx={{ mr: 4 }}>
+              <LayerIcon sx={{ color: COLORS.brandExtraDark, fontSize: 33 }} />
+            </Circle>
+          )}
 
-          <Box
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-            }}
-          >
-            {!isMobile && (
-              <Typography sx={{ fontSize: 10, color: COLORS.ceil, mr: 3 }} component="span">
-                {t('common.paraTimeOnline')}
+          <Box>
+            <Box
+              sx={{
+                display: 'flex',
+                alignItems: 'baseline',
+                justifyContent: 'flex-start',
+              }}
+            >
+              <Button
+                variant="text"
+                onClick={onLayerModalOpen}
+                sx={{
+                  textTransform: 'capitalize',
+                  '&&': { background: 'transparent' },
+                  p: 0,
+                  height: 'auto',
+                }}
+              >
+                <Typography
+                  variant="h2"
+                  color={COLORS.white}
+                  sx={{ pr: isTablet ? 3 : 4, fontSize: isTablet ? '16px' : '24px', fontWeight: 700 }}
+                >
+                  {content.header}
+                </Typography>
+              </Button>
+
+              <Box
+                sx={{
+                  display: 'flex',
+                  alignItems: 'flex-end',
+                }}
+              >
+                {!isTablet && (
+                  <Typography sx={{ fontSize: 10, color: COLORS.ceil, mr: 3 }} component="span">
+                    {t('common.paraTimeOnline')}
+                  </Typography>
+                )}
+                <Circle color={COLORS.eucalyptus} size={4}>
+                  <CheckIcon sx={{ fontSize: 16, color: COLORS.white }} />
+                </Circle>
+              </Box>
+            </Box>
+            {!isTablet && (
+              <Typography sx={{ fontSize: 12, lineHeight: '18px', color: COLORS.white }}>
+                {content.description}
               </Typography>
             )}
-            <Circle color={COLORS.eucalyptus} size={4}>
-              <CheckIcon sx={{ fontSize: 16, color: COLORS.white }} />
-            </Circle>
           </Box>
+
+          {!isTablet && (
+            <Button
+              variant="text"
+              color="inherit"
+              sx={theme => ({
+                padding: 0,
+                marginLeft: 'auto',
+                height: 'auto',
+                textTransform: 'capitalize',
+                position: 'absolute',
+                top: theme.spacing(3),
+                right: theme.spacing(2),
+              })}
+              onClick={onLayerModalOpen}
+            >
+              <Typography variant="body2" color={COLORS.white} sx={{ fontWeight: 700 }}>
+                {t('common.change')}
+              </Typography>
+            </Button>
+          )}
         </Box>
-        {!isMobile && (
-          <Typography sx={{ fontSize: 12, lineHeight: '18px', color: COLORS.white }}>
-            {content.description}
-          </Typography>
-        )}
       </Box>
-    </Box>
+      <LayerDetailsModal open={open} setOpen={setOpen} />
+    </>
   )
 }
