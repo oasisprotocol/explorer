6f11f399aa40aa683c9218ebd005aeaa680663a3
Michal Zielenkiewicz <michal@oasisprotocol.org>
2023-05-23 21:54:32 +0200

    Create ParaTime picker layout

---
+++ src/app/components/ParaTimePicker/index.tsx
@@ -0,0 +1,79 @@
+import { FC, useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight'
+import KeyboardDoubleArrowLeftIcon from '@mui/icons-material/KeyboardDoubleArrowLeft'
+import Divider from '@mui/material/Divider'
+import IconButton from '@mui/material/IconButton'
+import Grid from '@mui/material/Unstable_Grid2'
+import { Logotype } from './../PageLayout/Logotype'
+import { COLORS } from '../../../styles/theme/colors'
+import { Network } from '../../../types/network'
+import { Layer } from '../../../oasis-indexer/api'
+import { useRequiredScopeParam } from '../../hooks/useScopeParam'
+import { NetworkMenuIcon } from './NetworkMenuIcon'
+
+type ParaTimePickerProps = {
+  onClose: () => void
+  onConfirm: (network: Network, layer: Layer) => void
+}
+
+export const ParaTimePicker: FC<ParaTimePickerProps> = ({ onClose, onConfirm }) => {
+  const { t } = useTranslation()
+  const { network, layer } = useRequiredScopeParam()
+  const [showNetworkMenu, setShowNetworkMenu] = useState(false)
+  const [selectedLayer, setSelectedLayer] = useState<undefined | Layer>()
+  const [selectedNetwork, setSelectedNetwork] = useState<undefined | Network>(Network.mainnet)
+  const [hoveredLayer, setHoveredLayer] = useState<undefined | Layer>()
+  const [hoveredNetwork, setHoveredNetwork] = useState<undefined | Network>()
+
+  return (
+    <Box>
+      <Box sx={{ mb: 5, color: 'red' }}>
+        <Logotype color={COLORS.brandExtraDark} />
+      </Box>
+      <IconButton
+        aria-label={t('paraTimePicker.toggleNetworkMenu')}
+        onClick={() => setShowNetworkMenu(!showNetworkMenu)}
+        sx={{
+          color: COLORS.brandDark,
+          ml: 3,
+        }}
+      >
+        {showNetworkMenu ? <KeyboardDoubleArrowLeftIcon /> : <KeyboardDoubleArrowRightIcon />}
+      </IconButton>
+      <Divider />
+      <Box
+        onMouseLeave={() => {
+          setHoveredNetwork(undefined)
+        }}
+      >
+        <Grid container>
+          {!showNetworkMenu && (
+            <Grid xs={1} sx={{ maxWidth: '40px' }}>
+              <NetworkMenuIcon network={selectedNetwork || network} />
+            </Grid>
+          )}
+          {showNetworkMenu && <Grid xs={3} />}
+          <Grid xs={3} />
+          <Grid xs={6} />
+        </Grid>
+
+        <Box sx={{ display: 'flex', justifyContent: 'flex-end', gap: 4 }}>
+          <Button onClick={onClose} color="secondary" variant="outlined" sx={{ textTransform: 'capitalize' }}>
+            {t('common.cancel')}
+          </Button>
+          <Button
+            onClick={() => onConfirm(selectedNetwork!, selectedLayer!)}
+            disabled={!selectedLayer || !selectedNetwork}
+            color="primary"
+            variant="contained"
+          >
+            {t('common.select')}
+          </Button>
+        </Box>
+      </Box>
+    </Box>
+  )
+}
