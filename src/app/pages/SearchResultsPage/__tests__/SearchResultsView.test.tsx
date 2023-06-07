515025082bf00b65d468aa8910742bd6948a183f
Csillag Kristof <csillag@harmacolindor.com>
2023-06-08 00:21:43 +0200

    Move SearchResultsView into a separate file

--- src/app/pages/SearchResultsPage/__tests__/SearchResultsView.test.tsx
+++ src/app/pages/SearchResultsPage/__tests__/SearchResultsView.test.tsx
@@ -1,6 +1,6 @@
 import { screen } from '@testing-library/react'
 import { renderWithProviders } from '../../../utils/renderWithProviders'
-import { SearchResultsView } from '../'
+import { SearchResultsView } from '../SearchResultsView'
 import {
   sapphireParsedBlock,
   suggestedParsedAccount,
