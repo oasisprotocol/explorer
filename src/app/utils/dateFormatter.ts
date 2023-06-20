a54d307f3d4930274721143b6aa2083a9da396ac
lubej <9722540+lubej@users.noreply.github.com>
2023-06-20 14:47:48 +0200

    Use intlFormatDistance instead of formatDistanceToNow

--- src/app/utils/dateFormatter.ts
+++ src/app/utils/dateFormatter.ts
@@ -1,67 +1,21 @@
-import { formatDistanceToNow as dateFnsFormatDistanceToNow } from 'date-fns'
-import locale from 'date-fns/locale/en-US'
+import intlFormatDistance from 'date-fns/intlFormatDistance'
 
 const dateFormat = new Intl.DateTimeFormat()
 
 export const intlDateFormat = (date: Date | number) => dateFormat.format(date)
 
-const formatDistanceLocale = {
-  lessThanXSeconds: '{{count}}sec',
-  xSeconds: '{{count}}sec',
-  halfAMinute: 'secs',
-  lessThanXMinutes: '{{count}}min',
-  xMinutes: '{{count}}mins',
-  aboutXHours: '{{count}}hr',
-  xHours: '{{count}}hrs',
-  xDays: '{{count}}days',
-  aboutXWeeks: '{{count}}wk',
-  xWeeks: '{{count}}wks',
-  aboutXMonths: '{{count}}mos',
-  xMonths: '{{count}}mos',
-  aboutXYears: '{{count}}yr',
-  xYears: '{{count}}yrs',
-  overXYears: '{{count}}yrs',
-  almostXYears: '{{count}}yrs',
-}
-
-interface FormatDistanceOpts {
-  includeSeconds?: boolean
-  addSuffix?: boolean
-  comparison?: number
-}
-
-const formatDistance = (
-  token: keyof typeof formatDistanceLocale,
-  count: string,
-  options: FormatDistanceOpts = {},
+// TODO: Works only in en-US locale, as suffixes are hardcoded
+export const formatDistanceToNow = (
+  date: Date | number,
+  baseDate: Date | number = new Date(),
+  locale = 'en-US',
 ) => {
-  const { addSuffix, comparison = 0 } = options
-
-  const result = formatDistanceLocale[token].replace('{{count}}', `${count} `)
-
-  if (addSuffix) {
-    if (comparison > 0) {
-      return `in ${result}`
-    } else {
-      return `${result} ago`
-    }
-  }
-
-  return result
-}
-
-export const formatDistanceToNow = (date: Date, shortFormat = true, opts: FormatDistanceOpts = {}) => {
-  if (shortFormat) {
-    return dateFnsFormatDistanceToNow(date, {
-      locale: {
-        ...locale,
-        formatDistance,
-      },
-      ...opts,
-    })
-  }
-
-  return dateFnsFormatDistanceToNow(date, {
-    ...opts,
+  return intlFormatDistance(date, baseDate, {
+    style: 'short',
+    numeric: 'always',
+    locale,
   })
+    .replace(/ ago/, '')
+    .replace(/in /, '')
+    .replace(/\./, '')
 }
