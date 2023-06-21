ecf25011410d1fb16977a02f73c11bc0242586c6
lubej <9722540+lubej@users.noreply.github.com>
2023-06-21 19:28:20 +0200

    Add simple wrapper for storage

---
+++ src/app/utils/storage.ts
@@ -0,0 +1,40 @@
+import { AppError, AppErrors } from '../../types/errors'
+import { StorageKeys } from '../../types/storage'
+
+export const storage = (storage = localStorage) => {
+  const set = <T = any>(key: StorageKeys, value: T): void => {
+    try {
+      const serializedValue = JSON.stringify(value)
+      storage.setItem(key, serializedValue)
+    } catch (error) {
+      throw new AppError(AppErrors.Storage)
+    }
+  }
+
+  const get = <T = any>(key: StorageKeys): T | undefined => {
+    try {
+      const serializedValue = storage.getItem(key)
+      if (!serializedValue) {
+        return
+      }
+      return JSON.parse(serializedValue)
+    } catch (error) {
+      throw new AppError(AppErrors.Storage)
+    }
+  }
+
+  const removeItem = (key: StorageKeys) => {
+    storage.removeItem(key)
+  }
+
+  const clear = () => {
+    storage.clear()
+  }
+
+  return {
+    set,
+    get,
+    removeItem,
+    clear,
+  }
+}
