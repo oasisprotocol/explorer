bcbdc4a47ec708dc0776c8c84277cc72703b15fc
lukaw3d <lukajeran@oasislabs.com>
2023-07-03 19:26:40 +0200

    Remove feedback form from banner on staging

--- src/app/utils/externalLinks.ts
+++ src/app/utils/externalLinks.ts
@@ -24,10 +24,6 @@ export const docs = {
 
 export const referrals = { coinGecko: 'https://www.coingecko.com' }
 
-export const feedback = {
-  internalForm: 'https://forms.gle/456sjk9VXhhKkVz19',
-}
-
 const githubLink = 'https://github.com/oasisprotocol/explorer/'
 export const github = {
   home: githubLink,
