15bf206d1188160c3e2c2e4395665df715bac3ee
Michal Zielenkiewicz <michal@oasisprotocol.org>
2023-07-06 14:59:17 +0200

    Improve layout of search header

--- src/app/pages/SearchResultsPage/ResultListFrame.tsx
+++ src/app/pages/SearchResultsPage/ResultListFrame.tsx
@@ -4,6 +4,7 @@ import { cardClasses } from '@mui/material/Card'
 import { boxClasses } from '../../utils/mui-helpers'
 import useMediaQuery from '@mui/material/useMediaQuery'
 import { paperClasses } from '@mui/material/Paper'
+import { COLORS } from '../../../styles/theme/colors'
 
 export const ResultListFrame = styled(Box)(({ theme: wantedTheme }) => {
   const currentTheme = useTheme()
@@ -12,8 +13,7 @@ export const ResultListFrame = styled(Box)(({ theme: wantedTheme }) => {
     ? {
         [`&& > div > .${paperClasses.root}`]:
           currentTheme.palette.layout.border !== wantedTheme.palette.layout.networkBubbleBorder &&
-          wantedTheme.palette.layout.networkBubbleBackground !==
-            wantedTheme.palette.layout.networkBubbleBorder
+          wantedTheme.palette.layout.main !== wantedTheme.palette.layout.networkBubbleBorder
             ? {
                 borderRadius: 0,
                 border: `solid ${wantedTheme.palette.layout.networkBubbleBorder}`,
@@ -24,26 +24,16 @@ export const ResultListFrame = styled(Box)(({ theme: wantedTheme }) => {
     : {
         marginBottom: 20,
         border: `solid 15px ${wantedTheme.palette.layout.networkBubbleBorder}`,
-        background: wantedTheme.palette.layout.networkBubbleBackground,
+        background: COLORS.white,
         borderRadius: 12,
         boxShadow: '-20px 4px 40px rgba(34, 47, 63, 0.24)',
-        [`&& .${cardClasses.root}`]: {
-          borderTopLeftRadius: 0,
-          borderTopRightRadius: 0,
-          paddingLeft: 64,
-          paddingRight: 64,
-          background: wantedTheme.palette.layout.networkBubbleBackground,
-        },
         [`&& .${cardClasses.root} > .${boxClasses.root}`]: {
           background: wantedTheme.palette.layout.networkBubbleBorder,
-          borderRadius: 0,
-          marginLeft: -64,
-          marginTop: -32,
-          marginRight: -64,
+          marginLeft: -78,
+          marginTop: -48,
+          marginRight: -78,
           paddingLeft: 64,
           paddingRight: 64,
-          paddingBottom: 32,
-          paddingTop: 32,
         },
       }
 })
