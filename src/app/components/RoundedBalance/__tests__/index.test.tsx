27a26d8446b5bad16183c95cc9d1d72eeef5839b
lukaw3d <lukajeran@oasislabs.com>
2023-07-11 14:45:30 +0200

    Cleanup RoundedBalance test

--- src/app/components/RoundedBalance/__tests__/index.test.tsx
+++ src/app/components/RoundedBalance/__tests__/index.test.tsx
@@ -4,45 +4,31 @@ import { RoundedBalance } from './../'
 describe('RoundedBalance', () => {
   it('should keep original value', () => {
     render(<RoundedBalance value="0.00075" ticker="USDC" />)
-
     expect(screen.getByText('0.00075 USDC')).toBeInTheDocument()
-  })
-
-  it('should render value', () => {
-    render(<RoundedBalance value="0.002231" ticker="USDC" />)
-
-    expect(screen.getByText('0.00223… USDC')).toBeInTheDocument()
   })
 
   it('should render rounded value', () => {
     render(<RoundedBalance value="0.00223100001" ticker="USDC" />)
-
     expect(screen.getByText('0.00223… USDC')).toBeInTheDocument()
   })
 
   it('should render variant when rounded value is equal zero', () => {
     render(<RoundedBalance value="0.00000000000002231" ticker="USDC" />)
-
     expect(screen.getByText('< 0.00000 USDC')).toBeInTheDocument()
   })
 
   it('should not round value up', () => {
     render(<RoundedBalance value="0.004795600000000000" ticker="USDC" />)
-
     expect(screen.getByText('0.00479… USDC')).toBeInTheDocument()
   })
 
   it('should not round negative value up', () => {
     render(<RoundedBalance value="-0.004795600000000000" ticker="USDC" />)
-
     expect(screen.getByText('-0.00479… USDC')).toBeInTheDocument()
   })
-})
 
-describe('RoundedRoseBalance', () => {
   it('should render value with ROSE ticker symbol', () => {
     render(<RoundedBalance value="0.002231" ticker="ROSE" />)
-
     expect(screen.getByText('0.00223… ROSE')).toBeInTheDocument()
   })
 })
