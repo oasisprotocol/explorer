80560fee9a9c5f0a4f47b4418a2b01e47d9cb693
lukaw3d <lukajeran@oasislabs.com>
2023-07-20 01:56:41 +0200

    update urls

--- src/app/components/logo/SmallLogo.tsx
+++ src/app/components/logo/SmallLogo.tsx
@@ -1,6 +1,5 @@
 import { FC } from 'react'
-import logo from '../../../../public/logo512.png'
 
 export const SmallLogo: FC<{ size?: number }> = ({ size = 25 }) => (
-  <img src={logo} height={size} width={size} alt="" />
+  <img src="/logo512.png" height={size} width={size} alt="" />
 )
