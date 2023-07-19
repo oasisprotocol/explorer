916731749d638e5b469ab9fb1523b2ace5984cdb
lukaw3d <lukajeran@oasislabs.com>
2023-07-20 01:45:31 +0200

    Fix Buffer runtime issues

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './polyfill'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
