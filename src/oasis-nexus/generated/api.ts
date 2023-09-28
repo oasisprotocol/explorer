c234c8634fb034ed2130fd02bc9b34f5890fb361
Kaja <kaja.osojnik@gmail.com>
2023-09-28 17:41:20 +0200

    Update API bindings

--- src/oasis-nexus/generated/api.ts
+++ src/oasis-nexus/generated/api.ts
@@ -760,6 +760,8 @@ In practice, Nexus currently expects only the following methods:
   - "accounts.Transfer"
   - "consensus.Deposit"
   - "consensus.Withdraw"
+  - "consensus.Delegate"
+  - "consensus.Undelegate"
   - "evm.Create"
   - "evm.Call"
 May be null if the transaction was malformed or encrypted.
@@ -779,7 +781,9 @@ Note: Other transactions with method "evm.Call", and possibly "evm.Create", may 
 if applicable. The meaning varies based on the transaction method. Some notable examples:
   - For `method = "accounts.Transfer"`, this is the paratime account receiving the funds.
   - For `method = "consensus.Deposit"`, this is the paratime account receiving the funds.
-  - For `method = "consensus.Withdraw"`, this is a consensus (!) account receiving the funds.
+  - For `method = "consensus.Withdraw"`, this is the consensus (!) account receiving the funds.
+  - For `method = "consensus.Delegate"`, this is the consensus (!) account receiving the funds.
+  - For `method = "consensus.Undelegate"`, this is the consensus (!) account to which funds were previously delegated. Note that this corresponds with the `.from` field in the transaction body.
   - For `method = "evm.Create"`, this is the address of the newly created smart contract.
   - For `method = "evm.Call"`, this is the address of the called smart contract
  */
