2f9566b57c77ea353a1d9a4df23d283635f232e1
lukaw3d <lukajeran@oasislabs.com>
2023-10-18 01:34:11 +0200

    todo anonymize

--- src/app/components/AnalyticsConsent/initializeMatomo.ts
+++ src/app/components/AnalyticsConsent/initializeMatomo.ts
@@ -18,6 +18,30 @@ window._paq.push(['enableLinkTracking'])
 window._paq.push(['setTrackerUrl', `${matomoDomain}matomo.php`])
 window._paq.push(['setSiteId', '1'])
 
+window._paq.push([
+  'setCustomRequestProcessing',
+  (queryString: string) => {
+    // TODO: Anonymize
+    const params = new URLSearchParams(queryString)
+    // params.get('action_name')
+    // params.get('url')
+    // params.get('urlref')
+
+    // https://explorer.dev.oasis.io/mainnet/emerald/tx/0xf0f08f00150f5d4273c0cf07f36ee7545e5e130e477a8528271379ce3628e104
+    // https://explorer.dev.oasis.io/mainnet/sapphire/address/0xEd03EA9c96ec39097548256E428a163E5f524e47/tokens/erc-721#0xFcfed3be2d333F24854cA8d3A351E772272D5842
+    // .replaceAll(/0x[a-f0-9]+/ig, '0x...')
+    // https://explorer.dev.oasis.io/mainnet/emerald/address/oasis1qrvha284gfztn7wwq6z50c86ceu28jp7csqhpx9t
+    // https://explorer.dev.oasis.io/search?q=oasis1qrvha284gfztn7wwq6z50c86ceu28jp7csqhpx9t
+    // .replaceAll(/oasis1[a-z0-9]+/ig, 'oasis1...')
+    // https://explorer.dev.oasis.io/mainnet/emerald/tx/aa5a562c714c9a4e8ecd88230757bc60f66765e56bd4645e11198457b8b278dd
+    // .replaceAll TODO
+    // https://explorer.dev.oasis.io/search?q=test%20test%20test%20test%20test%20test%20test%20test%20test%20test%20test%20key
+    // .replaceAll TODO
+
+    return params.toString()
+  },
+])
+
 const d = document,
   g = d.createElement('script'),
   s = d.getElementsByTagName('script')[0]
