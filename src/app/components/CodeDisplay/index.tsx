dd2fd5f1dbe6c572aedc2898532088d41de502c5
Michal Zielenkiewicz <michal@oasisprotocol.org>
2023-11-23 11:08:37 +0100

    Allow to render json code and raw data

---
+++ src/app/components/CodeDisplay/index.tsx
@@ -0,0 +1,76 @@
+import { FC, ReactNode } from 'react'
+import { useTranslation } from 'react-i18next'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import { ScrollableDataDisplay } from '../../components/ScrollableDataDisplay'
+import { CopyToClipboard } from '../../components/CopyToClipboard'
+import { useScreenSize } from '../../hooks/useScreensize'
+import { base64ToHex } from '../../utils/helpers'
+import { type } from 'os'
+
+type CodeDisplayProps = {
+  code: ReactNode
+  copyToClipboardValue: string
+  label: string
+  extraTopPadding?: boolean
+}
+
+const CodeDisplay: FC<CodeDisplayProps> = ({ code, copyToClipboardValue, label, extraTopPadding }) => {
+  const { t } = useTranslation()
+  const { isMobile } = useScreenSize()
+
+  if (!code) {
+    return null
+  }
+
+  return (
+    <>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          my: 3,
+          pt: extraTopPadding ? 4 : 0,
+        }}
+      >
+        <Typography variant="h4" component="h4">
+          {label}
+        </Typography>
+        <CopyToClipboard
+          value={copyToClipboardValue}
+          label={isMobile ? t('common.copy') : t('contract.copyButton', { subject: label })}
+        />
+      </Box>
+
+      <ScrollableDataDisplay data={code} />
+    </>
+  )
+}
+
+type RawDataDisplayProps = {
+  data: string | undefined
+  label: string
+  extraTopPadding?: boolean
+}
+
+export const RawDataDisplay: FC<RawDataDisplayProps> = ({ data, label, extraTopPadding }) => {
+  const code = data === undefined ? undefined : base64ToHex(data)
+  if (!code) {
+    return null
+  }
+  return (
+    <CodeDisplay code={code} copyToClipboardValue={code} label={label} extraTopPadding={extraTopPadding} />
+  )
+}
+
+type JsonCodeDisplayProps = {
+  data: Record<string, any>
+  label: string
+}
+
+export const JsonCodeDisplay: FC<JsonCodeDisplayProps> = ({ data, label }) => {
+  const formattedJson = JSON.stringify(data, null, 2)
+
+  return <CodeDisplay code={<pre>{formattedJson}</pre>} copyToClipboardValue={formattedJson} label={label} />
+}
--- src/app/pages/AccountDetailsPage/ContractCodeCard.tsx
+++ src/app/pages/AccountDetailsPage/ContractCodeCard.tsx
@@ -5,48 +5,10 @@ import CardContent from '@mui/material/CardContent'
 import { LinkableDiv } from '../../components/PageLayout/LinkableDiv'
 import { useAccount } from './hook'
 import { CardEmptyState } from './CardEmptyState'
-import Typography from '@mui/material/Typography'
-import { ScrollableDataDisplay } from '../../components/ScrollableDataDisplay'
-import Box from '@mui/material/Box'
-import { CopyToClipboard } from '../../components/CopyToClipboard'
-import { useScreenSize } from '../../hooks/useScreensize'
-import { base64ToHex } from '../../utils/helpers'
 import { TokenDashboardContext } from '../TokenDashboardPage'
+import { RawDataDisplay } from '../../components/CodeDisplay'
 
 export const contractCodeContainerId = 'code'
-
-const CodeDisplay: FC<{ rawData: string | undefined; label: string; extraTopPadding?: boolean }> = ({
-  rawData,
-  label,
-  extraTopPadding,
-}) => {
-  const { t } = useTranslation()
-  const { isMobile } = useScreenSize()
-  const code = rawData === undefined ? undefined : base64ToHex(rawData)
-  return code === undefined ? null : (
-    <>
-      <Box
-        sx={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          my: 3,
-          pt: extraTopPadding ? 4 : 0,
-        }}
-      >
-        <Typography variant="h4" component="h4">
-          {label}
-        </Typography>
-        <CopyToClipboard
-          value={code}
-          label={isMobile ? t('common.copy') : t('contract.copyButton', { subject: label })}
-        />
-      </Box>
-
-      <ScrollableDataDisplay data={code} />
-    </>
-  )
-}
 
 export const ContractCodeCard: FC<TokenDashboardContext> = ({ scope, address }) => {
   const { t } = useTranslation()
@@ -60,9 +22,9 @@ export const ContractCodeCard: FC<TokenDashboardContext> = ({ scope, address }) 
       {contract && (contract.creation_bytecode || contract.runtime_bytecode) && (
         <CardContent>
           <LinkableDiv id={contractCodeContainerId}>
-            <CodeDisplay rawData={contract.creation_bytecode} label={t('contract.creationByteCode')} />
-            <CodeDisplay
-              rawData={contract.runtime_bytecode}
+            <RawDataDisplay data={contract.creation_bytecode} label={t('contract.creationByteCode')} />
+            <RawDataDisplay
+              data={contract.runtime_bytecode}
               label={t('contract.runtimeByteCode')}
               extraTopPadding={!!contract.creation_bytecode}
             />
