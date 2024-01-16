017d59660990830e56103cdad1bec28123473162
Michal Zielenkiewicz <michal@oasisprotocol.org>
2024-01-16 12:54:53 +0100

    Add TransactionStats card to Consensus dashboard

--- src/app/pages/ConsensusDashboardPage/index.tsx
+++ src/app/pages/ConsensusDashboardPage/index.tsx
@@ -2,14 +2,18 @@ import { FC } from 'react'
 import Divider from '@mui/material/Divider'
 import { PageLayout } from '../../components/PageLayout'
 import { useScreenSize } from '../../hooks/useScreensize'
+import { TransactionsStats } from '../../components/TransactionsStats'
 import { Social } from '../../components/Social'
+import { useRequiredScopeParam } from '../../hooks/useScopeParam'
 
 export const ConsensusDashboardPage: FC = () => {
   const { isMobile } = useScreenSize()
+  const scope = useRequiredScopeParam()
 
   return (
     <PageLayout>
       <Divider variant="layout" sx={{ mt: isMobile ? 4 : 0 }} />
+      <TransactionsStats scope={scope} />
       <Social />
     </PageLayout>
   )
