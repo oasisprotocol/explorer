fd46890e6f295bfcdf834bdeff355f5c5671e2ce
Michal Zielenkiewicz <michal@oasisprotocol.org>
2024-01-22 12:43:10 +0100

    Rename ParaTimePicker component

--- src/app/components/PageLayout/NetworkSelector.tsx
+++ src/app/components/PageLayout/NetworkSelector.tsx
@@ -9,7 +9,7 @@ import { NetworkButton, MobileNetworkButton } from './NetworkButton'
 import { COLORS } from '../../../styles/theme/colors'
 import { Network, getNetworkNames } from '../../../types/network'
 import { Layer } from '../../../oasis-nexus/api'
-import { ParaTimePicker } from './../ParaTimePicker'
+import { LayerPicker } from './../LayerPicker'
 import { RouteUtils } from '../../utils/route-utils'
 import { useConsensusFreshness, useRuntimeFreshness } from '../OfflineBanner/hook'
 
@@ -72,7 +72,7 @@ const NetworkSelectorView: FC<NetworkSelectorViewProps> = ({ isOutOfDate, layer,
         justifyContent: isTablet ? 'flex-end' : 'center',
       }}
     >
-      <ParaTimePicker
+      <LayerPicker
         open={openDrawer}
         onClose={handleDrawerClose}
         onConfirm={(network: Network, layer: Layer) => {
