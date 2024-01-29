f5207b40bdc85f6f03a064cf2163fff3f58a6a74
Csillag Kristof <csillag@harmacolindor.com>
2024-01-29 14:36:42 +0100

    Add hook for formatting timestamp with no distance

--- src/app/hooks/useFormattedTimestamp.ts
+++ src/app/hooks/useFormattedTimestamp.ts
@@ -2,6 +2,24 @@ import { formatDistanceStrict } from 'date-fns/formatDistanceStrict'
 import { useTranslation } from 'react-i18next'
 import { useScreenSize } from './useScreensize'
 
+export const useFormattedTimestamp = (timestamp: Date | undefined) => {
+  const { t } = useTranslation()
+  if (!timestamp) return ''
+  return t('common.formattedDateTime', {
+    timestamp,
+    formatParams: {
+      timestamp: {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        timeZoneName: 'short',
+      } satisfies Intl.DateTimeFormatOptions,
+    },
+  })
+}
+
 export const useFormattedTimestampWithDistance = (timestamp: Date | undefined) => {
   const { t } = useTranslation()
   const { isMobile } = useScreenSize()
