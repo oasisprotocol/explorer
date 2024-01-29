7f02c381cd7d91f98f38c4a41b9e9ab3db4b1cc8
Michal Zielenkiewicz <michal@oasisprotocol.org>
2024-01-29 13:30:59 +0100

    Add fallback to missing validator image

--- src/app/components/Validators/ValidatorImage.tsx
+++ src/app/components/Validators/ValidatorImage.tsx
@@ -1,13 +1,15 @@
 import { FC } from 'react'
 import Box from '@mui/material/Box'
 import { styled } from '@mui/material/styles'
+import ImageNotSupportedIcon from '@mui/icons-material/ImageNotSupported'
 import { hasValidProtocol } from '../../utils/url'
+import { COLORS } from 'styles/theme/colors'
+import { Circle } from '../Circle'
 
 const StyledImage = styled('img')({
   width: '28px',
   height: '28px',
   borderRadius: 5,
-  marginRight: 15,
 })
 
 type ValidatorImageProps = {
@@ -18,8 +20,14 @@ type ValidatorImageProps = {
 
 export const ValidatorImage: FC<ValidatorImageProps> = ({ address, name, logotype }) => {
   return (
-    <Box sx={{ display: 'flex', alignItems: 'center' }}>
-      {logotype && hasValidProtocol(logotype) && <StyledImage alt={name || address} src={logotype} />}
+    <Box sx={{ display: 'flex', alignItems: 'center' }} gap={3}>
+      {logotype && hasValidProtocol(logotype) ? (
+        <StyledImage alt={name || address} src={logotype} />
+      ) : (
+        <Circle color={COLORS.grayMedium2} size={5}>
+          <ImageNotSupportedIcon sx={{ color: COLORS.grayMedium, fontSize: 18 }} />
+        </Circle>
+      )}
       {name || address}
     </Box>
   )
