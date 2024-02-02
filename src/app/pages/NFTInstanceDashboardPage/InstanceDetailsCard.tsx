037e87f6c709708264c797cc3a5b4c67ee16e1b4
Michal Zielenkiewicz <michal@oasisprotocol.org>
2024-02-02 20:27:34 +0100

    Fix condition for number of transfer in token details

--- src/app/pages/NFTInstanceDashboardPage/InstanceDetailsCard.tsx
+++ src/app/pages/NFTInstanceDashboardPage/InstanceDetailsCard.tsx
@@ -72,7 +72,7 @@ export const InstanceDetailsCard: FC<InstanceDetailsCardProps> = ({
                 </dd>
               </>
             )}
-            {nft?.num_transfers && (
+            {typeof nft?.num_transfers === 'number' && !isNaN(nft?.num_transfers) && (
               <>
                 <dt>{t('nft.transfers')}</dt>
                 <dd>{nft.num_transfers.toLocaleString()}</dd>
