0518c8a97684a40a87ca4c934722f733f88816da
Michal Zielenkiewicz <michal@oasisprotocol.org>
2024-02-08 15:55:55 +0100

    Add panel with accounts charts to RuntimePreview

--- src/app/pages/ConsensusDashboardPage/RuntimePreview.tsx
+++ src/app/pages/ConsensusDashboardPage/RuntimePreview.tsx
@@ -1,11 +1,14 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { Link as RouterLink } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
 import Box from '@mui/material/Box'
+import IconButton from '@mui/material/IconButton'
 import Link from '@mui/material/Link'
 import Typography from '@mui/material/Typography'
 import { styled } from '@mui/material/styles'
 import FilterNoneIcon from '@mui/icons-material/FilterNone'
+import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked'
+import CircleIcon from '@mui/icons-material/Circle'
 import { Runtime, useGetRuntimeStatus } from 'oasis-nexus/api'
 import { COLORS } from '../../../styles/theme/colors'
 import { StatusIcon } from '../../components/StatusIcon'
@@ -17,6 +20,7 @@ import { RouteUtils } from '../../utils/route-utils'
 import { ChartDuration } from '../../utils/chart-utils'
 import { Network } from '../../../types/network'
 import { TransactionsChartCard } from '../ParatimeDashboardPage/TransactionsChartCard'
+import { ActiveAccounts } from '../ParatimeDashboardPage/ActiveAccounts'
 
 const StyledList = styled(InlineDescriptionList)(({ theme }) => ({
   marginBottom: theme.spacing(4),
@@ -84,8 +88,11 @@ type RuntimePreviewProps = RuntimeProps & {
   }
 }
 
+type Panels = 'transactions' | 'accounts'
+
 const RuntimePreview: FC<RuntimePreviewProps> = ({ network, runtime, status }) => {
   const { t } = useTranslation()
+  const [panel, setPanel] = useState<Panels>('transactions')
   const layerLabels = getLayerLabels(t)
   const runtimeLabel = layerLabels[runtime]
   const dashboardLink = RouteUtils.getDashboardRoute({ network, layer: runtime })
@@ -135,17 +142,29 @@ const RuntimePreview: FC<RuntimePreviewProps> = ({ network, runtime, status }) =
       </StyledList>
       <StyledBox>
         <Box gap={3} sx={{ flex: 1, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-          {status ? (
+          {status && (
             <Box sx={{ width: '100%' }}>
-              <TransactionsChartCard
-                scope={{
-                  layer: runtime,
-                  network,
-                }}
-                chartDuration={ChartDuration.TODAY}
-              />
+              {panel === 'transactions' && (
+                <TransactionsChartCard
+                  scope={{
+                    layer: runtime,
+                    network,
+                  }}
+                  chartDuration={ChartDuration.TODAY}
+                />
+              )}
+              {panel === 'accounts' && (
+                <ActiveAccounts
+                  scope={{
+                    layer: runtime,
+                    network,
+                  }}
+                  chartDuration={ChartDuration.TODAY}
+                />
+              )}
             </Box>
-          ) : (
+          )}
+          {!status && (
             <>
               <FilterNoneIcon sx={{ color: COLORS.brandDark, fontSize: '33px' }} />
               {t('paratimes.noData')}
@@ -153,6 +172,45 @@ const RuntimePreview: FC<RuntimePreviewProps> = ({ network, runtime, status }) =
           )}
         </Box>
       </StyledBox>
+      <Box sx={{ display: 'flex', justifyContent: 'center', paddingTop: 3 }}>
+        {status && (
+          <>
+            <PanelButton
+              activePanel={panel}
+              ariaLabel={t('common.transactions')}
+              panel="transactions"
+              setPanel={setPanel}
+            />
+            <PanelButton
+              activePanel={panel}
+              ariaLabel={t('account.listTitle')}
+              panel="accounts"
+              setPanel={setPanel}
+            />
+          </>
+        )}
+      </Box>
     </Box>
+  )
+}
+
+type PanelButtonProps = {
+  activePanel: Panels
+  ariaLabel: string
+  panel: Panels
+  setPanel: (panel: Panels) => void
+}
+
+const PanelButton: FC<PanelButtonProps> = ({ activePanel, ariaLabel, panel, setPanel }) => {
+  return (
+    <>
+      <IconButton aria-label={ariaLabel} onClick={() => setPanel(panel)}>
+        {panel === activePanel ? (
+          <CircleIcon sx={{ color: COLORS.brandDark, fontSize: '10px' }} />
+        ) : (
+          <RadioButtonUncheckedIcon sx={{ color: COLORS.brandDark, fontSize: '10px' }} />
+        )}
+      </IconButton>
+    </>
   )
 }
