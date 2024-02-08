0238c8bed08d8d70811f02c8096a39bd250c0a7c
Michal Zielenkiewicz <michal@oasisprotocol.org>
2024-02-08 12:20:58 +0100

    Move learning material links styles to common components

--- src/app/components/LearningMaterialsCard/LearningSection.tsx
+++ src/app/components/LearningMaterialsCard/LearningSection.tsx
@@ -1,26 +1,8 @@
 import Paper, { type PaperProps } from '@mui/material/Paper'
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward'
-import Link from '@mui/material/Link'
 import Typography from '@mui/material/Typography'
-import { styled } from '@mui/material/styles'
 import { FC } from 'react'
 import { COLORS } from 'styles/theme/colors'
-
-const StyledLink = styled(Link)(() => ({
-  width: '44px',
-  height: '44px',
-  borderRadius: '50%',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  marginLeft: 'auto',
-  backgroundColor: COLORS.brandDark,
-  color: COLORS.white,
-  boxShadow: '0px 10px 8px rgba(117, 60, 239, 0.1)',
-  '&:hover, &:focus-visible': {
-    backgroundColor: COLORS.cosmicCobalt,
-  },
-}))
+import { AnchorCircle } from '../StyledLinks'
 
 type LearningSectionProps = PaperProps & {
   description: string
@@ -37,9 +19,7 @@ export const LearningSection: FC<LearningSectionProps> = ({ description, title, 
       <Typography variant="body2" sx={{ color: COLORS.grayMedium }}>
         {description}
       </Typography>
-      <StyledLink href={url} rel="noopener noreferrer" target="_blank">
-        <ArrowForwardIcon sx={{ fontSize: 16 }} />
-      </StyledLink>
+      <AnchorCircle url={url} />
     </Paper>
   )
 }
