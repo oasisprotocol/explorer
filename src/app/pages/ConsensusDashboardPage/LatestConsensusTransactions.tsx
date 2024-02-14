0b90380e1392172157748b2d5e7423843280eb94
Michal Zielenkiewicz <michal@oasisprotocol.org>
2024-02-14 10:52:51 +0100

    Create Consensus transactions list components

---
+++ src/app/pages/ConsensusDashboardPage/LatestConsensusTransactions.tsx
@@ -0,0 +1,53 @@
+import { FC } from 'react'
+import { useTranslation } from 'react-i18next'
+import Card from '@mui/material/Card'
+import CardHeader from '@mui/material/CardHeader'
+import CardContent from '@mui/material/CardContent'
+import { Link as RouterLink } from 'react-router-dom'
+import Link from '@mui/material/Link'
+import { useGetConsensusTransactions } from '../../../oasis-nexus/api'
+import { SearchScope } from '../../../types/searchScope'
+import { ConsensusTransactions } from '../../components/Transactions'
+import { NUMBER_OF_ITEMS_ON_DASHBOARD as limit } from '../../config'
+import { RouteUtils } from '../../utils/route-utils'
+import { useScreenSize } from '../../hooks/useScreensize'
+
+export const LatestConsensusTransactions: FC<{ scope: SearchScope }> = ({ scope }) => {
+  const { isTablet } = useScreenSize()
+  const { t } = useTranslation()
+  const { network } = scope
+
+  const transactionsQuery = useGetConsensusTransactions(
+    network,
+    { limit },
+    {
+      query: {
+        cacheTime: 0,
+      },
+    },
+  )
+
+  return (
+    <Card>
+      <CardHeader
+        disableTypography
+        component="h3"
+        title={t('transactions.latest')}
+        action={
+          <Link component={RouterLink} to={RouteUtils.getLatestTransactionsRoute(scope)}>
+            {t('common.viewAll')}
+          </Link>
+        }
+      />
+      <CardContent>
+        <ConsensusTransactions
+          transactions={transactionsQuery.data?.data.transactions}
+          isLoading={transactionsQuery.isLoading}
+          limit={limit}
+          pagination={false}
+          verbose={!isTablet}
+        />
+      </CardContent>
+    </Card>
+  )
+}
---
+++ src/app/pages/ConsensusTransactionsPage/index.tsx
@@ -0,0 +1,102 @@
+import { FC, useEffect, useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import Divider from '@mui/material/Divider'
+import { useScreenSize } from '../../hooks/useScreensize'
+import { PageLayout } from '../../components/PageLayout'
+import { SubPageCard } from '../../components/SubPageCard'
+import { TableConsensusTransactionList, ConsensusTransactions } from '../../components/Transactions'
+import { useGetConsensusTransactions } from '../../../oasis-nexus/api'
+import { NUMBER_OF_ITEMS_ON_SEPARATE_PAGE as limit, REFETCH_INTERVAL } from '../../config'
+import { useSearchParamsPagination } from '../../components/Table/useSearchParamsPagination'
+import { AxiosResponse } from 'axios'
+import { AppErrors } from '../../../types/errors'
+import { LoadMoreButton } from '../../components/LoadMoreButton'
+import { TableLayout, TableLayoutButton } from '../../components/TableLayoutButton'
+import { useRequiredScopeParam } from '../../hooks/useScopeParam'
+import { VerticalList } from '../../components/VerticalList'
+
+export const ConsensusTransactionsPage: FC = () => {
+  const [tableView, setTableView] = useState<TableLayout>(TableLayout.Horizontal)
+  const { t } = useTranslation()
+  const { isMobile } = useScreenSize()
+  const pagination = useSearchParamsPagination('page')
+  const offset = (pagination.selectedPage - 1) * limit
+  const scope = useRequiredScopeParam()
+
+  useEffect(() => {
+    if (!isMobile) {
+      setTableView(TableLayout.Horizontal)
+    }
+  }, [isMobile, setTableView])
+
+  const transactionsQuery = useGetConsensusTransactions<AxiosResponse<TableConsensusTransactionList>>(
+    scope.network,
+    {
+      limit: tableView === TableLayout.Vertical ? offset + limit : limit,
+      offset: tableView === TableLayout.Vertical ? 0 : offset,
+    },
+    {
+      query: {
+        refetchInterval: REFETCH_INTERVAL,
+        structuralSharing: (previousState, nextState) => {
+          const oldTxHashes = new Set(previousState?.data.transactions.map(tx => tx.hash))
+          return {
+            ...nextState,
+            data: {
+              ...nextState.data,
+              transactions: nextState.data.transactions.map(tx => {
+                return {
+                  ...tx,
+                  markAsNew: previousState ? !oldTxHashes.has(tx.hash) : false,
+                }
+              }),
+            },
+          }
+        },
+        keepPreviousData: tableView === TableLayout.Vertical,
+      },
+    },
+  )
+
+  const { isLoading, isFetched, data } = transactionsQuery
+
+  const transactions = data?.data.transactions
+
+  if (isFetched && pagination.selectedPage > 1 && !transactions?.length) {
+    throw AppErrors.PageDoesNotExist
+  }
+
+  return (
+    <PageLayout
+      mobileFooterAction={
+        tableView === TableLayout.Vertical && <LoadMoreButton pagination={pagination} isLoading={isLoading} />
+      }
+    >
+      {!isMobile && <Divider variant="layout" />}
+      <SubPageCard
+        title={t('transactions.latest')}
+        action={isMobile && <TableLayoutButton tableView={tableView} setTableView={setTableView} />}
+        noPadding={tableView === TableLayout.Vertical}
+      >
+        {tableView === TableLayout.Horizontal && (
+          <ConsensusTransactions
+            transactions={data?.data.transactions}
+            isLoading={isLoading}
+            limit={limit}
+            pagination={{
+              selectedPage: pagination.selectedPage,
+              linkToPage: pagination.linkToPage,
+              totalCount: data?.data.total_count,
+              isTotalCountClipped: data?.data.is_total_count_clipped,
+              rowsPerPage: limit,
+            }}
+          />
+        )}
+
+        {tableView === TableLayout.Vertical && (
+          <VerticalList>{/* TODO: Add when details page is ready  */}</VerticalList>
+        )}
+      </SubPageCard>
+    </PageLayout>
+  )
+}
