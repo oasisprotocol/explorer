f89e5bf57f7e194dda22494f868d9d45a2a543a0
Michal Zielenkiewicz <michal@oasisprotocol.org>
2024-02-21 13:58:53 +0100

    Improve spacing of SnapshotDelegators card

--- src/app/pages/ConsensusDashboardPage/SnapshotDelegators.tsx
+++ src/app/pages/ConsensusDashboardPage/SnapshotDelegators.tsx
@@ -1,13 +1,18 @@
 import { FC } from 'react'
 import { useTranslation } from 'react-i18next'
+import Box from '@mui/material/Box'
 import { SnapshotTextCard } from '../../components/Snapshots/SnapshotCard'
+import { useScreenSize } from '../../hooks/useScreensize'
 
 export const SnapshotDelegators: FC = () => {
   const { t } = useTranslation()
+  const { isMobile } = useScreenSize()
+  // TODO: provide delegators number when API is ready
+  const delegators = undefined
 
   return (
     <SnapshotTextCard title={t('validator.delegators')}>
-      {/* TODO: provide delegators number when API is ready */}-
+      <Box sx={{ pb: isMobile ? 3 : 5 }}>{delegators}</Box>
     </SnapshotTextCard>
   )
 }
