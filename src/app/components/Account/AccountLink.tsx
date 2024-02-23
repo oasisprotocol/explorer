a0c54a5101ea81cc3254ef68145e66defa6d26de
Kristof Csillag <kristof@oasisprotocol.org>
2024-02-23 03:45:29 +0100

    Re-style account link to fill space in detaile on mobile

--- src/app/components/Account/AccountLink.tsx
+++ src/app/components/Account/AccountLink.tsx
@@ -1,30 +1,95 @@
-import { FC } from 'react'
+import { FC, ReactNode } from 'react'
 import { Link as RouterLink } from 'react-router-dom'
 import { useScreenSize } from '../../hooks/useScreensize'
 import Link from '@mui/material/Link'
-import { TrimLinkLabel } from '../TrimLinkLabel'
 import { RouteUtils } from '../../utils/route-utils'
+import InfoIcon from '@mui/icons-material/Info'
 import Typography from '@mui/material/Typography'
 import { SearchScope } from '../../../types/searchScope'
+import { trimLongString } from '../../utils/trimLongString'
+import { MaybeWithTooltip } from '../AdaptiveTrimmer/MaybeWithTooltip'
+import { AdaptiveTrimmer } from '../AdaptiveTrimmer/AdaptiveTrimmer'
+
+const WithTypographyAndLink: FC<{
+  to: string
+  mobile?: boolean
+  children: ReactNode
+}> = ({ children, to, mobile }) => {
+  return (
+    <Typography
+      variant="mono"
+      component="span"
+      sx={{
+        ...(mobile
+          ? {
+              maxWidth: '100%',
+              overflowX: 'hidden',
+            }
+          : {}),
+      }}
+    >
+      <Link component={RouterLink} to={to}>
+        {children}
+      </Link>
+    </Typography>
+  )
+}
 
 interface Props {
   scope: SearchScope
   address: string
+
+  /**
+   * Should we always trim the text to a short line?
+   */
   alwaysTrim?: boolean
+
+  /**
+   * Any extra tooltips to display
+   *
+   * (Besides the content necessary because of potential shortening)
+   */
+  extraTooltip?: ReactNode
 }
 
-export const AccountLink: FC<Props> = ({ scope, address, alwaysTrim }) => {
+export const AccountLink: FC<Props> = ({ scope, address, alwaysTrim, extraTooltip }) => {
   const { isTablet } = useScreenSize()
   const to = RouteUtils.getAccountRoute(scope, address)
+
+  const tooltipPostfix = extraTooltip ? (
+    <>
+      <InfoIcon />
+      {extraTooltip}
+    </>
+  ) : undefined
+
+  // Are we in a table?
+  if (alwaysTrim) {
+    // In a table, we only ever want a short line
+
+    return (
+      <WithTypographyAndLink to={to}>
+        <MaybeWithTooltip title={address}>{trimLongString(address, 6, 6)}</MaybeWithTooltip>
+      </WithTypographyAndLink>
+    )
+  }
+
+  if (!isTablet) {
+    // Details in desktop mode.
+    // We want one long line
+
+    return (
+      <WithTypographyAndLink to={to}>
+        <MaybeWithTooltip title={tooltipPostfix}>{address} </MaybeWithTooltip>
+      </WithTypographyAndLink>
+    )
+  }
+
+  // We need to show the data in details mode on mobile.
+  // Line adaptively shortened to fill available space
   return (
-    <Typography variant="mono" component="span">
-      {alwaysTrim || isTablet ? (
-        <TrimLinkLabel label={address} to={to} />
-      ) : (
-        <Link component={RouterLink} to={to}>
-          {address}
-        </Link>
-      )}
-    </Typography>
+    <WithTypographyAndLink to={to} mobile>
+      <AdaptiveTrimmer text={address} strategy="middle" extraTooltip={extraTooltip} />
+    </WithTypographyAndLink>
   )
 }
