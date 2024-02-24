dd6fc7ad0116d7c7359c813c8db77efe5c4b7921
Kristof Csillag <kristof@oasisprotocol.org>
2024-02-24 11:24:29 +0100

    Improve phrasing of error messages on reverted TXs

--- src/app/hooks/useTxErrorMessage.ts
+++ src/app/hooks/useTxErrorMessage.ts
@@ -3,5 +3,12 @@ import { TxError } from '../../oasis-nexus/api'
 
 export const useTxErrorMessage = (error: TxError | undefined): string | undefined => {
   const { t } = useTranslation()
-  return error ? `${error.message} (${t('errors.code')} ${error.code})` : undefined
+  if (!error) return undefined
+  if (error.module === 'evm' && error.code === 8 && !error.message) {
+    // EVM reverted, with missing error message
+    return `${t('errors.revertedWithoutMessage')} (${t('errors.code')} ${error.code})`
+  } else {
+    // Anything else
+    return `${error.message} (${t('errors.code')} ${error.code})`
+  }
 }
