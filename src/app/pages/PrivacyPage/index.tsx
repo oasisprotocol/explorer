d184612b4ce61fc8c24d93b7ddebf309e71e5eb1
Kristof Csillag <kristof@oasisprotocol.org>
2024-02-28 12:55:32 +0100

    Add PontusX Privacy Policy

--- src/app/pages/PrivacyPage/index.tsx
+++ src/app/pages/PrivacyPage/index.tsx
@@ -4,13 +4,15 @@ import { useTranslation } from 'react-i18next'
 import { PageLayout } from '../../components/PageLayout'
 import { SubPageCard } from '../../components/SubPageCard'
 
+import Policy from '../../../docs/pontusx_privacy_2024.mdx'
+
 export const PrivacyPage: FC = () => {
   const { t } = useTranslation()
 
   return (
     <PageLayout>
       <SubPageCard featured title={t('privacyPolicy.title')}>
-        <span>{t('common.missing')}</span>
+        <Policy />
       </SubPageCard>
     </PageLayout>
   )
