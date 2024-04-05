05b290febd6ad5af73e81e7ff43c3d9a9a46ec58
Matej Lubej <matej.lubej@oasisprotocol.org>
2024-04-05 21:42:40 +0200

    Remove Tooltip from withAddressSwitch

--- src/app/components/Account/withAddressSwitch.tsx
+++ src/app/components/Account/withAddressSwitch.tsx
@@ -1,9 +1,6 @@
 import { FC } from 'react'
 import { AddressSwitchOption } from '../AddressSwitch'
 import { CopyToClipboard } from '../CopyToClipboard'
-import Tooltip from '@mui/material/Tooltip'
-import Box from '@mui/material/Box'
-import InfoIcon from '@mui/icons-material/Info'
 
 interface WithAddressSwitchProps {
   addressSwitchOption: AddressSwitchOption
@@ -24,30 +21,11 @@ export const withAddressSwitch =
       [AddressSwitchOption.Oasis]: oasisAddress,
       [AddressSwitchOption.ETH]: ethAddress,
     }
-    const hasAddressOfExpectedType = !!addressesByType[addressSwitchOption]
     const displayedAddress = addressesByType[addressSwitchOption] ?? ethAddress ?? oasisAddress
 
     return (
       <>
-        <Tooltip
-          arrow
-          placement="top"
-          title={
-            <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
-              <InfoIcon />
-              {/* TODO: Replace with translations, text to be decided?, should be probably an input to this generic withAddressSwitch */}
-              {hasAddressOfExpectedType
-                ? oasisAddress
-                  ? 'Oasis address'
-                  : 'Ethereum address'
-                : 'Address not available in the expected type'}
-            </Box>
-          }
-        >
-          <Box>
-            <Component {...(restProps as unknown as T)} address={displayedAddress} />
-          </Box>
-        </Tooltip>
+        <Component {...(restProps as unknown as T)} address={displayedAddress} />
         {displayedAddress && <CopyToClipboard value={displayedAddress} />}
       </>
     )
