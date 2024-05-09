c8fbf1bd4f89e32866f15332a176fc6a672175f2
lukaw3d <lukajeran@oasislabs.com>
2024-05-09 03:57:15 +0200

    speedup tsc 10.28s -> 8.55s.

--- src/styles/theme/testnet/theme.ts
+++ src/styles/theme/testnet/theme.ts
@@ -1,31 +1,29 @@
-import { deepmerge } from '@mui/utils'
-import { createTheme } from '@mui/material/styles'
+import { ThemeOptions, createTheme } from '@mui/material/styles'
 import { COLORS } from './colors'
 import { defaultTheme } from '../defaultTheme'
 
-export const testnetTheme = createTheme(
-  deepmerge(defaultTheme, {
-    palette: {
-      background: {
-        default: COLORS.testnetLight,
-        empty: COLORS.white,
-      },
-      layout: {
-        main: COLORS.brandExtraDark,
-        border: COLORS.testnet,
-        contrastMain: COLORS.brandExtraDark,
-        contrastSecondary: COLORS.testnet,
-        darkBorder: COLORS.testnet,
-        hoverBorder: COLORS.testnet,
-        lightBorder: COLORS.testnet,
-        secondary: COLORS.testnet,
-        primaryBackground: COLORS.testnetLight,
-        secondaryBackground: COLORS.white,
-        networkBubbleBorder: COLORS.testnet,
-        titleOnBackground: COLORS.grayExtraDark,
-        graphZoomOutText: COLORS.brandDark,
-        helpScreenIconColor: COLORS.brandExtraDark,
-      },
+// Assigning `{}` first speeds up typescript compilation a lot.
+export const testnetTheme = createTheme({}, defaultTheme, {
+  palette: {
+    background: {
+      default: COLORS.testnetLight,
+      empty: COLORS.white,
     },
-  }),
-)
+    layout: {
+      main: COLORS.brandExtraDark,
+      border: COLORS.testnet,
+      contrastMain: COLORS.brandExtraDark,
+      contrastSecondary: COLORS.testnet,
+      darkBorder: COLORS.testnet,
+      hoverBorder: COLORS.testnet,
+      lightBorder: COLORS.testnet,
+      secondary: COLORS.testnet,
+      primaryBackground: COLORS.testnetLight,
+      secondaryBackground: COLORS.white,
+      networkBubbleBorder: COLORS.testnet,
+      titleOnBackground: COLORS.grayExtraDark,
+      graphZoomOutText: COLORS.brandDark,
+      helpScreenIconColor: COLORS.brandExtraDark,
+    },
+  },
+} satisfies ThemeOptions)
