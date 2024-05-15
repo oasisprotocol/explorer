ad881077ddcffea7973784f5cbdbe6d91c080ceb
Kristof Csillag <csillag@nolmecolindor.com>
2024-05-15 02:14:20 +0200

    CardHeaderWithResponsiveActions: tune how things wrap on mobile/tabled

--- src/app/components/CardHeaderWithResponsiveActions/index.tsx
+++ src/app/components/CardHeaderWithResponsiveActions/index.tsx
@@ -2,10 +2,16 @@ import CardHeader, { cardHeaderClasses } from '@mui/material/CardHeader'
 import { styled } from '@mui/material/styles'
 
 export const CardHeaderWithResponsiveActions = styled(CardHeader)(({ theme }) => ({
-  [theme.breakpoints.down('sm')]: {
+  [theme.breakpoints.down('md')]: {
+    display: 'inline',
     alignItems: 'flex-start',
     flexDirection: 'column',
+    [`.${cardHeaderClasses.content}`]: {
+      display: 'inline',
+      marginRight: theme.spacing(4),
+    },
     [`.${cardHeaderClasses.action}`]: {
+      display: 'inline',
       marginTop: theme.spacing(4),
       marginBottom: theme.spacing(4),
     },
