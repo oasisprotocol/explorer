71a6cc507264b2105d5d52e59c88aa3e43aeea6a
Michal Zielenkiewicz <michal@oasisprotocol.org>
2024-08-15 10:13:57 +0200

    Tweak ExternalLink component

--- src/app/pages/ValidatorDetailsPage/ValidatorSnapshot.tsx
+++ src/app/pages/ValidatorDetailsPage/ValidatorSnapshot.tsx
@@ -3,8 +3,7 @@ import Box from '@mui/material/Box'
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
 import { useScreenSize } from '../../hooks/useScreensize'
-import { styled, useTheme } from '@mui/material/styles'
-import OpenInNewIcon from '@mui/icons-material/OpenInNew'
+import { useTheme } from '@mui/material/styles'
 import { useTranslation } from 'react-i18next'
 import { AppendMobileSearch } from '../../components/AppendMobileSearch'
 import { ExternalLinkCard } from './ExternalLinkCard'
@@ -14,16 +13,6 @@ import { VotingPowerCard } from './VotingPowerCard'
 import { BalanceDistributionCard } from './BalanceDistributionCard'
 import { Validator } from '../../../oasis-nexus/api'
 import { StyledGrid } from '../../components/Snapshots/Snapshot'
-import { getHostname } from '../../utils/url'
-
-const StyledTypography = styled(Typography)(() => ({
-  fontSize: 18,
-  fontWeight: 500,
-  textTransform: 'lowercase',
-  '::first-letter': {
-    textTransform: 'uppercase',
-  },
-}))
 
 type ValidatorSnapshotProps = {
   scope: SearchScope
@@ -61,15 +50,7 @@ export const ValidatorSnapshot: FC<ValidatorSnapshotProps> = ({ scope, validator
           <UptimeCard />
         </StyledGrid>
         <StyledGrid item xs={22} md={5}>
-          <ExternalLinkCard
-            label={
-              <Box gap={2} sx={{ display: 'flex', alignItems: 'center' }}>
-                <StyledTypography>{getHostname(validator?.media?.url)}</StyledTypography>
-                <OpenInNewIcon sx={{ fontSize: 20 }} />
-              </Box>
-            }
-            link={validator?.media?.url}
-          />
+          <ExternalLinkCard link={validator?.media?.url} />
         </StyledGrid>
       </Grid>
     </>
