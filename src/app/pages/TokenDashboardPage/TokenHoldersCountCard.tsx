f9a55e9e3cf14149581d7b071bc62793449c9a6a
Michal Zielenkiewicz <michal@oasisprotocol.org>
2024-08-21 10:54:17 +0200

    Clean up token snapshot cards

--- src/app/pages/TokenDashboardPage/TokenHoldersCountCard.tsx
+++ src/app/pages/TokenDashboardPage/TokenHoldersCountCard.tsx
@@ -1,12 +1,9 @@
 import { FC } from 'react'
 import { useTranslation } from 'react-i18next'
-import Box from '@mui/material/Box'
-import Typography from '@mui/material/Typography'
-import { SnapshotCard } from '../../components/Snapshots/SnapshotCard'
-import { COLORS } from '../../../styles/theme/colors'
-import { useTokenInfo } from './hook'
 import Skeleton from '@mui/material/Skeleton'
+import { SnapshotTextCard } from '../../components/Snapshots/SnapshotCard'
 import { SearchScope } from '../../../types/searchScope'
+import { useTokenInfo } from './hook'
 
 export const TokenHoldersCountCard: FC<{ scope: SearchScope; address: string }> = ({ scope, address }) => {
   const { t } = useTranslation()
@@ -15,25 +12,12 @@ export const TokenHoldersCountCard: FC<{ scope: SearchScope; address: string }> 
 
   const title = t('tokens.holders')
   return (
-    <SnapshotCard title={title} withConstantHeight>
-      <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%' }}>
-        {isLoading ? (
-          <Skeleton variant="text" sx={{ width: '50%' }} />
-        ) : (
-          isFetched && (
-            <Typography
-              component="span"
-              sx={{
-                fontSize: '32px',
-                fontWeight: 700,
-                color: COLORS.brandDark,
-              }}
-            >
-              {t('tokens.holdersValue', { value: token?.num_holders })}
-            </Typography>
-          )
-        )}
-      </Box>
-    </SnapshotCard>
+    <SnapshotTextCard title={title} withConstantHeight>
+      {isLoading ? (
+        <Skeleton variant="text" />
+      ) : (
+        isFetched && <>{t('tokens.holdersValue', { value: token?.num_holders })}</>
+      )}
+    </SnapshotTextCard>
   )
 }
