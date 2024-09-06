e93296b839dcd2f896f315b58fb58950e3109598
Michal Zielenkiewicz <michal@oasisprotocol.org>
2024-09-06 10:30:07 +0200

    Make useWantedTransaction work with both tx types

--- src/app/hooks/useWantedTransaction.ts
+++ src/app/hooks/useWantedTransaction.ts
@@ -1,18 +1,25 @@
-import { RuntimeTransaction, RuntimeTransactionList } from '../../oasis-nexus/api'
+import {
+  RuntimeTransaction,
+  RuntimeTransactionList,
+  Transaction,
+  TransactionList,
+} from '../../oasis-nexus/api'
 
-type TransactionSelectionResult = {
-  wantedTransaction?: RuntimeTransaction
+type TransactionSelectionResult<T> = {
+  wantedTransaction?: T
   warningMultipleTransactionsSameHash?: boolean
 }
 
-/**
- * Find the wanted transaction, in case there are more.
- *
- * Normally we want the successful one. If there is none, then the latest.
- */
+export function useWantedTransaction(
+  transactionsList: TransactionList | undefined,
+): TransactionSelectionResult<Transaction>
 export function useWantedTransaction(
   transactionsList: RuntimeTransactionList | undefined,
-): TransactionSelectionResult {
+): TransactionSelectionResult<RuntimeTransaction>
+
+export function useWantedTransaction(
+  transactionsList: TransactionList | RuntimeTransactionList | undefined,
+): TransactionSelectionResult<Transaction | RuntimeTransaction> {
   const transactions = transactionsList?.transactions ?? []
 
   if (!transactions.length) {
@@ -24,7 +31,16 @@ export function useWantedTransaction(
     }
   } else {
     const successfulOne = transactions.find(transaction => transaction.success)
-    const latestOne = transactions.sort((a, b) => b.round - a.round)[0]
+    const latestOne = transactions.sort((a, b) => {
+      if ('round' in a && 'round' in b) {
+        return b.round - a.round // RuntimeTransaction
+      } else if ('block' in a && 'block' in b) {
+        return b.block - a.block //  Transaction
+      } else {
+        return 0
+      }
+    })[0]
+
     return {
       warningMultipleTransactionsSameHash: true,
       wantedTransaction: successfulOne ?? latestOne,
