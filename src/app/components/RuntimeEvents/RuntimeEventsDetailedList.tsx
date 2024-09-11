36dc1693d069480148a604fb3400bccae0f15f55
lukaw3d <lukajeran@oasislabs.com>
2024-09-11 17:04:19 +0200

    Inline event divider

--- src/app/components/RuntimeEvents/RuntimeEventsDetailedList.tsx
+++ src/app/components/RuntimeEvents/RuntimeEventsDetailedList.tsx
@@ -10,20 +10,6 @@ import { RuntimeEventDetails } from './RuntimeEventDetails'
 import Box from '@mui/material/Box'
 import Divider from '@mui/material/Divider'
 
-const RuntimeEventDetailsWithSeparator: FC<{
-  scope: SearchScope
-  event: RuntimeEvent
-  isFirst: boolean
-  addressSwitchOption: AddressSwitchOption
-}> = ({ scope, event, isFirst, addressSwitchOption }) => {
-  return (
-    <>
-      {!isFirst && <Divider variant="card" />}
-      <RuntimeEventDetails scope={scope} event={event} addressSwitchOption={addressSwitchOption} />
-    </>
-  )
-}
-
 export const RuntimeEventsDetailedList: FC<{
   scope: SearchScope
   events: RuntimeEvent[] | undefined
@@ -39,13 +25,10 @@ export const RuntimeEventsDetailedList: FC<{
       {isLoading && <TextSkeleton numberOfRows={10} />}
       {events &&
         events.map((event, index) => (
-          <RuntimeEventDetailsWithSeparator
-            key={`event-${index}`}
-            scope={scope}
-            isFirst={!index}
-            event={event}
-            addressSwitchOption={addressSwitchOption}
-          />
+          <div key={`event-${index}`}>
+            {index > 0 && <Divider variant="card" />}
+            <RuntimeEventDetails scope={scope} event={event} addressSwitchOption={addressSwitchOption} />
+          </div>
         ))}
       {pagination && (
         <Box sx={{ display: 'flex', justifyContent: 'center' }}>
