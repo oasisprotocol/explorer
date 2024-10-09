d8702bdb3869b4c04e9abd6268a79ae7ae104149
Michal Zielenkiewicz <michal@oasisprotocol.org>
2024-10-09 17:46:24 +0200

    Enable ProposedBlocks in validator details page

--- src/app/pages/ValidatorDetailsPage/ProposedBlocks.tsx
+++ src/app/pages/ValidatorDetailsPage/ProposedBlocks.tsx
@@ -4,14 +4,47 @@ import Card from '@mui/material/Card'
 import CardHeader from '@mui/material/CardHeader'
 import CardContent from '@mui/material/CardContent'
 import { SearchScope } from 'types/searchScope'
+import { useGetConsensusBlocks, Validator } from '../../../oasis-nexus/api'
+import { NUMBER_OF_ITEMS_ON_DASHBOARD as PAGE_SIZE } from '../../config'
+import { useSearchParamsPagination } from '../../components/Table/useSearchParamsPagination'
+import { ConsensusBlocks } from '../../components/Blocks'
 
-export const ProposedBlocks: FC<{ scope: SearchScope }> = ({ scope }) => {
+type ProposedBlocksProps = {
+  scope: SearchScope
+  validator: Validator | undefined
+}
+
+export const ProposedBlocks: FC<ProposedBlocksProps> = ({ scope, validator }) => {
   const { t } = useTranslation()
+  const pagination = useSearchParamsPagination('proposedBlocks')
+  const offset = (pagination.selectedPage - 1) * PAGE_SIZE
+  const blocksQuery = useGetConsensusBlocks(scope.network, {
+    limit: PAGE_SIZE,
+    offset,
+    proposed_by: validator?.entity_address,
+  })
+
+  const { isLoading, data } = blocksQuery
+  const blocks = data?.data.blocks
 
   return (
     <Card>
       <CardHeader disableTypography component="h3" title={t('validator.proposedBlocks')} />
-      <CardContent>{/* TODO: add proposed block paginated list when API is ready */}</CardContent>
+      <CardContent>
+        <ConsensusBlocks
+          isLoading={isLoading}
+          blocks={blocks}
+          limit={PAGE_SIZE}
+          pagination={{
+            selectedPage: pagination.selectedPage,
+            linkToPage: pagination.linkToPage,
+            totalCount: data?.data.total_count,
+            isTotalCountClipped: data?.data.is_total_count_clipped,
+            rowsPerPage: PAGE_SIZE,
+          }}
+          showEpoch
+        />
+      </CardContent>
     </Card>
   )
 }
