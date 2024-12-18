445bc26415a3a58ccc0b255f0cc27f057c1a3f1a
Michal Zielenkiewicz <michal@oasisprotocol.org>
2024-12-18 16:10:40 +0100

    Improve Consensus ParaTime cards for testnet

--- src/app/pages/ConsensusDashboardPage/ParaTimesCard.tsx
+++ src/app/pages/ConsensusDashboardPage/ParaTimesCard.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react'
 import { useTranslation } from 'react-i18next'
+import Box from '@mui/material/Box'
 import Card from '@mui/material/Card'
 import CardHeader from '@mui/material/CardHeader'
 import CardContent from '@mui/material/CardContent'
@@ -21,6 +22,18 @@ const StyledInnerGrid = styled(Grid)(({ theme }) => ({
   },
 }))
 
+const StyledBox = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  [theme.breakpoints.down('md')]: {
+    flexDirection: 'column',
+    gap: theme.spacing(4),
+  },
+  [theme.breakpoints.up('md')]: {
+    flexDirection: 'row',
+    gap: theme.spacing(5),
+  },
+}))
+
 type ParaTimesCardProps = { scope: SearchScope }
 
 export const ParaTimesCard: FC<ParaTimesCardProps> = ({ scope }) => {
@@ -31,6 +44,8 @@ export const ParaTimesCard: FC<ParaTimesCardProps> = ({ scope }) => {
   const disabledRuntimes = disabled.filter(shouldIncludeLayer) as Runtime[]
   const runtimesNumber = enabledRuntimes.length + disabledRuntimes.length
   const [firstEnabledRuntime, ...restEnabledRuntimes] = enabledRuntimes
+  const spaceForSecondaryParaTimes = enabledRuntimes.length > 2
+
   return (
     <Card>
       <CardHeader
@@ -46,17 +61,19 @@ export const ParaTimesCard: FC<ParaTimesCardProps> = ({ scope }) => {
       />
       <CardContent>
         <Grid container spacing={5}>
-          <Grid xs={12} md={6}>
+          <Grid xs={12} md={spaceForSecondaryParaTimes ? 5 : 6}>
             <EnabledRuntimePreview prominentItem network={scope.network} runtime={firstEnabledRuntime} />
           </Grid>
-          <Grid xs={12} md={6} container>
-            <StyledInnerGrid xs={12} md={8}>
-              {!!restEnabledRuntimes.length &&
-                restEnabledRuntimes.map(runtime => (
-                  <EnabledRuntimePreview key={runtime} network={scope.network} runtime={runtime} />
-                ))}
+          <Grid xs={12} md={spaceForSecondaryParaTimes ? 7 : 6} container>
+            <StyledInnerGrid xs={12} md={spaceForSecondaryParaTimes ? 9 : 8}>
+              <StyledBox>
+                {!!restEnabledRuntimes.length &&
+                  restEnabledRuntimes.map(runtime => (
+                    <EnabledRuntimePreview key={runtime} network={scope.network} runtime={runtime} />
+                  ))}
+              </StyledBox>
             </StyledInnerGrid>
-            <StyledInnerGrid xs={12} md={4}>
+            <StyledInnerGrid xs={12} md={spaceForSecondaryParaTimes ? 3 : 4}>
               {!!disabledRuntimes.length &&
                 disabledRuntimes.map(runtime => <DisabledRuntimePreview key={runtime} runtime={runtime} />)}
             </StyledInnerGrid>
