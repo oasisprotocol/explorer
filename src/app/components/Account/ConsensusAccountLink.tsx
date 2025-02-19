f049a3562b9dcc91d57f756df6acfd53a24b846d
Michal Zielenkiewicz <michal@oasisprotocol.org>
2025-02-19 11:38:30 +0100

    Rely on validator address presence not name in ConsensusAccountLink

--- src/app/components/Account/ConsensusAccountLink.tsx
+++ src/app/components/Account/ConsensusAccountLink.tsx
@@ -21,7 +21,7 @@ export const ConsensusAccountLink: FC<ConsensusAccountLinkProps> = ({
 }) => {
   const { data } = useGetConsensusValidatorsAddressNameMap(network)
 
-  if (data?.data?.[address]) {
+  if (data?.data && address in data.data) {
     return (
       <ValidatorLink
         address={address}
