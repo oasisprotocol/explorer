86481291c879e9fe5af31c0f338231e2c9120d43
Michal Zielenkiewicz <michal@oasisprotocol.org>
2025-03-04 11:12:29 +0100

    Show metadata and policy rows

--- src/app/pages/RoflAppDetailPage/MetaDataCard.tsx
+++ src/app/pages/RoflAppDetailPage/MetaDataCard.tsx
@@ -1,10 +1,22 @@
 import { FC } from 'react'
 import { useTranslation } from 'react-i18next'
+import { styled } from '@mui/material/styles'
+import Box from '@mui/material/Box'
 import Card from '@mui/material/Card'
 import CardHeader from '@mui/material/CardHeader'
 import CardContent from '@mui/material/CardContent'
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
 import { RoflAppMetadata } from '../../../oasis-nexus/api'
+import { COLORS } from '../../../styles/theme/colors'
 import { EmptyStateCard } from './EmptyStateCard'
+import { GridRow } from './GridRow'
+
+export const StyledGrid = styled(Grid)(({ theme }) => ({
+  borderBottom: 'solid 1px #F4F5F7',
+  paddingTop: theme.spacing(3),
+  paddingBottom: theme.spacing(3),
+}))
 
 type MetaDataCardProps = {
   isFetched: boolean
@@ -16,8 +28,33 @@ export const MetaDataCard: FC<MetaDataCardProps> = ({ isFetched, metadata }) => 
 
   return (
     <Card sx={{ flex: 1 }}>
-      <CardHeader disableTypography component="h3" title={t('rofl.metadata')} />
-      <CardContent>{isFetched && !metadata && <EmptyStateCard />}</CardContent>
+      <CardHeader
+        titleTypographyProps={{ variant: 'h1' }}
+        disableTypography
+        component="h3"
+        title={
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 5 }}>
+            <Box sx={{ flex: 1, whiteSpace: 'nowrap' }}>{t('rofl.metadata')}</Box>
+            <Typography component="span" sx={{ fontSize: '12px', color: COLORS.grayDark }}>
+              {t('rofl.metadataInfo')}
+            </Typography>
+          </Box>
+        }
+      />
+      <CardContent>
+        {isFetched && !metadata && <EmptyStateCard />}
+        {metadata && (
+          <>
+            <Grid container spacing={4}>
+              <GridRow label={t('rofl.roflName')}>{metadata.name}</GridRow>
+              <GridRow label={t('rofl.description')}>{metadata.description}</GridRow>
+              <GridRow label={t('rofl.author')}>{metadata.author}</GridRow>
+              <GridRow label={t('rofl.license')}>{metadata.licence}</GridRow>
+              <GridRow label={t('rofl.gitrev')}>{metadata.git}</GridRow>
+            </Grid>
+          </>
+        )}
+      </CardContent>
     </Card>
   )
 }
--- src/app/pages/RoflAppDetailPage/PolicyCard.tsx
+++ src/app/pages/RoflAppDetailPage/PolicyCard.tsx
@@ -3,8 +3,10 @@ import { useTranslation } from 'react-i18next'
 import Card from '@mui/material/Card'
 import CardHeader from '@mui/material/CardHeader'
 import CardContent from '@mui/material/CardContent'
+import Grid from '@mui/material/Grid'
 import { RoflAppPolicy } from '../../../oasis-nexus/api'
 import { EmptyStateCard } from './EmptyStateCard'
+import { GridRow } from './GridRow'
 
 type PolicyCardProps = {
   isFetched: boolean
@@ -17,7 +19,20 @@ export const PolicyCard: FC<PolicyCardProps> = ({ isFetched, policy }) => {
   return (
     <Card sx={{ flex: 1 }}>
       <CardHeader disableTypography component="h3" title={t('rofl.policy')} />
-      <CardContent>{isFetched && !policy && <EmptyStateCard />}</CardContent>
+      <CardContent>
+        {isFetched && !policy && <EmptyStateCard />}
+        {policy && (
+          <>
+            <Grid container spacing={4}>
+              <GridRow label={t('rofl.validity')}>{policy.period}</GridRow>
+              <GridRow label={t('rofl.evaluation')}>{policy.evaluation}</GridRow>
+              <GridRow label={t('rofl.feePolicy')}>{policy.fee}</GridRow>
+              <GridRow label={t('rofl.update')}>{policy.update}</GridRow>
+              <GridRow label={t('rofl.repositoryUrl')}>{policy.github}</GridRow>
+            </Grid>
+          </>
+        )}
+      </CardContent>
     </Card>
   )
 }
