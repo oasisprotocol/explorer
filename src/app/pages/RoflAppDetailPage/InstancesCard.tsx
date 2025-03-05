95a97cf91fc3a9068a5c6de4d1c6414463baea86
Michal Zielenkiewicz <michal@oasisprotocol.org>
2025-03-05 15:55:21 +0100

    Prepare instances list view

--- src/app/pages/RoflAppDetailPage/InstancesCard.tsx
+++ src/app/pages/RoflAppDetailPage/InstancesCard.tsx
@@ -1,11 +1,36 @@
 import { FC } from 'react'
+import { useTranslation } from 'react-i18next'
 import { LinkableCardLayout } from '../../components/LinkableCardLayout'
 import { instancesContainerId } from '../../utils/tabAnchors'
+import { CardEmptyState } from 'app/components/CardEmptyState'
+import { NUMBER_OF_ITEMS_ON_SEPARATE_PAGE } from 'app/config'
+import { InstancesList } from './InstancesList'
 
 export const InstancesCard: FC = () => {
   return (
     <LinkableCardLayout containerId={instancesContainerId} title="">
-      {' '}
+      <InstancesView />
     </LinkableCardLayout>
+  )
+}
+
+const InstancesView: FC = () => {
+  const { t } = useTranslation()
+
+  // TODO: waits for API, add pagination
+  const isLoading = false
+  const isFetched = true
+  const instances = []
+
+  return (
+    <>
+      {isFetched && !instances?.length && <CardEmptyState label={t('rofl.emptyInstancesList')} />}
+      <InstancesList
+        instances={instances}
+        isLoading={isLoading}
+        limit={NUMBER_OF_ITEMS_ON_SEPARATE_PAGE}
+        pagination={false}
+      />
+    </>
   )
 }
