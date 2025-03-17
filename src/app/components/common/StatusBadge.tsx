d534832d542dbf505cb731d7cda0256dda396d02
Michal Zielenkiewicz <michal@oasisprotocol.org>
2025-03-17 10:09:22 +0100

    Simplify StatusBadge variant styles

--- src/app/components/common/StatusBadge.tsx
+++ src/app/components/common/StatusBadge.tsx
@@ -9,22 +9,18 @@ import ErrorIcon from '@mui/icons-material/Error'
 
 export type StatusVariant = 'success' | 'warning' | 'danger' | 'info'
 
-const variantStyles: Record<StatusVariant, { bgColor: string; fgColor: string }> = {
+const variantStyles: Record<StatusVariant, { bgColor: string }> = {
   success: {
     bgColor: COLORS.honeydew,
-    fgColor: COLORS.brandExtraDark,
   },
   warning: {
     bgColor: COLORS.warningBackground,
-    fgColor: COLORS.brandExtraDark,
   },
   danger: {
     bgColor: COLORS.linen,
-    fgColor: COLORS.brandExtraDark,
   },
   info: {
     bgColor: COLORS.brandLightBlue,
-    fgColor: COLORS.brandExtraDark,
   },
 }
 
@@ -35,17 +31,15 @@ const variantIcon: Record<StatusVariant, ReactNode> = {
   info: <InfoIcon color="info" fontSize="small" />,
 }
 
-export type StatusBadgeProps = {
+type StatusBadgeProps = {
   label: string
   icon?: ReactNode
   variant?: StatusVariant
-  bgColor?: string
-  fgColor?: string
 }
 
 const StyledBadge = styled(Box, {
-  shouldForwardProp: prop => !['bgColor', 'fgColor'].includes(prop as string),
-})<{ bgColor: string; fgColor: string }>(({ bgColor, fgColor }) => {
+  shouldForwardProp: prop => prop !== 'bgColor',
+})<{ bgColor: string }>(({ bgColor }) => {
   return {
     display: 'inline-flex',
     gap: 8,
@@ -54,7 +48,7 @@ const StyledBadge = styled(Box, {
     height: 28,
     fontSize: '12px',
     backgroundColor: bgColor,
-    color: fgColor,
+    color: COLORS.brandExtraDark,
     borderRadius: 10,
     padding: 4,
     paddingLeft: 10,
@@ -64,7 +58,7 @@ const StyledBadge = styled(Box, {
 
 export const StatusBadge = ({ label, icon, variant = 'info' }: StatusBadgeProps) => {
   return (
-    <StyledBadge bgColor={variantStyles[variant].bgColor} fgColor={variantStyles[variant].fgColor}>
+    <StyledBadge bgColor={variantStyles[variant].bgColor}>
       {label}
       {icon || variantIcon[variant]}
     </StyledBadge>
