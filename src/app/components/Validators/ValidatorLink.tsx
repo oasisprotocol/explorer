61a1dab29129597c59577ed695e4705c7887e92d
Michal Zielenkiewicz <michal@oasisprotocol.org>
2025-03-19 10:29:59 +0100

    Create new ValidatorLink component

---
+++ src/app/components/Validators/ValidatorLink.tsx
@@ -0,0 +1,35 @@
+import { FC } from 'react'
+import { RouteUtils } from '../../utils/route-utils'
+import { Network } from '../../../types/network'
+import { useValidatorName } from '../../hooks/useValidatorName'
+import { Link } from '../../components/Link'
+
+type ValidatorLinkProps = {
+  address: string
+  alwaysTrim?: boolean
+  highlightedPartOfName?: string
+  name?: string
+  network: Network
+}
+
+export const ValidatorLink: FC<ValidatorLinkProps> = ({
+  address,
+  alwaysTrim,
+  highlightedPartOfName,
+  name,
+  network,
+}) => {
+  const to = RouteUtils.getValidatorRoute(network, address)
+  const validatorName = useValidatorName(network, address)
+  const displayName = name ?? validatorName
+
+  return (
+    <Link
+      address={address}
+      alwaysTrim={alwaysTrim}
+      highlightedPartOfName={highlightedPartOfName}
+      name={displayName}
+      to={to}
+    />
+  )
+}
