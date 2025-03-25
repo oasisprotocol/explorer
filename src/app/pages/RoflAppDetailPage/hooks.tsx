73b32b80fca39e86293129247b95314734b3f2c6
Michal Zielenkiewicz <michal@oasisprotocol.org>
2025-03-25 11:39:15 +0100

    Create InstancesCard component for details page

---
+++ src/app/pages/RoflAppDetailPage/hooks.tsx
@@ -0,0 +1,8 @@
+import { SearchScope } from '../../../types/searchScope'
+
+export type RoflAppDetailsContext = {
+  scope: SearchScope
+  id: string
+}
+
+// TOOD: Placeholder file for a hook used within a router. Add when details page is ready
