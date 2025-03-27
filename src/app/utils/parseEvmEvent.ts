2dad3b9f0c85d89fb8bab697c50d67142061abac
lukaw3d <lukajeran@oasislabs.com>
2025-03-27 21:46:42 +0100

    Fix detecting burning events
    This transaction has a Swap with param `To: 0x0000`
    0xc78d5387868fd4f1991a35a39eff51366e2ba45e38c563c5c3f6724ba378d7a6

--- src/app/utils/parseEvmEvent.ts
+++ src/app/utils/parseEvmEvent.ts
@@ -5,8 +5,8 @@ const NULL_ADDRESS = '0x0000000000000000000000000000000000000000'
 export function parseEvmEvent(event: RuntimeEvent) {
   const fromAddress = event.evm_log_params?.find(param => param.name === 'from')?.value as string | undefined
   const toAddress = event.evm_log_params?.find(param => param.name === 'to')?.value as string | undefined
-  const isMinting = fromAddress === NULL_ADDRESS
-  const isBurning = toAddress === NULL_ADDRESS
+  const isMinting = event.evm_log_name === 'Transfer' && fromAddress === NULL_ADDRESS
+  const isBurning = event.evm_log_name === 'Transfer' && toAddress === NULL_ADDRESS
   const parsedEvmLogName = isMinting ? 'Minting' : isBurning ? 'Burning' : event.evm_log_name || undefined
 
   return {
