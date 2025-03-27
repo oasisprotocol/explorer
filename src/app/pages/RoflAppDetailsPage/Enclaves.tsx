da5dd67fc3732be705a58ab73dae2ee56fe994eb
Michal Zielenkiewicz <michal@oasisprotocol.org>
2025-03-27 11:32:37 +0100

    Prepare component to render ROFL enclaves

---
+++ src/app/pages/RoflAppDetailsPage/Enclaves.tsx
@@ -0,0 +1,34 @@
+import { FC } from 'react'
+import { useTranslation } from 'react-i18next'
+import Typography from '@mui/material/Typography'
+import { RoflAppPolicy } from '../../../oasis-nexus/api'
+import { CopyToClipboard } from 'app/components/CopyToClipboard'
+
+type EnclavesProps = {
+  policy: RoflAppPolicy
+}
+
+export const Enclaves: FC<EnclavesProps> = ({ policy }) => {
+  const { t } = useTranslation()
+
+  if (!policy.enclaves || policy.enclaves.length === 0) {
+    return <>{t('common.missing')}</>
+  }
+
+  return (
+    <table>
+      {policy.enclaves.map((enclave: string) => (
+        <tr key={enclave}>
+          <td>
+            <Typography variant="mono" component="span">
+              {enclave}
+            </Typography>
+          </td>
+          <td>
+            <CopyToClipboard value={enclave} />
+          </td>
+        </tr>
+      ))}
+    </table>
+  )
+}
