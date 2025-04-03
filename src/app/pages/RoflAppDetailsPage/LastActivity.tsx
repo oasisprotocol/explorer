b228885436f188b09e838f0a986fc01fd2271a1e
Michal Zielenkiewicz <michal@oasisprotocol.org>
2025-04-03 09:02:40 +0200

    Improve last activity row on mobile

--- src/app/pages/RoflAppDetailsPage/LastActivity.tsx
+++ src/app/pages/RoflAppDetailsPage/LastActivity.tsx
@@ -2,9 +2,17 @@ import { FC } from 'react'
 import { useTranslation } from 'react-i18next'
 import { formatDistanceStrict } from 'date-fns'
 import Box from '@mui/material/Box'
+import { styled } from '@mui/material/styles'
 import { RuntimeTransaction } from '../../../oasis-nexus/api'
 import { SearchScope } from '../../../types/searchScope'
 import { TransactionLink } from '../../components/Transactions/TransactionLink'
+import { useScreenSize } from '../../hooks/useScreensize'
+
+const StyledBox = styled(Box)(() => ({
+  display: 'flex',
+  alignItems: 'center',
+  gap: 5,
+}))
 
 type LastActivityProps = {
   transaction: RuntimeTransaction | undefined
@@ -13,17 +21,25 @@ type LastActivityProps = {
 
 export const LastActivity: FC<LastActivityProps> = ({ scope, transaction }) => {
   const { t } = useTranslation()
+  const { isTablet } = useScreenSize()
 
   return (
     <>
       {transaction ? (
-        <Box sx={{ display: 'flex', alignItems: 'center', gap: 3 }}>
-          <TransactionLink scope={scope} hash={transaction.eth_hash ?? transaction.hash} />(
-          {formatDistanceStrict(transaction.timestamp, new Date(), {
-            addSuffix: true,
-          })}
-          )
-        </Box>
+        <StyledBox>
+          <TransactionLink
+            scope={scope}
+            hash={transaction.eth_hash ?? transaction.hash}
+            alwaysTrim={isTablet}
+          />
+          <span>
+            (
+            {formatDistanceStrict(transaction.timestamp, new Date(), {
+              addSuffix: true,
+            })}
+            )
+          </span>
+        </StyledBox>
       ) : (
         t('common.missing')
       )}
