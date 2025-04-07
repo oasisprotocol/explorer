8f84e296ff15016e9165a027dd96f804da01f6d3
Michal Zielenkiewicz <michal@oasisprotocol.org>
2025-04-07 10:09:32 +0200

    Add status column to ROFL instance list

--- src/app/pages/RoflAppDetailsPage/InstancesCard.tsx
+++ src/app/pages/RoflAppDetailsPage/InstancesCard.tsx
@@ -1,10 +1,8 @@
 import { FC } from 'react'
-import { useTranslation } from 'react-i18next'
-import { Layer, useGetRuntimeRoflAppsIdInstances } from '../../../oasis-nexus/api'
+import { Layer, useGetConsensusEpochs, useGetRuntimeRoflAppsIdInstances } from '../../../oasis-nexus/api'
 import { NUMBER_OF_ITEMS_ON_SEPARATE_PAGE as limit } from '../../config'
 import { instancesContainerId } from '../../utils/tabAnchors'
 import { LinkableCardLayout } from '../../components/LinkableCardLayout'
-import { CardEmptyState } from '../../components/CardEmptyState'
 import { useSearchParamsPagination } from '../../components/Table/useSearchParamsPagination'
 import { InstancesList } from './InstancesList'
 import { RoflAppDetailsContext } from './hooks'
@@ -18,7 +16,6 @@ export const InstancesCard: FC<RoflAppDetailsContext> = context => {
 }
 
 const InstancesView: FC<RoflAppDetailsContext> = ({ scope, id }) => {
-  const { t } = useTranslation()
   const { network } = scope
   const pagination = useSearchParamsPagination('page')
   const offset = (pagination.selectedPage - 1) * limit
@@ -26,24 +23,24 @@ const InstancesView: FC<RoflAppDetailsContext> = ({ scope, id }) => {
     limit,
     offset,
   })
-  const { isFetched, isLoading, data } = instancesQuery
+  const { isLoading, data } = instancesQuery
   const instances = data?.data.instances
+  const { isLoading: isEpochLoading, data: epochData } = useGetConsensusEpochs(scope.network, { limit: 1 })
+  const currentEpoch = epochData?.data.epochs[0].id
 
   return (
-    <>
-      {isFetched && !instances?.length && <CardEmptyState label={t('rofl.emptyInstancesList')} />}
-      <InstancesList
-        instances={instances}
-        isLoading={isLoading}
-        limit={limit}
-        pagination={{
-          selectedPage: pagination.selectedPage,
-          linkToPage: pagination.linkToPage,
-          totalCount: data?.data.total_count,
-          isTotalCountClipped: data?.data.is_total_count_clipped,
-          rowsPerPage: limit,
-        }}
-      />
-    </>
+    <InstancesList
+      instances={instances}
+      currentEpoch={currentEpoch}
+      isLoading={isLoading || isEpochLoading}
+      limit={limit}
+      pagination={{
+        selectedPage: pagination.selectedPage,
+        linkToPage: pagination.linkToPage,
+        totalCount: data?.data.total_count,
+        isTotalCountClipped: data?.data.is_total_count_clipped,
+        rowsPerPage: limit,
+      }}
+    />
   )
 }
--- src/app/pages/RoflAppDetailsPage/InstancesList.tsx
+++ src/app/pages/RoflAppDetailsPage/InstancesList.tsx
@@ -4,45 +4,63 @@ import Typography from '@mui/material/Typography'
 import { RoflInstance } from '../../../oasis-nexus/api'
 import { Table, TableCellAlign, TableColProps } from '../../components/Table'
 import { TablePaginationProps } from '../../components/Table/TablePagination'
+import { RoflAppInstanceStatusBadge } from 'app/components/Rofl/RoflAppInstanceStatusBadge'
 
 type InstancesListProps = {
+  currentEpoch: number | undefined
   instances: RoflInstance[] | undefined
   isLoading: boolean
   limit: number
   pagination: false | TablePaginationProps
 }
 
-export const InstancesList: FC<InstancesListProps> = ({ isLoading, limit, pagination, instances }) => {
+export const InstancesList: FC<InstancesListProps> = ({
+  isLoading,
+  limit,
+  pagination,
+  instances,
+  currentEpoch,
+}) => {
   const { t } = useTranslation()
   const tableColumns: TableColProps[] = [
     { key: 'rak', content: t('rofl.rakAbbreviation') },
     { key: 'node', content: t('rofl.nodeId') },
-    { key: 'expiration', content: t('rofl.expirationEpoch'), align: TableCellAlign.Right },
+    { key: 'expirationEpoch', content: t('rofl.expirationEpoch'), align: TableCellAlign.Right },
+    { key: 'expirationStatus', content: t('common.status'), align: TableCellAlign.Right },
   ]
 
-  const tableRows = instances?.map(instance => {
-    return {
-      key: instance.rak,
-      data: [
-        {
-          key: 'rak',
-          content: <Typography variant="mono">{instance.rak}</Typography>,
-        },
-        {
-          key: 'node',
-          content: <Typography variant="mono">{instance.endorsing_node_id}</Typography>,
-        },
-        {
-          key: 'expiration',
-          content: instance.expiration_epoch.toLocaleString(),
-          align: TableCellAlign.Right,
-        },
-      ],
-    }
-  })
+  const tableRows =
+    currentEpoch !== undefined && instances
+      ? instances?.map(instance => {
+          return {
+            key: instance.rak,
+            data: [
+              {
+                key: 'rak',
+                content: <Typography variant="mono">{instance.rak}</Typography>,
+              },
+              {
+                key: 'node',
+                content: <Typography variant="mono">{instance.endorsing_node_id}</Typography>,
+              },
+              {
+                key: 'expirationEpoch',
+                content: instance.expiration_epoch.toLocaleString(),
+                align: TableCellAlign.Right,
+              },
+              {
+                key: 'expirationStatus',
+                content: <RoflAppInstanceStatusBadge isActive={instance.expiration_epoch > currentEpoch} />,
+                align: TableCellAlign.Right,
+              },
+            ],
+          }
+        })
+      : undefined
 
   return (
     <Table
+      emptyMessage={t('rofl.emptyInstancesList')}
       columns={tableColumns}
       rows={tableRows}
       rowsNumber={limit}
