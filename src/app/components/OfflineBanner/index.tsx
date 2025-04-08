1ad526a7874ac643c38a7d8ef49ed1c7ddcd01c8
lukaw3d <lukajeran@oasislabs.com>
2025-04-08 14:22:53 +0200

    Differentiate if node is stuck too, not just Nexus

--- src/app/components/OfflineBanner/index.tsx
+++ src/app/components/OfflineBanner/index.tsx
@@ -55,6 +55,9 @@ export const OfflineBanner: FC<OfflineBannerProps> = props => {
     // Don't display lastUpdate. It's updating, but still many blocks behind.
     return <StickyAlert severity="warning">{t('home.indexerOutOfDate', { scope })}</StickyAlert>
   }
+  if (outOfDateReason === 'node') {
+    return <StickyAlert severity="warning">{t('home.nodeOutOfDateSince', { scope, lastUpdate })}</StickyAlert>
+  }
   exhaustedTypeWarning('Unexpected outOfDateReason', outOfDateReason)
   return <StickyAlert severity="warning">{t('home.indexerOutOfDate', { scope })}</StickyAlert>
 }
