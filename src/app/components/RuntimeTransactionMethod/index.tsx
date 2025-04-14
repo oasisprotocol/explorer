422225ee65ef1ae41e78bb068a3e04a7d8bc4cb5
lukaw3d <lukajeran@oasislabs.com>
2025-04-14 20:16:28 +0200

    Fix KnownRuntimeTxMethod type when unavailable

--- src/app/components/RuntimeTransactionMethod/index.tsx
+++ src/app/components/RuntimeTransactionMethod/index.tsx
@@ -18,10 +18,10 @@ import { SelectOptionBase } from '../Select'
 import { paraTimesConfig } from '../../../config'
 import { exhaustedTypeWarning } from '../../../types/errors'
 
-const getRuntimeTransactionLabel = (t: TFunction, method: KnownRuntimeTxMethod | undefined) => {
+const getRuntimeTransactionLabel = (t: TFunction, method: KnownRuntimeTxMethod) => {
   // TODO: when adding new types here, please also update knownRuntimeTxMethods below.
   switch (method) {
-    case undefined:
+    case '':
       // Method may be undefined if the transaction was malformed.
       return t('transactions.method.unavailable')
     case 'accounts.Transfer':
@@ -86,7 +86,7 @@ const knownRuntimeTxMethods = [
   'roflmarket.InstanceCancel',
   'roflmarket.InstanceExecuteCmds',
 ] as const
-type KnownRuntimeTxMethod = (typeof knownRuntimeTxMethods)[number]
+type KnownRuntimeTxMethod = (typeof knownRuntimeTxMethods)[number] | ''
 
 export const getRuntimeTxMethodOptions = (t: TFunction, layer: Layer): SelectOptionBase[] => {
   const hasRofl = !!paraTimesConfig[layer]?.offerRoflTxTypes
@@ -137,11 +137,7 @@ export const getRuntimeRoflUpdatesMethodOptions = (t: TFunction): SelectOptionBa
  *   - "roflmarket.InstanceCancel"
  *   - "roflmarket.InstanceExecuteCmds"
  */
-const getRuntimeTransactionIcon = (
-  method: KnownRuntimeTxMethod | undefined,
-  label: string,
-  truncate?: boolean,
-) => {
+const getRuntimeTransactionIcon = (method: KnownRuntimeTxMethod, label: string, truncate?: boolean) => {
   const props = {
     border: false,
     label,
@@ -185,7 +181,7 @@ const getRuntimeTransactionIcon = (
       return <MethodIcon color="orange" icon={<DeveloperBoardOffIcon />} {...props} />
     case 'roflmarket.InstanceExecuteCmds':
       return <MethodIcon icon={<DeveloperBoard />} {...props} />
-    case undefined:
+    case '':
       return <MethodIcon color="gray" icon={<QuestionMarkIcon />} {...props} />
     default:
       exhaustedTypeWarning('Unknown runtime tx method', method)
@@ -200,7 +196,7 @@ type RuntimeTransactionLabelProps = {
 
 export const RuntimeTransactionMethod: FC<RuntimeTransactionLabelProps> = ({ transaction, truncate }) => {
   const { t } = useTranslation()
-  let label = getRuntimeTransactionLabel(t, transaction.method as KnownRuntimeTxMethod | undefined)
+  let label = getRuntimeTransactionLabel(t, transaction.method as KnownRuntimeTxMethod)
   if (transaction.evm_fn_name) {
     if (truncate) {
       label = `${transaction.evm_fn_name}`
@@ -209,9 +205,7 @@ export const RuntimeTransactionMethod: FC<RuntimeTransactionLabelProps> = ({ tra
     }
   }
 
-  return (
-    <>{getRuntimeTransactionIcon(transaction.method as KnownRuntimeTxMethod | undefined, label, truncate)}</>
-  )
+  return <>{getRuntimeTransactionIcon(transaction.method as KnownRuntimeTxMethod, label, truncate)}</>
 }
 
 export const getRuntimeTransactionMethodFilteringParam = (
