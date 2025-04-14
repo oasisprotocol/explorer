79a843db456072e414aefdcefc4fa017fa29e021
Michal Zielenkiewicz <michal@oasisprotocol.org>
2025-04-14 13:24:11 +0200

    Add transaction card to instance details

--- src/app/pages/RoflAppInstanceDetailsPage/hooks.tsx
+++ src/app/pages/RoflAppInstanceDetailsPage/hooks.tsx
@@ -1,5 +1,10 @@
 import { useOutletContext } from 'react-router-dom'
+import { Layer, useGetRuntimeRoflAppsIdInstancesRakTransactions } from '../../../oasis-nexus/api'
+import { AppErrors } from '../../../types/errors'
 import { SearchScope } from '../../../types/searchScope'
+import { NUMBER_OF_ITEMS_ON_SEPARATE_PAGE as limit } from '../../config'
+import { getRuntimeTransactionMethodFilteringParam } from '../../components/RuntimeTransactionMethod'
+import { useSearchParamsPagination } from '../..//components/Table/useSearchParamsPagination'
 
 export type RoflAppInstanceDetailsContext = {
   scope: SearchScope
@@ -10,3 +15,41 @@ export type RoflAppInstanceDetailsContext = {
 }
 
 export const useRoflAppInstanceDetailsProps = () => useOutletContext<RoflAppInstanceDetailsContext>()
+
+export const useRoflAppInstanceRakTransactions = (
+  scope: SearchScope,
+  id: string,
+  rak: string,
+  method: string,
+) => {
+  const { network, layer } = scope
+  const pagination = useSearchParamsPagination('page')
+  const offset = (pagination.selectedPage - 1) * limit
+  if (layer !== Layer.sapphire) {
+    throw AppErrors.UnsupportedLayer
+  }
+
+  const query = useGetRuntimeRoflAppsIdInstancesRakTransactions(network, Layer.sapphire, id, rak, {
+    limit,
+    offset: offset,
+    ...getRuntimeTransactionMethodFilteringParam(method),
+  })
+  const { isFetched, isLoading, data } = query
+  const transactions = data?.data.transactions
+
+  if (isFetched && pagination.selectedPage > 1 && !transactions?.length) {
+    throw AppErrors.PageDoesNotExist
+  }
+
+  const totalCount = data?.data.total_count
+  const isTotalCountClipped = data?.data.is_total_count_clipped
+
+  return {
+    isLoading,
+    isFetched,
+    transactions,
+    pagination,
+    totalCount,
+    isTotalCountClipped,
+  }
+}
