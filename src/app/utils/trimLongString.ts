5597a47682a26a3b3310a8512fb86553af6b65b7
Kristof Csillag <kristof@oasisprotocol.org>
2025-05-21 11:44:51 +0200

    Make trimLongString more robust

--- src/app/utils/trimLongString.ts
+++ src/app/utils/trimLongString.ts
@@ -1,11 +1,19 @@
+/**
+ * Get a shortened version of a string
+ *
+ * @param value The original string
+ * @param trimStart How many characters to use at the beginning? (Minimum: 1)
+ * @param trimEnd How many characters to use at the end? (Minimum: 0)
+ * @param ellipsis What string should we use to indicate the shortening?
+ */
 export function trimLongString(value: string, trimStart = 6, trimEnd = 6, ellipsis = '…') {
-  if (!value) {
-    return
-  }
-  const trimmedLength = trimStart + ellipsis.length + trimEnd
-  if (trimmedLength > value.length) {
-    return value
-  }
+  if (!value) return
+  // We always want to show at least one character at the beginning and at the end
+  const wantedStart = Math.max(trimStart, 1)
+  // We might not want to show anything from the end of the string, but the number should not be negative
+  const wantedEnd = Math.max(trimEnd, 0)
+  const trimmedLength = wantedStart + ellipsis.length + wantedEnd
+  if (trimmedLength > value.length) return value
 
-  return `${value.slice(0, trimStart)}${ellipsis}${trimEnd ? value.slice(-trimEnd) : ''}`
+  return `${value.slice(0, wantedStart)}${ellipsis}${trimEnd ? value.slice(-wantedEnd) : ''}`
 }
