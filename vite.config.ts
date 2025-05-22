04dc4141b968026613afe5e3c8a95eceab28ce81
Michal Zielenkiewicz <michal@oasisprotocol.org>
2025-05-22 13:04:15 +0200

    Use Vite instead of Parcel to build project

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+export default defineConfig(() => {
+  return {
+    plugins: [react(), tsconfigPaths()],
+    build: {
+      outDir: 'build',
+      sourcemap: true,
+      emptyOutDir: true,
+    },
+    server: {
+      port: 1234,
+      open: true,
+    },
+    publicDir: 'public',
+  }
+})
