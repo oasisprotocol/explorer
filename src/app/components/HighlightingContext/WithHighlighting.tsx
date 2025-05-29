d588ce973cc06cbb40f45bdcf2ca1501f1f1245e
lukaw3d <luka@oasisprotocol.org>
2025-05-29 03:56:48 +0200

    Fix highlighting address after hovering a tooltip

--- src/app/components/HighlightingContext/WithHighlighting.tsx
+++ src/app/components/HighlightingContext/WithHighlighting.tsx
@@ -6,7 +6,11 @@ import { useScreenSize } from '../../hooks/useScreensize'
 
 export const WithHighlighting: FC<{ children: ReactNode; address: string }> = ({ children, address }) => {
   const { highlightedAddress, highlightAddress, releaseAddress } = useAddressHighlighting()
-  useEffect(() => () => releaseAddress(address)) // Release address on umount
+  useEffect(() => {
+    // Release address on unmount
+    return () => releaseAddress(address)
+    // eslint-disable-next-line react-hooks/exhaustive-deps -- Don't trigger on every render
+  }, [address])
   const { isTablet } = useScreenSize()
   // We have decided that we only want this feature on desktop,
   // so we are on tablet (or mobile), just return the wrapped contnt directly.
