41b7c8611787bf77c24469bffae17836df763e4c
Kristof Csillag <kristof@oasisprotocol.org>
2025-05-31 07:27:49 +0200

    Consensus dashboard: protect against network proposals failing to load

--- src/app/pages/ConsensusDashboardPage/NetworkProposalsCard.tsx
+++ src/app/pages/ConsensusDashboardPage/NetworkProposalsCard.tsx
@@ -11,13 +11,27 @@ import { COLORS } from '../../../styles/theme/colors'
 import { SearchScope } from '../../../types/searchScope'
 import { NetworkProposalsList } from '../../components/NetworkProposalsList'
 import { RouteUtils } from 'app/utils/route-utils'
+import { ErrorBoundary } from '../../components/ErrorBoundary'
 
 const limit = NUMBER_OF_ITEMS_ON_DASHBOARD
+
+const NetworkProposalsCardContent: FC<{ scope: SearchScope }> = ({ scope }) => {
+  const { network } = scope
+  const proposalsQuery = useGetConsensusProposals(network, { limit })
+
+  return (
+    <NetworkProposalsList
+      proposals={proposalsQuery.data?.data.proposals}
+      isLoading={proposalsQuery.isLoading}
+      limit={limit}
+      pagination={false}
+    />
+  )
+}
 
 export const NetworkProposalsCard: FC<{ scope: SearchScope }> = ({ scope }) => {
   const { t } = useTranslation()
   const { network } = scope
-  const proposalsQuery = useGetConsensusProposals(network, { limit })
 
   return (
     <Card>
@@ -36,12 +50,9 @@ export const NetworkProposalsCard: FC<{ scope: SearchScope }> = ({ scope }) => {
         }
       />
       <CardContent>
-        <NetworkProposalsList
-          proposals={proposalsQuery.data?.data.proposals}
-          isLoading={proposalsQuery.isLoading}
-          limit={limit}
-          pagination={false}
-        />
+        <ErrorBoundary light={true}>
+          <NetworkProposalsCardContent scope={scope} />
+        </ErrorBoundary>
       </CardContent>
     </Card>
   )
