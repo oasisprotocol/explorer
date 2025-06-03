ed5399e4c43b30adafe00b25f043323f6592e2f5
Michal Zielenkiewicz <michal@oasisprotocol.org>
2025-06-03 11:52:31 +0200

    Setup Tailwind CSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig(() => {
   return {
-    plugins: [react(), tsconfigPaths()],
+    plugins: [react(), tsconfigPaths(), tailwindcss()],
     build: {
       outDir: 'build',
       sourcemap: true,
