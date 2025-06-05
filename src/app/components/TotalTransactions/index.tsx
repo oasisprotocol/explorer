8094eea74e0fe89a305ad86ae2ee71ef027e3e23
Kristof Csillag <kristof@oasisprotocol.org>
2025-06-05 16:49:54 +0200

    Paratime Dashboard: handle failure to load total TXs

--- src/app/components/TotalTransactions/index.tsx
+++ src/app/components/TotalTransactions/index.tsx
@@ -2,22 +2,22 @@ import { FC, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
-import { LineChart } from '../../components/charts/LineChart'
+import { LineChart } from '../charts/LineChart'
 import { useGetLayerStatsTxVolume } from '../../../oasis-nexus/api'
 import { chartUseQueryStaleTimeMs, durationToQueryParams } from '../../utils/chart-utils'
-import { DurationPills } from '../../components/DurationPills'
-import { CardHeaderWithResponsiveActions } from '..//../components/CardHeaderWithResponsiveActions'
+import { DurationPills } from '../DurationPills'
+import { CardHeaderWithResponsiveActions } from '../CardHeaderWithResponsiveActions'
 import { ChartDuration, cumulativeSum } from '../../utils/chart-utils'
 import { useScreenSize } from '../../hooks/useScreensize'
 import { SearchScope } from '../../../types/searchScope'
+import { ErrorBoundary } from '../ErrorBoundary'
 
-export const TotalTransactions: FC<{ chartContainerHeight?: number; scope: SearchScope }> = ({
-  chartContainerHeight = 450,
-  scope,
-}) => {
+const TotalTransactionsContent: FC<{
+  scope: SearchScope
+  chartDuration: ChartDuration
+}> = ({ scope, chartDuration }) => {
   const { isMobile } = useScreenSize()
   const { t } = useTranslation()
-  const [chartDuration, setChartDuration] = useState<ChartDuration>(ChartDuration.MONTH)
   const statsParams = {
     ...durationToQueryParams[chartDuration],
     // We want to start from the beginning of offset as cumulative sum generates a line chart that always goes up
@@ -35,6 +35,43 @@ export const TotalTransactions: FC<{ chartContainerHeight?: number; scope: Searc
     : undefined
 
   return (
+    windows && (
+      <LineChart
+        tooltipActiveDotRadius={9}
+        cartesianGrid
+        strokeWidth={3}
+        dataKey="tx_volume"
+        data={windows}
+        margin={{ bottom: 16, top: isMobile ? 0 : 16 }}
+        tickMargin={16}
+        withLabels
+        formatters={{
+          data: (value: number) =>
+            t('totalTransactions.tooltip', {
+              value,
+              formatParams: {
+                value: {
+                  maximumFractionDigits: 2,
+                } satisfies Intl.NumberFormatOptions,
+              },
+            }),
+          label: (value: string) =>
+            t('common.formattedDateTime', {
+              timestamp: new Date(value),
+            }),
+        }}
+      />
+    )
+  )
+}
+
+export const TotalTransactions: FC<{ chartContainerHeight?: number; scope: SearchScope }> = ({
+  chartContainerHeight = 450,
+  scope,
+}) => {
+  const { t } = useTranslation()
+  const [chartDuration, setChartDuration] = useState<ChartDuration>(ChartDuration.MONTH)
+  return (
     <Card sx={{ flex: 1 }}>
       <CardHeaderWithResponsiveActions
         action={<DurationPills handleChange={setChartDuration} value={chartDuration} />}
@@ -43,33 +80,9 @@ export const TotalTransactions: FC<{ chartContainerHeight?: number; scope: Searc
         title={t('totalTransactions.header')}
       />
       <CardContent sx={{ height: chartContainerHeight }}>
-        {windows && (
-          <LineChart
-            tooltipActiveDotRadius={9}
-            cartesianGrid
-            strokeWidth={3}
-            dataKey="tx_volume"
-            data={windows}
-            margin={{ bottom: 16, top: isMobile ? 0 : 16 }}
-            tickMargin={16}
-            withLabels
-            formatters={{
-              data: (value: number) =>
-                t('totalTransactions.tooltip', {
-                  value,
-                  formatParams: {
-                    value: {
-                      maximumFractionDigits: 2,
-                    } satisfies Intl.NumberFormatOptions,
-                  },
-                }),
-              label: (value: string) =>
-                t('common.formattedDateTime', {
-                  timestamp: new Date(value),
-                }),
-            }}
-          />
-        )}
+        <ErrorBoundary light>
+          <TotalTransactionsContent scope={scope} chartDuration={chartDuration} />
+        </ErrorBoundary>
       </CardContent>
     </Card>
   )
