7a0683fc618e398a05ef0ab82c4aa54e9eab62ce
lukaw3d <luka@oasisprotocol.org>
2025-07-25 02:44:15 +0200

    Support EIP-1167 0age/thirdweb proxy variant

--- src/app/components/ContractVerificationIcon/extractMinimalProxyERC1167.ts
+++ src/app/components/ContractVerificationIcon/extractMinimalProxyERC1167.ts
@@ -8,6 +8,7 @@ export function extractMinimalProxyERC1167(account: RuntimeAccount) {
 
   const hexBytecode = base64ToHex(account.evm_contract.runtime_bytecode)
 
+  // https://explorer.dev.oasis.io/testnet/sapphire/address/0xbA7F3eE55b7939294caFA0F1e096E79722e1BD88/code#code
   const proxyToAddress1 = hexBytecode.match(
     /^0x363d3d373d3d3d363d73([0-9a-fA-F]{40})5af43d82803e903d91602b57fd5bf3$/,
   )?.[1]
@@ -18,6 +19,17 @@ export function extractMinimalProxyERC1167(account: RuntimeAccount) {
     /^0x363d3d373d3d3d363d6f([0-9a-fA-F]{32})5af43d82803e903d91602757fd5bf3$/,
   )?.[1]
   if (proxyToAddress2) return toChecksumAddress(`0x00000000${proxyToAddress2}`)
+
+  // 0age's minimal proxy - https://medium.com/coinmonks/the-more-minimal-proxy-5756ae08ee48
+  // Created using thirdweb https://explorer.oasis.io/testnet/sapphire/address/0xd5fc21B36D5153Cd0bfF4d774539ba9dFd13ADA8/code#code
+  const proxyToAddress3 = hexBytecode.match(
+    /^0x3d3d3d3d363d3d37363d73([0-9a-fA-F]{40})5af43d3d93803e602a57fd5bf3$/,
+  )?.[1]
+  if (proxyToAddress3) return toChecksumAddress(`0x${proxyToAddress3}`)
+
+  // Support more?
+  // https://github.com/thirdweb-dev/js/blob/1607229b21cb3dde3647c4121fff90612cc77275/packages/thirdweb/src/utils/bytecode/extractMinimalProxyImplementationAddress.ts#L13
+  // https://github.com/ApeWorX/ape/blob/9858def38564b0e96ad3fb03edb05084667cd263/src/ape_ethereum/ecosystem.py#L476-L487
 
   return undefined
 }
