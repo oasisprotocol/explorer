d328d80f222ccae8e037ba05ef781063f1d2664e
Kaja <kaja.osojnik@gmail.com>
2025-09-02 12:45:04 +0100

    Learning Section update to tailwind

--- src/app/components/LearningMaterialsCard/LearningSection.tsx
+++ src/app/components/LearningMaterialsCard/LearningSection.tsx
@@ -1,6 +1,5 @@
-import Typography from '@mui/material/Typography'
+import { Typography } from '@oasisprotocol/ui-library/src/components/typography'
 import { FC } from 'react'
-import { COLORS } from 'styles/theme/colors'
 import { AnchorCircle } from '../StyledLinks'
 import { cn } from '@oasisprotocol/ui-library/src/lib/utils'
 
@@ -17,12 +16,10 @@ export const LearningSection: FC<LearningSectionProps> = ({ className, descripti
       className={cn('flex flex-col px-6 py-5 gap-4 rounded-md border border-zinc-200 bg-zinc-50', className)}
     >
       <div className="flex-1 flex flex-col gap-1">
-        <Typography variant="h4" sx={{ mb: 4 }}>
+        <Typography variant="h4" className="mb-2">
           {title}
         </Typography>
-        <Typography variant="body2" sx={{ color: COLORS.grayMedium }}>
-          {description}
-        </Typography>
+        <Typography>{description}</Typography>
       </div>
       <div>
         <AnchorCircle url={url} />
