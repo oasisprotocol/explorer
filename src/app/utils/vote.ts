8bf8fda7a98cf5c0169eecfec336b45cb0933104
Michal Zielenkiewicz <michal@oasisprotocol.org>
2025-09-17 16:59:28 +0200

    Use votes API type filter

--- src/app/utils/vote.ts
+++ src/app/utils/vote.ts
@@ -1,17 +1,5 @@
-import { ExtendedVote, ProposalVoteValue, VoteFilter, VoteType } from '../../types/vote'
+import { ExtendedVote } from '../../types/vote'
 import { hasTextMatchesForAll } from '../components/HighlightedText/text-matching'
-
-export const getRandomVote = (): ProposalVoteValue =>
-  [ProposalVoteValue.yes, ProposalVoteValue.no, ProposalVoteValue.abstain][Math.floor(Math.random() * 3)]
-
-const voteFilters: Record<VoteType, VoteFilter> = {
-  any: () => true,
-  yes: vote => vote.vote === ProposalVoteValue.yes,
-  no: vote => vote.vote === ProposalVoteValue.no,
-  abstain: vote => vote.vote === ProposalVoteValue.abstain,
-}
-
-export const getFilterForVoteType = (voteType: VoteType): VoteFilter => voteFilters[voteType]
 
 export const getFilterForVoterNameFragment = (fragments: string[]) =>
   fragments.length
