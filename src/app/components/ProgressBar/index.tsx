956777356286396284fa6ad302c0247126e74f5e
Michal Zielenkiewicz <michal@oasisprotocol.org>
2025-10-06 15:33:54 +0200

    Replace BrandProgressBar with custom Oasis UI Library component

--- src/app/components/ProgressBar/index.tsx
+++ src/app/components/ProgressBar/index.tsx
@@ -1,8 +1,9 @@
-import { FC } from 'react'
+import { FC, ReactNode } from 'react'
 import Box from '@mui/material/Box'
 import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress'
 import { styled } from '@mui/material/styles'
 import { COLORS } from 'styles/theme/colors'
+import { Progress } from '@oasisprotocol/ui-library/src/components/progress'
 
 type VerticalProgressBarProps = {
   value: number
@@ -43,11 +44,23 @@ const StyledLinearProgress = styled(LinearProgress, {
   ...(barBackgroundColor && { backgroundColor: barBackgroundColor }),
 }))
 
-export const BrandProgressBar = styled(LinearProgress)(() => ({
-  [`&.${linearProgressClasses.determinate} > .${linearProgressClasses.bar1Determinate}`]: {
-    backgroundColor: COLORS.brandDark,
-  },
-  borderColor: COLORS.grayLight,
-  backgroundColor: COLORS.grayLight,
-  height: '12px',
-}))
+type CustomProgressProps = {
+  value?: number
+  max?: number
+  label: ReactNode
+}
+
+export const CustomProgress: FC<CustomProgressProps> = ({ value, max, label }) => {
+  if (!value || !max) {
+    return null
+  }
+
+  return (
+    <div className="w-full relative">
+      <Progress value={(value / max) * 100} className="h-8 bg-muted-foreground rounded-[8px]" />
+      <span className="absolute inset-y-0 left-3 flex items-center text-white text-xs font-normal">
+        {label}
+      </span>
+    </div>
+  )
+}
