9aa86dc31591e59708f6f4a64979d1e93841971e
Michal Zielenkiewicz <michal@oasisprotocol.org>
2025-10-07 09:22:54 +0200

    Remove not used code from CodeDisplay component

--- src/app/components/CodeDisplay/index.tsx
+++ src/app/components/CodeDisplay/index.tsx
@@ -1,8 +1,8 @@
-import React, { FC, useState, Suspense } from 'react'
+import React, { FC, Suspense } from 'react'
 import { useTranslation } from 'react-i18next'
 import Box from '@mui/material/Box'
 import { Typography } from '@oasisprotocol/ui-library/src/components/typography'
-import { CopyToClipboard, FloatingCopyToClipboard } from '../../components/CopyToClipboard'
+import { CopyToClipboard } from '../../components/CopyToClipboard'
 import { useScreenSize } from '../../hooks/useScreensize'
 import { base64ToHex } from '../../utils/helpers'
 import { MonacoLanguages } from './MonacoLanguages'
@@ -43,48 +43,33 @@ type CodeDisplayProps = {
   language: MonacoLanguages
   label?: string
   extraTopPadding?: boolean
-  floatingCopyButton?: boolean
 }
 
-const CodeDisplay: FC<CodeDisplayProps> = ({
-  code,
-  language,
-  label = undefined,
-  extraTopPadding = false,
-  floatingCopyButton = false,
-}) => {
+const CodeDisplay: FC<CodeDisplayProps> = ({ code, language, label = undefined }) => {
   const { t } = useTranslation()
   const { isMobile } = useScreenSize()
-  const [isHovering, setIsHovering] = useState(false)
 
   if (!code) {
     return null
   }
 
   return (
-    <Box
-      sx={{ flex: 1, position: 'relative' }}
-      onMouseEnter={() => setIsHovering(true)}
-      onMouseLeave={() => setIsHovering(false)}
-    >
+    <Box sx={{ flex: 1, position: 'relative' }}>
       <Box
         sx={{
           display: 'flex',
           justifyContent: 'space-between',
           alignItems: 'center',
-          my: floatingCopyButton ? 0 : 3,
-          pt: extraTopPadding ? 4 : 0,
+          my: 3,
+          pt: 0,
         }}
       >
         {label && <Typography variant="h4">{label}</Typography>}
-        {floatingCopyButton ? (
-          <FloatingCopyToClipboard isVisible={isHovering} value={code} />
-        ) : (
-          <CopyToClipboard
-            value={code}
-            label={isMobile ? t('common.copy') : t('contract.copyButton', { subject: label })}
-          />
-        )}
+
+        <CopyToClipboard
+          value={code}
+          label={isMobile ? t('common.copy') : t('contract.copyButton', { subject: label })}
+        />
       </Box>
       <Box sx={{ height: '350px', overflow: 'auto', resize: 'vertical' }}>
         {/* While loading wrapper show <textarea> instead */}
@@ -133,13 +118,10 @@ export const FileDisplay: FC<FileDisplayProps> = ({ code, filename }) => {
 type JsonCodeDisplayProps = {
   data: Record<string, any>
   label?: string
-  floatingCopyButton?: boolean
 }
 
-export const JsonCodeDisplay: FC<JsonCodeDisplayProps> = ({ data, label, floatingCopyButton }) => {
+export const JsonCodeDisplay: FC<JsonCodeDisplayProps> = ({ data, label }) => {
   const formattedJson = JSON.stringify(data, null, 2)
 
-  return (
-    <CodeDisplay code={formattedJson} label={label} floatingCopyButton={floatingCopyButton} language="json" />
-  )
+  return <CodeDisplay code={formattedJson} label={label} language="json" />
 }
