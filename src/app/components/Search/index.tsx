2ef34935c9a66542356270aed06893eafabf566b
Michal Zielenkiewicz <michal@oasisprotocol.org>
2025-10-14 12:21:45 +0200

    Replace MUI dependencies in Search with Oasis UI Library

--- src/app/components/Search/index.tsx
+++ src/app/components/Search/index.tsx
@@ -1,102 +1,21 @@
 import { FC, FormEvent, memo, useEffect, useState } from 'react'
 import { useNavigate, useSearchParams } from 'react-router-dom'
-import TextField, { textFieldClasses } from '@mui/material/TextField'
-import InputAdornment, { inputAdornmentClasses } from '@mui/material/InputAdornment'
-import { styled } from '@mui/material/styles'
-import MuiButton, { ButtonProps } from '@mui/material/Button'
-import Box from '@mui/material/Box'
 import SearchIcon from '@mui/icons-material/Search'
 import { useTranslation } from 'react-i18next'
 import { COLORS } from '../../../styles/theme/colors'
 import { RouteUtils } from '../../utils/route-utils'
 import { useScreenSize } from '../../hooks/useScreensize'
-import HighlightOffIcon from '@mui/icons-material/HighlightOff'
-import ErrorIcon from '@mui/icons-material/Error'
-import WarningIcon from '@mui/icons-material/Warning'
 import { Button } from '@oasisprotocol/ui-library/src/components/ui/button'
-import { SearchSuggestionsButtons } from './SearchSuggestionsButtons'
-import { formHelperTextClasses } from '@mui/material/FormHelperText'
-import { outlinedInputClasses } from '@mui/material/OutlinedInput'
 import { SearchScope } from '../../../types/searchScope'
 import { textSearchMinimumLength } from './search-utils'
-import Typography from '@mui/material/Typography'
 import { isValidBlockHeight } from '../../utils/helpers'
 import { typingDelay } from '../../../styles/theme'
 import { isValidMnemonic } from '../../utils/helpers'
-import Collapse from '@mui/material/Collapse'
 import { getAppTitle } from '../../../config'
+import { SearchInput } from '@oasisprotocol/ui-library/src/components/input'
+import { cn } from '@oasisprotocol/ui-library/src/lib/utils'
 
 export type SearchVariant = 'button' | 'icon' | 'expandable'
-
-interface StyledBaseProps {
-  searchVariant: SearchVariant
-}
-
-interface SearchFormProps extends StyledBaseProps {}
-
-const SearchForm = styled('form', {
-  shouldForwardProp: (prop: PropertyKey) =>
-    !(['searchVariant'] as (keyof SearchFormProps)[]).includes(prop as keyof SearchFormProps),
-})<SearchFormProps>(({ searchVariant }) => ({
-  // Approximate size of TextField without helperText, so helperText floats.
-  height: '47px',
-
-  ...(searchVariant === 'expandable'
-    ? {
-        position: 'absolute',
-        zIndex: 1,
-        // Collapsed
-        ':not(:hover, :focus-within)': {
-          [`.${textFieldClasses.root}`]: {
-            [`input, .${formHelperTextClasses.root}, .${outlinedInputClasses.notchedOutline}`]: {
-              display: 'none',
-            },
-            [`.${inputAdornmentClasses.positionEnd}`]: {
-              'button[type="button"]': {
-                display: 'none',
-              },
-              marginLeft: 0,
-            },
-          },
-        },
-        // Expanded
-        ':hover, :focus-within': {
-          left: 0,
-          width: '100%',
-        },
-      }
-    : {
-        position: 'relative',
-        width: '100%',
-      }),
-}))
-
-interface SearchButtonProps extends StyledBaseProps {}
-
-export const StyledSearchButton = styled(MuiButton, {
-  shouldForwardProp: (prop: PropertyKey) =>
-    !(['searchVariant'] as (keyof SearchButtonProps)[]).includes(prop as keyof SearchButtonProps),
-})<SearchButtonProps>(({ theme, searchVariant }) => ({
-  minWidth: 'unset',
-  padding: '12px',
-  [theme.breakpoints.up('sm')]: {
-    ...(searchVariant === 'button'
-      ? {
-          paddingLeft: theme.spacing(5),
-          paddingRight: theme.spacing(5),
-        }
-      : {}),
-  },
-  ...(searchVariant === 'expandable'
-    ? {
-        backgroundColor: COLORS.brandDark,
-      }
-    : {}),
-}))
-
-export const SearchButton = (props: ButtonProps & SearchButtonProps) => (
-  <StyledSearchButton variant="contained" color="primary" {...props} />
-)
 
 export interface SearchProps {
   scope?: SearchScope
@@ -105,7 +24,7 @@ export interface SearchProps {
   onFocusChange?: (hasFocus: boolean) => void
 }
 
-const SearchCmp: FC<SearchProps> = ({ scope, variant, disabled, onFocusChange: onFocusChangeProp }) => {
+export const Search: FC<SearchProps> = ({ scope, variant, disabled, onFocusChange: onFocusChangeProp }) => {
   const { t } = useTranslation()
   const navigate = useNavigate()
   const { isMobile } = useScreenSize()
@@ -139,31 +58,12 @@ const SearchCmp: FC<SearchProps> = ({ scope, variant, disabled, onFocusChange: o
       navigate(RouteUtils.getSearchRoute(scope, trimmedValue))
     }
   }
-
-  const onClearValue = () => {
-    setValue('')
-  }
-
-  const startAdornment = variant === 'button' && (
-    <InputAdornment
-      position="start"
-      disablePointerEvents // Pass clicks through, so it focuses the input
-    >
-      <SearchIcon sx={{ color: isFocused ? COLORS.grayMediumLight : COLORS.grayDark }} />
-    </InputAdornment>
-  )
-
-  const searchButtonContent =
-    variant !== 'button' ? <SearchIcon sx={{ color: COLORS.grayMediumLight }} /> : t('search.searchBtnText')
-
   const errorMessage = isTooShort ? t('search.error.tooShort') : undefined
   const hasError = !!errorMessage
-
   const warningMessage = isValidMnemonic(trimmedValue)
     ? t('search.error.privacy', { appName: getAppTitle() })
     : undefined
   const hasWarning = !!warningMessage
-
   const hasProblem = hasError || hasWarning
 
   useEffect(() => {
@@ -178,115 +78,32 @@ const SearchCmp: FC<SearchProps> = ({ scope, variant, disabled, onFocusChange: o
   }, [hasProblem])
 
   return (
-    <SearchForm
-      searchVariant={variant}
+    <form
+      className="flex gap-2"
       onSubmit={onFormSubmit}
       role="search"
       aria-label={searchPlaceholderTranslated}
     >
-      <TextField
-        sx={{
-          ...(!isProblemFresh && hasWarning && !hasError
-            ? {
-                [`& .${outlinedInputClasses.error} .${outlinedInputClasses.notchedOutline}`]: {
-                  borderColor: COLORS.warningColor,
-                },
-              }
-            : {}),
-          border: `1px solid ${COLORS.grayLight}`,
-        }}
-        value={value}
-        onChange={e => onChange(e.target.value)}
-        error={!isProblemFresh && hasProblem}
-        onFocus={() => onFocusChange(true)}
-        onBlur={() => onFocusChange(false)}
-        InputProps={{
-          inputProps: {
-            sx: { p: 0, fontSize: '14px' },
-          },
-          startAdornment,
-          endAdornment: (
-            <InputAdornment position="end">
-              <>
-                {value && (
-                  <Button
-                    variant="ghost"
-                    size="icon"
-                    onClick={onClearValue}
-                    className="hover:bg-black/[0.04] rounded-full"
-                  >
-                    <HighlightOffIcon />
-                  </Button>
-                )}
-                <SearchButton disabled={disabled || hasProblem} searchVariant={variant} type="submit">
-                  {searchButtonContent}
-                </SearchButton>
-              </>
-            </InputAdornment>
-          ),
-        }}
+      <SearchInput
+        className=""
+        size="lg"
+        onChange={onChange}
         placeholder={searchPlaceholderTranslated}
-        fullWidth
-        FormHelperTextProps={{
-          component: 'div', // replace p with div tag
-          sx: {
-            marginTop: 0,
-            marginBottom: 0,
-            marginLeft: variant === 'button' ? '48px' : '17px',
-            marginRight: variant === 'button' ? '48px' : '17px',
-          },
-        }}
-        helperText={
-          <Collapse in={!!value && value !== valueInSearchParams}>
-            <Box sx={{ py: '10px' }}>
-              <Collapse in={!isProblemFresh && hasError}>
-                <Typography
-                  component="span"
-                  sx={{
-                    display: 'inline-flex',
-                    color: COLORS.errorIndicatorBackground,
-                    fontSize: 12,
-                    lineHeight: 2,
-                    alignItems: 'center',
-                    verticalAlign: 'middle',
-                    mt: 3,
-                    mb: 4,
-                  }}
-                >
-                  <ErrorIcon sx={{ mr: 3 }} />
-                  {errorMessage}
-                </Typography>
-              </Collapse>
-              <Collapse in={!isProblemFresh && hasWarning}>
-                <Typography
-                  component="span"
-                  sx={{
-                    display: 'inline-flex',
-                    color: COLORS.warningColor,
-                    fontSize: 12,
-                    lineHeight: 2,
-                    alignItems: 'center',
-                    verticalAlign: 'middle',
-                    mt: 3,
-                    mb: 4,
-                  }}
-                >
-                  <WarningIcon sx={{ mr: 3 }} />
-                  {warningMessage}
-                </Typography>
-              </Collapse>
-              <SearchSuggestionsButtons
-                scope={scope}
-                onClickSuggestion={suggestion => {
-                  setValue(suggestion)
-                }}
-              />
-            </Box>
-          </Collapse>
-        }
+        value={value}
       />
-    </SearchForm>
+      <Button
+        className={cn('', variant === 'icon' && '!px-2.5')}
+        onClick={onFormSubmit}
+        disabled={disabled || hasProblem}
+        type="submit"
+        size="lg"
+      >
+        {variant !== 'button' ? (
+          <SearchIcon sx={{ color: COLORS.grayMediumLight }} />
+        ) : (
+          t('search.searchBtnText')
+        )}
+      </Button>
+    </form>
   )
 }
-
-export const Search = memo(SearchCmp)
