9b3d83e27dcbbb2ead1dadf4fe6bf9cfd500d5c1
Michal Zielenkiewicz <michal@oasisprotocol.org>
2025-11-03 16:16:19 +0100

    Style search container

--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -1,25 +1,21 @@
 import { FC } from 'react'
-import { Search } from '../../components/Search'
 import { Footer } from '../../components/PageLayout/Footer'
-import { useTranslation } from 'react-i18next'
 import { BuildBanner } from '../../components/BuildBanner'
 import { useSearchQueryNetworkParam } from '../../hooks/useSearchQueryNetworkParam'
 import { NetworkOfflineBanner } from '../../components/OfflineBanner'
-import { useIsApiReachable } from '../../components/OfflineBanner/hook'
 import { Social } from '../../components/Social'
 import { RoflAppsCard } from './RoflAppsCard'
+import { HomeSearch } from './HomeSearch'
 
 export const HomePage: FC = () => {
-  const { t } = useTranslation()
   const { network } = useSearchQueryNetworkParam()
-  const isApiReachable = useIsApiReachable(network).reachable
 
   return (
     <>
       <BuildBanner />
       <NetworkOfflineBanner wantedNetwork={network} />
-      <div className="px-6">
-        <Search disabled={!isApiReachable} />
+      <div className="flex flex-col px-6">
+        <HomeSearch />
         <RoflAppsCard />
         <Social />
       </div>
