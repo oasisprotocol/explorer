3d738857512b1e499508c84edccc2e98095596d2
Michal Zielenkiewicz <michal@oasisprotocol.org>
2025-11-03 16:16:19 +0100

    Style search container

---
+++ src/app/pages/HomePage/HomeSearch.tsx
@@ -0,0 +1,31 @@
+import { FC } from 'react'
+import { Search } from '../../components/Search'
+import { useTranslation } from 'react-i18next'
+import { useSearchQueryNetworkParam } from '../../hooks/useSearchQueryNetworkParam'
+import { useIsApiReachable } from '../../components/OfflineBanner/hook'
+
+import SearchBg from './images/search-bg.png'
+import { Typography } from '@oasisprotocol/ui-library/src/components/typography'
+
+export const HomeSearch: FC = () => {
+  const { t } = useTranslation()
+  const { network } = useSearchQueryNetworkParam()
+  const isApiReachable = useIsApiReachable(network).reachable
+
+  return (
+    <div
+      id="search"
+      style={{ backgroundImage: `url(${SearchBg})` }}
+      className="py-12 px-8 md:p-24 bg-[#DFDEF5] bg-cover bg-center bg-no-repeat rounded-md mb-6"
+    >
+      <Typography
+        variant="h2"
+        className="text-center justify-start text-foreground text-2xl md:text-3xl font-semibold leading-8 mb-4 md:mb-8"
+      >
+        {t('home.search')}
+      </Typography>
+
+      <Search disabled={!isApiReachable} />
+    </div>
+  )
+}
--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -1,25 +1,23 @@
 import { FC } from 'react'
-import { Search } from '../../components/Search'
 import { Footer } from '../../components/PageLayout/Footer'
 import { BuildBanner } from '../../components/BuildBanner'
 import { useSearchQueryNetworkParam } from '../../hooks/useSearchQueryNetworkParam'
 import { NetworkOfflineBanner } from '../../components/OfflineBanner'
-import { useIsApiReachable } from '../../components/OfflineBanner/hook'
 import { Social } from '../../components/Social'
 import { RecentBlocksCard } from './RecentBlocksCard'
 import { Ecosystem } from './Ecosystem'
 import { RoflAppsCard } from './RoflAppsCard'
+import { HomeSearch } from './HomeSearch'
 
 export const HomePage: FC = () => {
   const { network } = useSearchQueryNetworkParam()
-  const isApiReachable = useIsApiReachable(network).reachable
 
   return (
     <>
       <BuildBanner />
       <NetworkOfflineBanner wantedNetwork={network} />
-      <div className="px-6">
-        <Search disabled={!isApiReachable} />
+      <div className="flex flex-col px-6">
+        <HomeSearch />
         <Ecosystem />
         <RecentBlocksCard />
         <RoflAppsCard />
