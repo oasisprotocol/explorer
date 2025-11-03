3f6aa7a832c395b1d7234c7678a945b36850c368
Michal Zielenkiewicz <michal@oasisprotocol.org>
2025-11-03 09:36:11 +0100

    Replace MUI Tooltip with Oasis UI Library component

--- src/app/components/Tooltip/MaybeWithTooltip.tsx
+++ src/app/components/Tooltip/MaybeWithTooltip.tsx
@@ -1,5 +1,5 @@
 import { FC, ReactNode } from 'react'
-import Tooltip from '@mui/material/Tooltip'
+import { Tooltip } from '@oasisprotocol/ui-library/src/components/tooltip'
 
 type MaybeWithTooltipProps = {
   /**
@@ -26,11 +26,8 @@ type MaybeWithTooltipProps = {
 export const MaybeWithTooltip: FC<MaybeWithTooltipProps> = ({ title, children, spanClassName }) => {
   return (
     <Tooltip
-      placement="top"
       title={<div className="flex items-center gap-2 md:mx-0 -mx-3 min-w-0 wrap-anywhere">{title}</div>}
-      disableFocusListener={!title}
-      disableHoverListener={!title}
-      disableTouchListener={!title}
+      disabled={!title}
     >
       <span className={spanClassName}>{children}</span>
     </Tooltip>
