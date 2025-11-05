daa500a67e7cadc13f3b486014ea501876fb5fd3
Kaja <kaja.osojnik@gmail.com>
2025-11-05 15:26:04 +0100

    fix Paratime card layout on Consensus dashboard

--- src/app/pages/ConsensusDashboardPage/ParaTimesCard.tsx
+++ src/app/pages/ConsensusDashboardPage/ParaTimesCard.tsx
@@ -5,10 +5,9 @@ import { Layer, Runtime } from '../../../oasis-nexus/api'
 import { CardHeaderWithCounter } from '../../components/CardHeaderWithCounter'
 import { isNotInHiddenScope, RouteUtils } from '../../utils/route-utils'
 import { SearchScope } from '../../../types/searchScope'
-import { EnabledRuntimePreview, DisabledRuntimePreview } from './RuntimePreview'
+import { EnabledRuntimePreview } from './RuntimePreview'
 import { Network } from '../../../types/network'
 import { Typography } from '@oasisprotocol/ui-library/src/components/typography'
-import { cn } from '@oasisprotocol/ui-library/src/lib/utils'
 
 function shouldIncludeLayer(network: Network, layer: Layer) {
   return layer !== 'consensus' && isNotInHiddenScope({ network, layer })
@@ -24,7 +23,6 @@ export const ParaTimesCard: FC<ParaTimesCardProps> = ({ scope }) => {
   const disabledRuntimes = disabled.filter(layer => shouldIncludeLayer(network, layer)) as Runtime[]
   const runtimesNumber = enabledRuntimes.length + disabledRuntimes.length
   const [firstEnabledRuntime, ...restEnabledRuntimes] = enabledRuntimes
-  const spaceForSecondaryParaTimes = enabledRuntimes.length > 2
 
   return (
     <Card variant="layout">
@@ -40,32 +38,21 @@ export const ParaTimesCard: FC<ParaTimesCardProps> = ({ scope }) => {
         </CardTitle>
       </CardHeader>
       <CardContent>
-        <div className="grid grid-cols-12 gap-4">
-          <div className={cn('col-span-12', spaceForSecondaryParaTimes ? 'lg:col-span-5' : 'lg:col-span-6')}>
+        <div className="flex flex-col gap-4 lg:flex-row lg:gap-5">
+          <div className="flex-1 min-w-0 lg:w-1/2">
             <EnabledRuntimePreview prominentItem network={scope.network} runtime={firstEnabledRuntime} />
           </div>
 
-          <div
-            className={cn(
-              'grid col-span-12 grid-cols-12 gap-x-4',
-              spaceForSecondaryParaTimes ? 'lg:col-span-7' : 'lg:col-span-6',
-            )}
-          >
-            <div
-              className={cn('col-span-12', spaceForSecondaryParaTimes ? 'lg:col-span-9' : 'lg:col-span-6')}
-            >
-              <div className="flex flex-col gap-4 lg:flex-row lg:gap-5">
-                {restEnabledRuntimes.map(runtime => (
-                  <EnabledRuntimePreview key={runtime} network={scope.network} runtime={runtime} />
-                ))}
-              </div>
-            </div>
-
-            <div
-              className={cn('col-span-12', spaceForSecondaryParaTimes ? 'lg:col-span-3' : 'lg:col-span-6')}
-            >
-              {disabledRuntimes.map(runtime => (
-                <DisabledRuntimePreview key={runtime} runtime={runtime} />
+          <div className="flex gap-4 flex-1 min-w-0 lg:w-1/2">
+            <div className="flex flex-col gap-4 lg:flex-row lg:gap-5 w-full">
+              {restEnabledRuntimes.map(runtime => (
+                <EnabledRuntimePreview
+                  key={runtime}
+                  network={scope.network}
+                  runtime={runtime}
+                  lastNoBorder
+                  showBothCharts={restEnabledRuntimes.length === 1}
+                />
               ))}
             </div>
           </div>
