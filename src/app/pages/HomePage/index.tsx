6ac819c9eee2af721985a1b3ebbbb35ab4a04bb1
Michal Zielenkiewicz <michal@oasisprotocol.org>
2025-11-13 10:29:31 +0100

    Add recent blocks section to homepage

--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -7,6 +7,7 @@ import { Social } from '../../components/Social'
 import { RoflAppsCard } from './RoflAppsCard'
 import { HomeSearch } from './HomeSearch'
 import { Header } from 'app/components/PageLayout/Header'
+import { RecentBlocksCard } from './RecentBlocksCard'
 
 export const HomePage: FC = () => {
   const { network } = useSearchQueryNetworkParam()
@@ -18,6 +19,10 @@ export const HomePage: FC = () => {
       <Header sticky={false} />
       <div className="flex flex-col px-6">
         <HomeSearch />
+        <div className="flex gap-6 flex-col md:flex-row">
+          <RecentBlocksCard />
+          <div>Placeholder</div>
+        </div>
         <RoflAppsCard />
         <Social />
       </div>
