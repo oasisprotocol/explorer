675a93529f85f45d5e9c7a67fffa981b47bc8404
Michal Zielenkiewicz <michal@oasisprotocol.org>
2025-11-17 09:10:31 +0100

    Init new homepage

--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -1,14 +1,15 @@
 import { FC } from 'react'
 import { Search } from '../../components/Search'
 import { Footer } from '../../components/PageLayout/Footer'
-import { useTranslation } from 'react-i18next'
 import { BuildBanner } from '../../components/BuildBanner'
 import { useSearchQueryNetworkParam } from '../../hooks/useSearchQueryNetworkParam'
 import { NetworkOfflineBanner } from '../../components/OfflineBanner'
 import { useIsApiReachable } from '../../components/OfflineBanner/hook'
+import { Social } from '../../components/Social'
+import { RecentBlocksCard } from './RecentBlocksCard'
+import { Ecosystem } from './Ecosystem'
 
 export const HomePage: FC = () => {
-  const { t } = useTranslation()
   const { network } = useSearchQueryNetworkParam()
   const isApiReachable = useIsApiReachable(network).reachable
 
@@ -17,6 +18,9 @@ export const HomePage: FC = () => {
       <BuildBanner />
       <NetworkOfflineBanner wantedNetwork={network} />
       <Search disabled={!isApiReachable} />
+      <Ecosystem />
+      <RecentBlocksCard />
+      <Social />
       <Footer />
     </>
   )
