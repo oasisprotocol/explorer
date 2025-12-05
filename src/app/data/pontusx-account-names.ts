a67b0f3a491a29c14304542eeabb2bac38f06565
moritzkirstein <moritz@delta-dao.com>
2025-12-05 14:39:18 +0100

    feat: replace registry logic with registry-hooks

--- src/app/data/pontusx-account-names.ts
+++ src/app/data/pontusx-account-names.ts
@@ -1,65 +1,59 @@
-import axios from 'axios'
-import { useQuery, UseQueryOptions } from '@tanstack/react-query'
-import {
-  AccountMetadata,
-  AccountMap,
-  AccountMetadataInfo,
-  AccountNameSearchRuntimeMatch,
-  AccountNameSearchRuntimeResults,
-} from './named-accounts'
+import { usePontusXRegistry } from '@deltadao/pontusx-registry-hooks'
+import { UseQueryOptions } from '@tanstack/react-query'
 import { useGetRuntimeAccountsAddresses } from '../../oasis-nexus/api'
 import { Network } from '../../types/network'
 import { hasTextMatchesForAll } from '../components/HighlightedText/text-matching'
 import { getOasisAddress } from '../utils/helpers'
-import * as externalLinks from '../utils/externalLinks'
+import {
+  AccountMap,
+  AccountMetadata,
+  AccountMetadataInfo,
+  AccountNameSearchRuntimeMatch,
+  AccountNameSearchRuntimeResults,
+} from './named-accounts'
 
 type PontusXAccountsMetadata = {
   map: AccountMap
   list: AccountMetadata[]
 }
 
-const getPontusXAccountsMetadata = async (): Promise<PontusXAccountsMetadata> => {
-  const response = await axios.get(externalLinks.api.deltadao_named_addresses)
-  if (response.status !== 200) throw new Error("Couldn't load names")
-  if (!response.data) throw new Error("Couldn't load names")
+export const usePontusXAccountsMetadata = () => {
+  const { isLoading, error, data } = usePontusXRegistry({
+    apiBaseUrl: 'https://cache.registry.staging.pontus-x.eu',
+    includeDeprecated: true,
+  })
+
+  if (error) {
+    console.log('Failed to load Pontus-X account names', error)
+  }
+
   const map: AccountMap = new Map()
   const list: AccountMetadata[] = []
-  Object.entries(response.data).forEach(([evmAddress, name]) => {
+
+  if (!data) {
+    return { map, list, isLoading, error }
+  }
+
+  data.forEach(identity => {
     const account: AccountMetadata = {
       source: 'DeltaDaoRegistry',
-      address: getOasisAddress(evmAddress),
-      name: name as string,
+      address: getOasisAddress(identity.walletAddress),
+      name: identity.legalName as string,
     }
     map.set(account.address, account)
     list.push(account)
   })
-  return {
-    map,
-    list,
-  }
+
+  return { map, list, isLoading, error }
 }
 
-export const usePontusXAccountsMetadata = (
-  queryOptions: UseQueryOptions<PontusXAccountsMetadata, unknown, PontusXAccountsMetadata, string[]>,
-) => {
-  return useQuery(['pontusXNames'], getPontusXAccountsMetadata, {
-    staleTime: Infinity,
-    ...queryOptions,
-  })
-}
+export const usePontusXAccountMetadata = (oasisAddress: string): AccountMetadataInfo => {
+  const { map, isLoading, error } = usePontusXAccountsMetadata()
 
-export const usePontusXAccountMetadata = (
-  oasisAddress: string,
-  queryOptions: UseQueryOptions<PontusXAccountsMetadata, unknown, PontusXAccountsMetadata, string[]>,
-): AccountMetadataInfo => {
-  const { isLoading, isError, error, data: allData } = usePontusXAccountsMetadata(queryOptions)
-  if (isError) {
-    console.log('Failed to load Pontus-X account names', error)
-  }
   return {
-    metadata: allData?.map.get(oasisAddress),
+    metadata: map.get(oasisAddress),
     isLoading,
-    isError,
+    isError: error !== undefined,
   }
 }
 
@@ -75,10 +69,12 @@ export const useSearchForPontusXAccountsByName = (
 ): AccountNameSearchRuntimeResults => {
   const {
     isLoading: isMetadataLoading,
-    isError: isMetadataError,
     error: metadataError,
-    data: namedAccounts,
-  } = usePontusXAccountsMetadata(queryOptions)
+    list: namedAccountsList,
+  } = usePontusXAccountsMetadata()
+
+  const isMetadataError = metadataError !== undefined
+
   if (isMetadataError) {
     console.log('Failed to load Pontus-X account names', metadataError)
   }
@@ -86,7 +82,7 @@ export const useSearchForPontusXAccountsByName = (
   const matches =
     isMetadataLoading || !nameFragments.length || !queryOptions.enabled
       ? undefined
-      : namedAccounts?.list
+      : namedAccountsList
           .filter(account => hasTextMatchesForAll(account.name, nameFragments))
           .map(
             (account): AccountNameSearchRuntimeMatch => ({
