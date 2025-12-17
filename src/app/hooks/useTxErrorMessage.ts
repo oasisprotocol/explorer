49b4c24ec4f486bfe5a2bf2c468364288c4c8984
lukaw3d <luka@oasisprotocol.org>
2025-12-17 12:29:48 +0100

    Revert "Don't show TX error messages for empty error blocks"
    This reverts commit eb18b00738dd7088ad0c2a272326b4a2a4db1f7e.

--- src/app/hooks/useTxErrorMessage.ts
+++ src/app/hooks/useTxErrorMessage.ts
@@ -3,11 +3,9 @@ import { TxError } from '../../oasis-nexus/api'
 
 export const useTxErrorMessage = (error: TxError | undefined): string | undefined => {
   const { t } = useTranslation()
-  if (
-    !error ||
-    (!error.module && !error.code) // TODO: This second check shouldn't be necessary. Remove when https://github.com/oasisprotocol/nexus/issues/704 is fixed.
-  )
+  if (!error) {
     return undefined
+  }
   if (error.module === 'evm' && error.code === 8 && !error.message && !error.raw_message) {
     // EVM reverted, without any message
     return `${t('errors.revertedWithoutMessage')} (${t('errors.code')} ${error.code}, ${t('errors.module')}: ${error.module})`
