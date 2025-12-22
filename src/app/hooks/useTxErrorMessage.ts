dab4d78f6e453bc91222490a791262479973a57e
lukaw3d <luka@oasisprotocol.org>
2025-12-22 21:09:01 +0100

    Revert workaround adding empty revert message to transactions without it
    Reverts dd6fc7ad0116d7c7359c813c8db77efe5c4b7921

--- src/app/hooks/useTxErrorMessage.ts
+++ src/app/hooks/useTxErrorMessage.ts
@@ -3,13 +3,6 @@ import { TxError } from '../../oasis-nexus/api'
 
 export const useTxErrorMessage = (error: TxError | undefined): string | undefined => {
   const { t } = useTranslation()
-  if (!error) {
-    return undefined
-  }
-  if (error.module === 'evm' && error.code === 8 && !error.message && !error.raw_message) {
-    // EVM reverted, without any message
-    return `${t('errors.revertedWithoutMessage')} (${t('errors.code')} ${error.code}, ${t('errors.module')}: ${error.module})`
-  }
-
+  if (!error) return undefined
   return `${error.message || error.raw_message} (${t('errors.code')} ${error.code}, ${t('errors.module')}: ${error.module})`
 }
