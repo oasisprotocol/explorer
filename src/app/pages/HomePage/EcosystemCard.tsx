030d70abbb0988467504eb63a784c79991e951da
Kaja <kaja.osojnik@gmail.com>
2026-01-09 14:34:08 +0100

    Ecosystem card button alignment for large screens

--- src/app/pages/HomePage/EcosystemCard.tsx
+++ src/app/pages/HomePage/EcosystemCard.tsx
@@ -128,13 +128,13 @@ export const EcosystemCard: FC<EcosystemCardProps> = ({
       </CardContent>
       <CardFooter>
         {footer || (
-          <div className="flex w-full gap-4">
-            <Button variant="outline" size="lg" className="flex-1" asChild>
+          <div className="flex w-full lg:flex-col xl:flex-row gap-4">
+            <Button variant="outline" size="lg" className="flex-1 py-[10px]" asChild>
               <RouterLink to={RouteUtils.getDashboardRoute({ network: 'testnet', layer })}>
                 {t('common.testnet')}
               </RouterLink>
             </Button>
-            <Button size="lg" className="flex-1" asChild>
+            <Button size="lg" className="flex-1 py-[10px]" asChild>
               <RouterLink to={RouteUtils.getDashboardRoute({ network: 'mainnet', layer })}>
                 {t('common.mainnet')}
                 <ArrowRight />
