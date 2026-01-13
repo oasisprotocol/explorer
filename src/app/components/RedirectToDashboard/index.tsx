7c60e23d37561305b5cd29acc2bf2654efca8dfc
Michal Zielenkiewicz <michal@oasisprotocol.org>
2026-01-13 16:39:05 +0100

    Fix lint issues in routes

---
+++ src/app/components/RedirectToDashboard/index.tsx
@@ -0,0 +1,35 @@
+import { useLocalSettings } from 'app/hooks/useLocalSettings'
+import { RouteUtils, fixedNetwork, fixedLayer } from 'app/utils/route-utils'
+import { FC, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { SearchScope } from 'types/searchScope'
+
+/**
+ * In case of being restricted to a specific layer or layers, jump to a dashboard
+ *
+ * This should be rendered on the landing page, since we don't want the opening graph.
+ */
+export const RedirectToDashboard: FC = () => {
+  const navigate = useNavigate()
+  const {
+    settings: { preferredScope },
+  } = useLocalSettings()
+
+  const getPreferredScope = () =>
+    !preferredScope
+      ? undefined
+      : RouteUtils.getEnabledScopes().find(
+          scope => scope.network === preferredScope.network && scope.layer === preferredScope.layer,
+        )
+
+  const getDefaultScope = (): SearchScope => ({
+    network:
+      (fixedNetwork ?? fixedLayer)
+        ? RouteUtils.getEnabledNetworksForLayer(fixedLayer)[0]!
+        : RouteUtils.getEnabledScopes()[0].network,
+    layer: fixedLayer ?? RouteUtils.getEnabledScopes()[0].layer,
+  })
+
+  useEffect(() => navigate(RouteUtils.getDashboardRoute(getPreferredScope() ?? getDefaultScope())))
+  return null
+}
---
+++ src/app/components/ScopeSpecificPart/index.tsx
@@ -0,0 +1,12 @@
+import { useRequiredScopeParam } from 'app/hooks/useScopeParam'
+import { Outlet } from 'react-router-dom'
+import { ThemeByScope } from '../ThemeByScope'
+
+export const ScopeSpecificPart = () => {
+  const { network } = useRequiredScopeParam()
+  return (
+    <ThemeByScope network={network}>
+      <Outlet />
+    </ThemeByScope>
+  )
+}
