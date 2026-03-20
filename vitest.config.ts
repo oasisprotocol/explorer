0a9a64d08aef9c73a6e05e788205eb8b18c2d858
buberdds <michal@oasisprotocol.org>
2026-03-20 10:36:46 +0100

    Adjust build configs to updated dependecies

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,12 @@
 import { loadEnv } from 'vite'
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
+  resolve: {
+    tsconfigPaths: true,
+  },
   test: {
     globals: true,
     environment: 'jsdom',
